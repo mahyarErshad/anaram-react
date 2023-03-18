d166ed68f55cbd0e5ccb7d34c2976e79cd552cc9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:12:42 +0330

    Text comes from props

--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function NavLinks() {
+function NavLinks(props) {
   return (
     <li className="flex-center gap-2 text-sm text-NeutralN300 cursor-pointer px-6 py-3 border-l border-NeutralN30">
-      باتری بر اساس سایز <Icon />
+      {props.text}<Icon />
     </li>
   );
 }
