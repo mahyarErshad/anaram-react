a1e76924345b77702c1afc98e0f7cd75e8372e93
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:28:12 +0330

    Text color is conditional

--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -3,8 +3,9 @@ import { ReactComponent as Icon } from "../../../assets/images/arrow-icon-toBott
 
 function NavLinks(props) {
   return (
-    <li className="flex-center gap-2 text-sm text-NeutralN300 cursor-pointer px-6 py-3 border-l border-NeutralN30">
-      {props.text}{props.hasIcon && <Icon />}
+    <li className={`flex-center gap-2 text-sm ${props.textColor || "text-NeutralN300"} cursor-pointer px-6 py-3 border-l border-NeutralN30`}>
+      {props.text}
+      {props.hasIcon && <Icon />}
     </li>
   );
 }
