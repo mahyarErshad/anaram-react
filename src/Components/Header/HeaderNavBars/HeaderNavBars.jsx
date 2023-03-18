e42554078c30036ad9681da8c78e314a86c389b9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:06:26 +0330

    Defined NeutralN30 color

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -4,8 +4,8 @@ import { ReactComponent as Icon } from "../../../assets/images/arrow-icon-toBott
 function HeaderNavBars() {
   return (
     <nav>
-      <ul>
-        <li className="flex-center gap-2 text-sm text-NeutralN300">
+      <ul className="flex justify-start">
+        <li className="flex-center gap-2 text-sm text-NeutralN300 cursor-pointer px-6 py-4 border-l ">
           باتری بر اساس سایز <Icon />
         </li>
       </ul>
