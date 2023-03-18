c2b4f641cca76470b3e55880cf6e0059d3813e3f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:12:19 +0330

    Created a component for navLinks

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -5,9 +5,7 @@ function HeaderNavBars() {
   return (
     <nav>
       <ul className="flex justify-start">
-        <li className="flex-center gap-2 text-sm text-NeutralN300 cursor-pointer px-6 py-3 border-l border-NeutralN30">
-          باتری بر اساس سایز <Icon />
-        </li>
+
       </ul>
     </nav>
   );
---
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavLinks() {
+  return (
+    <li className="flex-center gap-2 text-sm text-NeutralN300 cursor-pointer px-6 py-3 border-l border-NeutralN30">
+      باتری بر اساس سایز <Icon />
+    </li>
+  );
+}
+
+export default NavLinks;
