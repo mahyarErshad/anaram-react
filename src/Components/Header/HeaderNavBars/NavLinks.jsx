15e8b7712bb7b3ca72bbb30fb81c1bc4e32e8332
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 15:54:04 +0330

    Changed the fonts

--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as Icon } from "../../../assets/images/arrow-icon-toBott
 
 function NavLinks(props) {
   return (
-    <li className={`flex-center gap-2 text-sm ${props.textColor || "text-NeutralN300"} cursor-pointer px-6 py-3 border-l border-NeutralN30`}>
+    <li className={`flex-center gap-2 text-sm ${props.textColor || "text-NeutralN300"} cursor-pointer px-6 py-3 border-l border-NeutralN30 font-semibold`}>
       {props.text}
       {props.hasIcon && <Icon />}
     </li>
