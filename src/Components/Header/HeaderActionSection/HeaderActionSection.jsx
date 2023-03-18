f823de4dc98e6ffd334786397dd04b2d27d821a5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 11:16:45 +0330

    width is 2/4

--- src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
+++ src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
@@ -5,7 +5,7 @@ import SearchBox from "../../Utils/SearchBox/SearchBox"
 function HeaderActionSection() {
   return (
     <div className="flex justify-between items-center w-full">
-      <div className="flex-center gap-6 w-[45%]">
+      <div className="flex-center gap-6 w-2/4">
         <Logo />
         <SearchBox className="flex-grow" />
       </div>
