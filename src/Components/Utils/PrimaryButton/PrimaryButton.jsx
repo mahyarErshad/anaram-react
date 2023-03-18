2349476e913444d10a434ca47d2d01bcd98b1e38
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:09:15 +0330

    Added icon and text

--- src/Components/Utils/PrimaryButton/PrimaryButton.jsx
+++ src/Components/Utils/PrimaryButton/PrimaryButton.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { ReactComponent as UserIcon } from "../../../assets/images/user-icon.svg";
 
 function PrimaryButton(props) {
-  return <button className={`${props.className} rounded-5px py-4 px-3 bg-primary6 flex-center gap-3 text-white text-sm`}>
-    
-  </button>;
+  return (
+    <button className={`${props.className} rounded-5px py-4 px-3 bg-primary6 flex-center gap-3 text-white text-sm`}>
+      <UserIcon />
+      ورود / ثبت نام
+    </button>
+  );
 }
 
 export default PrimaryButton;
