0b63e9b58eb84970bc803ef6e256bd113243313b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:53:40 +0330

    Added an svg file

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function HeaderNavBars() {
   return (
     <nav>
-        
+        <ul>
+            <li>
+            باتری بر اساس سایز 
+            </li>
+        </ul>
     </nav>
   )
 }
