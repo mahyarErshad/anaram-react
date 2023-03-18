b3cb139fcec908861d4b0d17833b870fb75abb9f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:12:09 +0330

    Changed the px and py values

--- src/Components/Utils/PrimaryButton/PrimaryButton.jsx
+++ src/Components/Utils/PrimaryButton/PrimaryButton.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as UserIcon } from "../../../assets/images/user-icon.svg
 
 function PrimaryButton(props) {
   return (
-    <button className={`${props.className} rounded-5px py-4 px-3 bg-primary6 flex-center gap-3 text-white text-sm`}>
+    <button className={`${props.className} rounded-5px px-4 py-3 bg-primary6 flex-center gap-3 text-white text-sm`}>
       <UserIcon />
       ورود / ثبت نام
     </button>
