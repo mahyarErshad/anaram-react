ed7ef7328047c490386cad9dc46a03a1655fa562
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:13:03 +0330

    Using NavLinks as a component

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ReactComponent as Icon } from "../../../assets/images/arrow-icon-toBottom.svg";
+import NavLinks from "./NavLinks";
 
 function HeaderNavBars() {
   return (
     <nav>
       <ul className="flex justify-start">
-
+        <NavLinks text="باتری بر اساس سایز " />
       </ul>
     </nav>
   );
