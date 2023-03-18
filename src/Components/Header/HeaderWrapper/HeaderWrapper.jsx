a1033fab10d69001d48faad309c4b2e969e481f4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:11:16 +0330

    Responsive is much better

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -4,7 +4,7 @@ import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
 
 function HeaderWrapper() {
   return (
-    <header className="py-4 px-36 max-w-[120rem] flex flex-col gap-6">
+    <header className="py-4 max-lg:px-4 lg:px-36 max-w-[120rem] flex flex-col gap-6">
       <HeaderActionSection />
       <HeaderNavBars />
     </header>
