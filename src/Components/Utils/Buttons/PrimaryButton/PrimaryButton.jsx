f9a636e0c7a43204ed1dc6addbcbb17ccc28c6c2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:36:19 +0330

    Fixed a bug

--- src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
+++ src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as UserIcon } from "../../../assets/images/user-icon.svg";
+import { ReactComponent as UserIcon } from "../../../../assets/images/user-icon.svg";
 
 function PrimaryButton(props) {
   return (
