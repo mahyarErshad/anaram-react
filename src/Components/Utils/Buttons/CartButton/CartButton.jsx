55ee588cfebf3911d538082aaf28e66685baa1f6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:39:36 +0330

    CartButton has styles

--- src/Components/Utils/Buttons/CartButton/CartButton.jsx
+++ src/Components/Utils/Buttons/CartButton/CartButton.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as Icon } from "../../../../assets/images/bag-icon.svg";
 
 function CartButton() {
   return (
-    <button className="flex-center">
+    <button className="flex-center w-12 h-12 bg-NeutralN10 rounded-5px">
       <Icon />
     </button>
   );
