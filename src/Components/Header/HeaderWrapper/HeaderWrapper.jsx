b175bc6a634989c31ea2f6901ad39cd62fd31e74
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:50:21 +0330

    Imported HeaderNavBars

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderActionSection from "../HeaderActionSection/HeaderActionSection";
+import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
 
 function HeaderWrapper() {
   return (
     <header className="py-4 px-36 max-w-[120rem] flex flex-col gap-6">
       <HeaderActionSection />
+      <HeaderNavBars />
     </header>
   );
 }
