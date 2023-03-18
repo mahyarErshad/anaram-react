5d76b7e336d9c9de21461482dd04ee46d3c233d4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 11:13:19 +0330

    Project is ready to go on with react

--- src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
+++ src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import React from "react";
-import logo from "../../../assets/images/logo.svg";
+import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
+import SearchBox from "../../Utils/SearchBox/SearchBox"
 
 function HeaderActionSection() {
   return (
     <div className="flex justify-between items-center w-full">
       <div className="flex-center gap-6 w-[45%]">
-        <Image src={logo} alt="Anaram shop logo" />
+        <Logo />
         <SearchBox className="flex-grow" />
       </div>
     </div>
--- src/Components/Utils/SearchBox/SearchBox.jsx
+++ src/Components/Utils/SearchBox/SearchBox.jsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import React from "react";
-import searchIcon from "@/public/images/search-icon.svg";
-import styles from "@/styles/SearchBox.module.css"
+import { ReactComponent as SearchIcon } from "../../../assets/images/search-icon.svg";
+import styles from "./styles.module.css";
 
 function SearchBox(props) {
   return (
     <div className={`${props.className} ${styles.wrapper} bg-[#f8f8f8] border border-transparent transition-all duration-300 py-4 flex px-6 rounded-[5px] gap-4 hover:border-[#E8ECF5]`}>
-      <Image src={searchIcon} alt="search icon" />
+      <SearchIcon />
       <input type="search" placeholder="جستجو" className="bg-transparent border-0 text-sm focus:outline-none w-full" />
     </div>
   );
