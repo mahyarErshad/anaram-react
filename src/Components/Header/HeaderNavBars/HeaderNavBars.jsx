fe5a25d71cd6db7cdaec0f76e3d4d4505a04740f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:23:04 +0330

    Having icon is conditional

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -6,6 +6,11 @@ function HeaderNavBars() {
     <nav>
       <ul className="flex justify-start">
         <NavLinks text="باتری بر اساس سایز " />
+        <NavLinks text="باتری بر اساس کاربرد " />
+        <NavLinks text="باتری بر اساس تکنولوژی " />
+        <NavLinks text="درباره آنارام" />
+        <NavLinks text="تماس با آنارام" />
+        <NavLinks text="وارتا" />
       </ul>
     </nav>
   );
--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as Icon } from "../../../assets/images/arrow-icon-toBott
 function NavLinks(props) {
   return (
     <li className="flex-center gap-2 text-sm text-NeutralN300 cursor-pointer px-6 py-3 border-l border-NeutralN30">
-      {props.text}<Icon />
+      {props.text}{props.hasIcon && <Icon />}
     </li>
   );
 }
