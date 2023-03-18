037f00541488e08355fbaa5846629ad3df4cba6b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:58:07 +0330

    Added Neutral300 color

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
+import { ReactComponent as Icon } from "../../../assets/images/arrow-icon-toBottom.svg";
 
 function HeaderNavBars() {
   return (
     <nav>
-        <ul>
-            <li>
-            باتری بر اساس سایز 
-            </li>
-        </ul>
+      <ul>
+        <li className="flex-center gap-2 text-sm">
+          باتری بر اساس سایز <Icon />
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default HeaderNavBars
+export default HeaderNavBars;
