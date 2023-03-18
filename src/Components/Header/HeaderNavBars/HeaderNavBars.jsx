a20de9d6a2c76a9190f4863a78429b55a0df671a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 14:24:01 +0330

    3 navlinks have icon

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -5,9 +5,9 @@ function HeaderNavBars() {
   return (
     <nav>
       <ul className="flex justify-start">
-        <NavLinks text="باتری بر اساس سایز " />
-        <NavLinks text="باتری بر اساس کاربرد " />
-        <NavLinks text="باتری بر اساس تکنولوژی " />
+        <NavLinks hasIcon text="باتری بر اساس سایز " />
+        <NavLinks hasIcon text="باتری بر اساس کاربرد " />
+        <NavLinks hasIcon text="باتری بر اساس تکنولوژی " />
         <NavLinks text="درباره آنارام" />
         <NavLinks text="تماس با آنارام" />
         <NavLinks text="وارتا" />
