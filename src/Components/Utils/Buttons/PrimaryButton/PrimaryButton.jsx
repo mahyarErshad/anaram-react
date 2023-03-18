0eacad5d17d7dee19a54fde7391edb2cf8377588
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:42:39 +0330

    Duration of transition has been increased

--- src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
+++ src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css"
 
 function PrimaryButton(props) {
   return (
-    <button className={`${props.className} ${styles.button} transition-all rounded-5px px-4 py-3 bg-primary6 flex-center gap-3 text-white text-sm`}>
+    <button className={`${props.className} ${styles.button} transition-all duration-300 rounded-5px px-4 py-3 bg-primary6 flex-center gap-3 text-white text-sm`}>
       <UserIcon />
       ورود / ثبت نام
     </button>
