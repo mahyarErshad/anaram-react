b64fb5a03a57fe14ba076dd868739903c6686452
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-18 13:07:59 +0330

    Changed the favicon path

--- src/Components/Utils/PrimaryButton/PrimaryButton.jsx
+++ src/Components/Utils/PrimaryButton/PrimaryButton.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function PrimaryButton(props) {
-  return (
-    <button className={`${props.className} rounded-5px `}>
-
-    </button>
-  )
+  return <button className={`${props.className} rounded-5px py-4 px-3 bg-primary6 flex-center gap-3 text-white text-sm`}>
+    
+  </button>;
 }
 
-export default PrimaryButton
+export default PrimaryButton;
