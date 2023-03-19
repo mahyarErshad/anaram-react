9d4d7c15b950a10a073d6d29dc811174c04f1687
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-19 09:30:49 +0330

    Imported MainPageWrapper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderWrapper from "./Components/Header/HeaderWrapper/HeaderWrapper";
+import MainPageWrapper from "./Components/MainPage/MainPageWrapper/MainPageWrapper";
 
 function App() {
   return (
     <>
       <HeaderWrapper />
+      <MainPageWrapper />
     </>
   );
 }
