f0bcdd6c9f94a96dfd4405a8773605baf88cf3b9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-19 08:29:12 +0330

    Added a wrapper for this section

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -3,16 +3,18 @@ import NavLinks from "./NavLinks";
 
 function HeaderNavBars() {
   return (
-    <nav>
-      <ul className="flex justify-start">
-        <NavLinks hasIcon text="باتری بر اساس سایز " />
-        <NavLinks hasIcon text="باتری بر اساس کاربرد " />
-        <NavLinks hasIcon text="باتری بر اساس تکنولوژی " />
-        <NavLinks text="درباره آنارام" />
-        <NavLinks text="تماس با آنارام" />
-        <NavLinks textColor="text-primary6" text="وارتا" />
-      </ul>
-    </nav>
+    <section className="flex items-center justify-between">
+      <nav>
+        <ul className="flex justify-start">
+          <NavLinks hasIcon text="باتری بر اساس سایز " />
+          <NavLinks hasIcon text="باتری بر اساس کاربرد " />
+          <NavLinks hasIcon text="باتری بر اساس تکنولوژی " />
+          <NavLinks text="درباره آنارام" />
+          <NavLinks text="تماس با آنارام" />
+          <NavLinks textColor="text-primary6" text="وارتا" />
+        </ul>
+      </nav>
+    </section>
   );
 }
 
