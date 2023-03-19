b6e772834dbbcba8ea205b608ce22e37c5984089
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-19 11:42:59 +0330

    Imported slider css

--- src/Components/MainPage/MainPageSlider/MainPageSlider.jsx
+++ src/Components/MainPage/MainPageSlider/MainPageSlider.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "./slider.css"
+import "./slider.css";
 
 // import required modules
 import { Autoplay, Navigation, Pagination, Keyboard } from "swiper";
