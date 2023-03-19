044a14ba3e1808cf23294e134c00d7caec96d869
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-19 09:45:37 +0330

    Imported MainPageSlider

--- src/Components/MainPage/MainPageWrapper/MainPageWrapper.jsx
+++ src/Components/MainPage/MainPageWrapper/MainPageWrapper.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import MainPageSlider from '../MainPageSlider/MainPageSlider'
 
 function MainPageWrapper() {
   return (
-    <div>MainPageWrapper</div>
+    <>
+        <MainPageSlider />
+    </>
   )
 }
 
