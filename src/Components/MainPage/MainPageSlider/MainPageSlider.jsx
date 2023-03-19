a0a96e136e0b648c618275535159a3c466926a3e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-19 14:43:11 +0330

    Added icon to prev button

--- src/Components/MainPage/MainPageSlider/MainPageSlider.jsx
+++ src/Components/MainPage/MainPageSlider/MainPageSlider.jsx
@@ -14,7 +14,7 @@ import { Autoplay, Pagination, Keyboard } from "swiper";
 import xbox from "../../../assets/images/Sliders/Xbox.jpg";
 import light1 from "../../../assets/images/Sliders/light1.jpg";
 import light2 from "../../../assets/images/Sliders/light2.jpg";
-import prevIcon from "../../../assets/images/icons/prev-icon.svg"
+import { ReactComponent as PrevIcon } from "../../../assets/images/icons/prev-icon.svg";
 
 export default function MainPageSlider() {
   const images = [xbox, light1, light2];
@@ -53,8 +53,8 @@ export default function MainPageSlider() {
           );
         })}
         <div className="pagination-navigation-wrapper w-fit flex-center gap-3">
-          <div onClick={prevHandler} className="swiper-button-prev">
-
+          <div onClick={prevHandler} className="swiper-button-prev flex-center">
+            <PrevIcon />
           </div>
           <div className="swiper-pagination flex-center cursor-pointer gap-2"></div>
           <div onClick={nextHandler} className="swiper-button-next"></div>
