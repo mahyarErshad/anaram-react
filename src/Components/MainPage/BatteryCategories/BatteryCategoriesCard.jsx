d12502d0c931a37e85fc7f16d420fb4957430b96
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:03:01 +0430

    Added Images and title

--- src/Components/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
+++ src/Components/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import stage from "../../../assets/images/data/battery-categories/stage.png";
 
-function BatteryCategoriesCard() {
+function BatteryCategoriesCard(props) {
   return (
-    <figure className='flex flex-col'>
-    
+    <figure className="flex flex-col">
+      <img className="mb-2" src={props.image} alt={props.text} />
+      <img className="mb-3" src={stage} alt="stage for batteries" />
+      <figcaption className="text-NeutralN300 text-base">{props.text}</figcaption>
     </figure>
-  )
+  );
 }
 
-export default BatteryCategoriesCard
+export default BatteryCategoriesCard;
