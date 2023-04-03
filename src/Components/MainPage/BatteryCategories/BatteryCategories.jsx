08d465b35ff67cdbeb75e4606f39b93089057047
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 16:53:16 +0430

    Div Created for cards

--- src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
+++ src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function BatteryCategories() {
   return (
-    <section className='w-full px-4 mt-16 flex-center flex-col'>
+    <section className='w-full px-4 mt-16 flex-center flex-col gap-6'>
         <h2 className='text-xl font-bold text-NeutralN500'>انواع باتری <strong className='text-xl font-bold text-primary6'>وارتا</strong></h2>
+        <div className='flex-center pt-8 pb-6 max-w-[82.5rem] bg-NeutralN10 border border-NeutralN30 rounded-2xl'>
+
+        </div>
     </section>
   )
 }
