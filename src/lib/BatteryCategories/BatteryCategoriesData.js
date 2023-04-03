30b5fde468e1008541ccbbc3f060f9802b927155
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:07:44 +0430

    Texts are editted

--- src/lib/BatteryCategories/BatteryCategoriesData.js
+++ src/lib/BatteryCategories/BatteryCategoriesData.js
@@ -1,13 +1,34 @@
-import black from "../../assets/images/data/battery-categories/black.png"
-import gold from "../../assets/images/data/battery-categories/gold.png"
-import blue from "../../assets/images/data/battery-categories/blue.png"
-import red from "../../assets/images/data/battery-categories/red.png"
-import green from "../../assets/images/data/battery-categories/green.png"
-import coin from "../../assets/images/data/battery-categories/coin.png"
+import black from "../../assets/images/data/battery-categories/black.png";
+import gold from "../../assets/images/data/battery-categories/gold.png";
+import blue from "../../assets/images/data/battery-categories/blue.png";
+import red from "../../assets/images/data/battery-categories/red.png";
+import green from "../../assets/images/data/battery-categories/green.png";
+import coin from "../../assets/images/data/battery-categories/coin.png";
 
 const BatteryCategories = [
-    {
-
-    },
-]
-export default BatteryCategories
+  {
+    image: black,
+    text: "باتری قلمی",
+  },
+  {
+    image: gold,
+    text: "باتری نیم قلمی",
+  },
+  {
+    image: blue,
+    text: "باتری متوسط",
+  },
+  {
+    image: red,
+    text: "باتری بزرگ",
+  },
+  {
+    image: green,
+    text: "باتری کتابی",
+  },
+  {
+    image: coin,
+    text: "باتری سکه ای",
+  },
+];
+export default BatteryCategories;
