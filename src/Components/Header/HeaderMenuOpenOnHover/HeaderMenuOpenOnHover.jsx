791dca1bf81a2e057b35ae9363fe91b644139f27
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 12:35:50 +0430

    Created module style file

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -3,8 +3,8 @@ import headerDropDownMockData from "../../../lib/HeaderDropDownMockData/HeaderDr
 
 function HeaderMenuOpenOnHover() {
   return (
-    <div className="absolute flex-center bg-transparent opacity-0 top-[11rem] right-0 z-50 transition-all duration-300 w-full px-6 pt-6 pb-8">
-      <ul>
+    <div className="absolute flex-center bg-transparent opacity-0 top-[11rem] right-0 z-50 transition-all duration-300 w-full">
+      <ul className="md:w-full max-w-[103.125rem] bg-white flex-center px-6 pt-6 pb-8">
         {headerDropDownMockData.map((item, index) => {
           return <li key={index}>{item.text}</li>;
         })}
