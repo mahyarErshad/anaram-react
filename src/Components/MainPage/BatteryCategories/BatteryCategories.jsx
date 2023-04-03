426bd52aec3130e1abbd10ebe8fb7fab7ee1eed0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 16:47:21 +0430

    Defined NeutralN500 color

--- src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
+++ src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function BatteryCategories() {
   return (
-    <section className='w-full px-4 mt-16 flex-center flex-col'>BatteryCategories</section>
+    <section className='w-full px-4 mt-16 flex-center flex-col'>
+        <h2>انواع باتری <strong>وارتا</strong></h2>
+    </section>
   )
 }
 
