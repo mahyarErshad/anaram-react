b269518f0bf5d06ec73b37df08ed30cdfcc78e8a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 12:37:34 +0430

    Styled the container

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -5,7 +5,7 @@ import styles from "./style.module.css"
 function HeaderMenuOpenOnHover() {
   return (
     <div className="absolute flex-center bg-transparent opacity-0 top-[11rem] right-0 z-50 transition-all duration-300 w-full">
-      <ul className="md:w-full max-w-[103.125rem] bg-white flex-center px-6 pt-6 pb-8">
+      <ul className={`${styles.container} md:w-full max-w-[103.125rem] bg-white flex-center px-6 pt-6 pb-8`}>
         {headerDropDownMockData.map((item, index) => {
           return <li key={index}>{item.text}</li>;
         })}
