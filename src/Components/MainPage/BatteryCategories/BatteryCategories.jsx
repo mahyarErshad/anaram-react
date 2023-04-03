1505e50730578444e060e496d3d14ba394c4243e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:20:18 +0430

    Max lg flex wrap is activated

--- src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
+++ src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
@@ -8,7 +8,7 @@ function BatteryCategories() {
       <h2 className="text-xl font-bold text-NeutralN500">
         انواع باتری <strong className="text-xl font-bold text-primary6">وارتا</strong>
       </h2>
-      <div className="flex-center pt-8 pb-6 max-w-[82.5rem] bg-NeutralN10 border border-NeutralN30 rounded-2xl px-1">
+      <div className="flex-center pt-8 pb-6 max-w-[82.5rem] bg-NeutralN10 border border-NeutralN30 rounded-2xl px-1 max-lg:flex-wrap">
       {
         BatteryCategoriesData.map((data,index)=>{
             return <BatteryCategoriesCard key={index} data={data} />
