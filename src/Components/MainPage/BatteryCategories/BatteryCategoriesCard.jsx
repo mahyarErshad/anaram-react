442ae3ffe9df24e2e25ebddde294a92be6c30ce4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:21:23 +0430

    Max lg 1.5 rem margin top is added

--- src/Components/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
+++ src/Components/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
@@ -4,7 +4,7 @@ import styles from "./style.module.css"
 
 function BatteryCategoriesCard({data}) {
   return (
-    <figure className={`flex-center flex-col cursor-pointer ${styles.card}`}>
+    <figure className={`flex-center flex-col cursor-pointer max-lg:mt-6 ${styles.card}`}>
       <img className="mb-2 max-h-[5.75rem] object-contain transition-all duration-300" src={data.image} alt={data.text} />
       <img className="mb-3" src={stage} alt="stage for batteries" />
       <figcaption className="text-NeutralN300 text-base">{data.text}</figcaption>
