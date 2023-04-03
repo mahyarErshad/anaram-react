17c20d46c0c230021532d4aff9c419217d476756
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:37:13 +0430

    Heading is created

--- src/Pages/MainPage/OtherProducts/OtherProducts.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProducts.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import OtherProductsCards from "./OtherProductsCards";
 
 function OtherProducts() {
   return (
-    <section className="w-full px-4 mt-16 flex-center">
-    
+    <section className="w-full px-4 mt-16 flex-center flex-col gap-14">
+      <h2 className="text-xl font-bold text-NeutralN500">
+        محصولات دیگر<strong className="text-xl font-bold text-primary6">آنارام</strong>
+      </h2>
+      <OtherProductsCards />
     </section>
-  )
+  );
 }
 
-export default OtherProducts
+export default OtherProducts;
