f4be5c3176de0edf6d2e1ddcd59633def08e2176
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:05:21 +0430

    Imported images

---
+++ src/lib/BatteryCategories/BatteryCategoriesData.js
@@ -0,0 +1,13 @@
+import black from "../../assets/images/data/battery-categories/black.png"
+import gold from "../../assets/images/data/battery-categories/gold.png"
+import blue from "../../assets/images/data/battery-categories/blue.png"
+import red from "../../assets/images/data/battery-categories/red.png"
+import green from "../../assets/images/data/battery-categories/green.png"
+import coin from "../../assets/images/data/battery-categories/coin.png"
+
+const BatteryCategories = [
+    {
+
+    },
+]
+export default BatteryCategories
