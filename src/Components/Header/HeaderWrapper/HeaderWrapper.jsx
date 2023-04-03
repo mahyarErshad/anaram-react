2f49e15df7970b55a3a509337f98a6a1f90a8aa0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 10:43:26 +0430

    Imported HeaderHoverMenu

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderActionSection from "../HeaderActionSection/HeaderActionSection";
 import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
+import HeaderHoverMenu from "../HeaderHoverMenu/HeaderHoverMenu";
 
 function HeaderWrapper() {
   return (
     <header className="py-4 px-4 flex flex-col gap-6">
       <HeaderActionSection />
       <HeaderNavBars />
+      <HeaderHoverMenu />
     </header>
   );
 }
