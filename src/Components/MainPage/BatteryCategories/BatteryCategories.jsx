8e945daf3c8b3eb9cd5ff6fcd94341300c43e6f2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 16:49:37 +0430

    Added a new heading

--- src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
+++ src/Components/MainPage/BatteryCategories/BatteryCategories.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BatteryCategories() {
   return (
     <section className='w-full px-4 mt-16 flex-center flex-col'>
-        <h2>انواع باتری <strong>وارتا</strong></h2>
+        <h2 className='text-xl font-bold text-NeutralN500'>انواع باتری <strong className='text-xl font-bold text-primary6'>وارتا</strong></h2>
     </section>
   )
 }
