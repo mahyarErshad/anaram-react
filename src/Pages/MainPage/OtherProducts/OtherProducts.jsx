46aac28e4419bfa7306b4b9f9aa323702b073c78
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:38:42 +0430

    Text edited

--- src/Pages/MainPage/OtherProducts/OtherProducts.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProducts.jsx
@@ -5,7 +5,7 @@ function OtherProducts() {
   return (
     <section className="w-full px-4 mt-16 flex-center flex-col gap-14">
       <h2 className="text-xl font-bold text-NeutralN500">
-        محصولات دیگر<strong className="text-xl font-bold text-primary6">آنارام</strong>
+        محصولات دیگر <strong className="text-xl font-bold text-primary6">آنارام</strong>
       </h2>
       <OtherProductsCards />
     </section>
