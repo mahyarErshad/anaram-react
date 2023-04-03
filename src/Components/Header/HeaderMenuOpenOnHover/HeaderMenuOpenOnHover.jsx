61db5e29541a9ce17bd77f538e36910efbe994ae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 14:03:01 +0430

    Fixed afew bugs

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import headerDropDownMockData from "../../../lib/HeaderDropDownMockData/HeaderDropDownMockData";
 import styles from "./style.module.css";
 
-function HeaderMenuOpenOnHover() {
+function HeaderMenuOpenOnHover(props) {
   return (
-    <div className="absolute flex-center bg-transparent opacity-0 top-[11rem] right-0 z-50 transition-all duration-300 w-full cursor-default">
-      <ul className={`${styles.container} md:w-full max-w-[103.125rem] bg-white flex-center px-6 pt-6 pb-8 cursor-default`}>
-        {headerDropDownMockData.map((item, index) => {
+    <div className="absolute flex-center bg-transparent max-w-[103.125rem] opacity-0 top-[11rem] right-0 z-50 w-full cursor-default">
+      <ul className={`${styles.container} md:w-full w-full bg-white flex justify-start items-center px-6 pt-6 pb-8 cursor-default`}>
+        {props.data.map((item, index) => {
           return (
             <li className="px-[3.1875rem] flex-center flex-col gap-4 border-l min-h-[9.8125rem] border-NeutralN30 cursor-pointer" key={index}>
               <img src={item.image} alt={item.text} />
--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { ReactComponent as Icon } from "../../../assets/images/icons/arrow-icon-toBottom.svg";
 import HeaderMenuOpenOnHover from "../HeaderMenuOpenOnHover/HeaderMenuOpenOnHover";
 import styles from "./styles.module.css";
+import headerDropDownMockData from "../../../lib/HeaderDropDownMockData/HeaderDropDownMockData";
 
 function NavLinks(props) {
   return (
     <li className={`flex-center gap-2 text-sm ${props.textColor || "text-NeutralN300"} cursor-pointer px-6 py-3 border-l border-NeutralN30 font-semibold ${styles.navlink}`}>
       {props.text}
       {props.hasIcon && <Icon />}
-      {props.hasIcon && <HeaderMenuOpenOnHover />}
+      {props.hasIcon && <HeaderMenuOpenOnHover data={headerDropDownMockData} />}
     </li>
   );
 }
