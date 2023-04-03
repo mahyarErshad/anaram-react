781ec68396b3b6c4aec74267ec3fcea555215ffc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 11:37:09 +0430

    Working on opening and closing of HeaderMenuOpenOnHover

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderMenuOpenOnHover() {
   return (
-    <ul className="absolute scale-y-0 bottom-[-150%] right-0 z-50 bg-white">
+    <ul className="absolute opacity-0 bottom-[-164%] right-0 z-50 bg-white transition-all duration-300 max-w-[1651px] px-6 pt-6 pb-8">
     <li>adfdsfsdf</li>
   </ul>
   )
