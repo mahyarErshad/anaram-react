17e3830f7bc05cc0392418ccff622a700a945af1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 15:04:33 +0430

    Removed border

--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -6,7 +6,7 @@ import headerDropDownMockData from "../../../lib/HeaderDropDownMockData/HeaderDr
 
 function NavLinks(props) {
   return (
-    <li className={`flex-center gap-2 text-sm ${props.textColor || "text-NeutralN300"} cursor-pointer px-6 py-7 border-l border-NeutralN30 font-semibold ${styles.navlink}`}>
+    <li className={`flex-center gap-2 text-sm ${props.textColor || "text-NeutralN300"} cursor-pointer px-6 py-7 font-semibold ${styles.navlink}`}>
       {props.text}
       {props.hasIcon && <Icon />}
       {props.hasIcon && <HeaderMenuOpenOnHover data={headerDropDownMockData} />}
