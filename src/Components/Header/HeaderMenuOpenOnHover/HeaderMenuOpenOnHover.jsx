12662b3468f62083470fdd721ebcc03f4f79ba4b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 13:39:35 +0430

    Cursor is default

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -4,7 +4,7 @@ import styles from "./style.module.css";
 
 function HeaderMenuOpenOnHover() {
   return (
-    <div className="absolute flex-center bg-transparent opacity-0 top-[11rem] right-0 z-50 transition-all duration-300 w-full">
+    <div className="absolute flex-center bg-transparent opacity-0 top-[11rem] right-0 z-50 transition-all duration-300 w-full cursor-default">
       <ul className={`${styles.container} md:w-full max-w-[103.125rem] bg-white flex-center px-6 pt-6 pb-8 cursor-default`}>
         {headerDropDownMockData.map((item, index) => {
           return (
