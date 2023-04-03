c785c6f906f70dc2968d2e186629195c452dfcc5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 15:38:34 +0430

    Added animation to cards image

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -7,8 +7,8 @@ function HeaderMenuOpenOnHover(props) {
       <ul className={`${styles.container} md:w-full w-full bg-white flex justify-start items-center px-6 pt-6 pb-8 cursor-default`}>
         {props.data.map((item, index) => {
           return (
-            <li className="px-[3.1875rem] flex-center flex-col gap-4 border-l min-h-[9.8125rem] border-NeutralN30 cursor-pointer" key={index}>
-              <img src={item.image} alt={item.text} />
+            <li className={`px-[3.1875rem] flex-center flex-col gap-4 border-l min-h-[9.8125rem] border-NeutralN30 cursor-pointer ${styles.card}`} key={index}>
+              <img className="transition-all" src={item.image} alt={item.text} />
               <h2 className="text-xs text-[#616161]">{item.text}</h2>
             </li>
           );
