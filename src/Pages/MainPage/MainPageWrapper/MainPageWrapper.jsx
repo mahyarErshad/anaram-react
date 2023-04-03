a22c1e932e7a2e315904bd63b2bf94be4b2688fd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 17:37:29 +0430

    Imported OtherProductsCards

--- src/Pages/MainPage/MainPageWrapper/MainPageWrapper.jsx
+++ src/Pages/MainPage/MainPageWrapper/MainPageWrapper.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MainPageSlider from "../MainPageSlider/MainPageSlider";
 import BatteryCategories from "../BatteryCategories/BatteryCategories";
+import OtherProductsCards from "../OtherProducts/OtherProductsCards";
 
 function MainPageWrapper() {
   return (
     <>
       <MainPageSlider />
       <BatteryCategories />
+      <OtherProductsCards />
     </>
   );
 }
