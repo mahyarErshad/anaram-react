5ca08c2b6f99575ec1a48e59174ff3dc4d06fed4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 11:43:52 +0430

    aaaRed is done

--- src/lib/HeaderDropDownMockData/HeaderDropDownMockData.js
+++ src/lib/HeaderDropDownMockData/HeaderDropDownMockData.js
@@ -1,9 +1,14 @@
 import aaaBlack from "../../assets/images/dropdown-data/aaa-black.png";
+import aaaRed from "../../assets/images/dropdown-data/aa-red.png";
 
 const headerDropDownMockData = [
   {
     image: aaaBlack,
     text: "باتری قلمی",
+  },
+  {
+    image: aaaRed,
+    text: "باتری نیم قلمی",
   },
 ];
 export default headerDropDownMockData
