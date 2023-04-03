e3eae8915751fc2f10bfbbc6fbd3d1b35f5e4d83
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 11:00:08 +0430

    Background color is white

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderMenuOpenOnHover() {
   return (
-    <ul className="absolute scale-y-0 bottom-[-150%] right-0 z-50">
+    <ul className="absolute scale-y-0 bottom-[-150%] right-0 z-50 bg-white">
     <li>adfdsfsdf</li>
   </ul>
   )
