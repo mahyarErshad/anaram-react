769dc39bde96dbccceeeed3aa1bf32f4f4a8f5d2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 12:32:21 +0430

    Changed the ul to div

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import headerDropDownMockData from '../../../lib/HeaderDropDownMockData/HeaderDropDownMockData'
+import React from "react";
+import headerDropDownMockData from "../../../lib/HeaderDropDownMockData/HeaderDropDownMockData";
 
 function HeaderMenuOpenOnHover() {
   return (
-    <ul className="absolute flex-center bg-white opacity-0 top-[11rem] right-0 z-50 bg-transparent transition-all duration-300 w-full px-6 pt-6 pb-8">
-    {headerDropDownMockData.map((item, index)=>{
-        return <li key={index}>{item.text}</li>
-    })}
-  </ul>
-  )
+    <div className="absolute flex-center bg-transparent opacity-0 top-[11rem] right-0 z-50 transition-all duration-300 w-full px-6 pt-6 pb-8">
+      <ul>
+        {headerDropDownMockData.map((item, index) => {
+          return <li key={index}>{item.text}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default HeaderMenuOpenOnHover
+export default HeaderMenuOpenOnHover;
