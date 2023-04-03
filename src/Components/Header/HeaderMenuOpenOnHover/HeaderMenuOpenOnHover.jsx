4480c9f244859e971cdbf43f8ba947715f951882
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 10:59:49 +0430

    Menu opens using scale-y

--- src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
+++ src/Components/Header/HeaderMenuOpenOnHover/HeaderMenuOpenOnHover.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderMenuOpenOnHover() {
   return (
-    <ul className="absolute hidden bottom-[-150%] right-0 z-50">
+    <ul className="absolute scale-y-0 bottom-[-150%] right-0 z-50">
     <li>adfdsfsdf</li>
   </ul>
   )
