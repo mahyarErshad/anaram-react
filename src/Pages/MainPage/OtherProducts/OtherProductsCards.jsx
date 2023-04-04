52c7bbfd395b99d229336fc89e33d7c33ac33b98
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 09:40:24 +0430

    Image and text are added

--- src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function OtherProductsCards({data}) {
+function OtherProductsCards({ data }) {
   return (
     <figure className={`flex flex-col relative overflow-visible px-4 pt-4 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
-        
+      <figcaption className="self-start text-2xl font-bold">{data.text}</figcaption>
+      <img className="self-end" src={data.image} alt={data.text} />
     </figure>
-  )
+  );
 }
 
-export default OtherProductsCards
+export default OtherProductsCards;
