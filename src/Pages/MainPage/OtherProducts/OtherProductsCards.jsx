9b5c7b3bea62063ead97c00e1b85797fdf044b2d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 09:56:53 +0430

    gap increased to 40

--- src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import stage from "../../../assets/images/data/other-products/stage.png"
+import stage from "../../../assets/images/data/other-products/stage.png";
 
 function OtherProductsCards({ data }) {
   return (
-    <figure className={`flex flex-col relative overflow-visible pr-4 pt-4 gap-2 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
+    <figure className={`flex flex-col relative overflow-y-visible pr-4 pt-4 gap-2 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
+      <img className="absolute object-contain left-[6.6875rem] top-[-6.5625rem]" src={data.image} alt={data.text} />
       <figcaption className="self-start text-2xl font-bold">{data.text}</figcaption>
       <img className="self-end mr-16 -translate-x-6" src={stage} alt="Stage for showing the products" />
     </figure>
