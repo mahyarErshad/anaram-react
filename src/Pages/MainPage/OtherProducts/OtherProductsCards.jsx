15733769f2d2534310dc18c1efe412416e27b4a1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 10:59:24 +0430

    OtherProducts is done

--- src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import stage from "../../../assets/images/data/other-products/stage.png";
 
 function OtherProductsCards({ data }) {
   return (
-    <figure className={`flex flex-col relative overflow-y-visible pr-4 pt-4 gap-2 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
-      <img className="absolute object-contain left-[6.6875rem] top-[-6.5625rem]" src={data.image} alt={data.text} />
+    <figure className={`flex flex-col relative overflow-y-visible pr-4 pt-4 pb-2 gap-2 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
       <figcaption className="self-start text-2xl font-bold">{data.text}</figcaption>
-      <img className="self-end mr-16 -translate-x-6" src={stage} alt="Stage for showing the products" />
+      <img className="self-end mr-16 mt-[-8.5rem] w-[70%]" src={data.image} alt={data.text} />
     </figure>
   );
 }
