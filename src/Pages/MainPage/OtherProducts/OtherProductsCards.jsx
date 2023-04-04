845ff295e830f2bda50b223826cfbd99f3db122f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 09:43:18 +0430

    Add stage image

--- src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
@@ -3,9 +3,9 @@ import stage from "../../../assets/images/data/other-products/stage.png"
 
 function OtherProductsCards({ data }) {
   return (
-    <figure className={`flex flex-col relative overflow-visible px-4 pt-4 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
+    <figure className={`flex flex-col relative overflow-visible pr-4 pt-4 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
       <figcaption className="self-start text-2xl font-bold">{data.text}</figcaption>
-      <img className="self-end" src={data.image} alt={data.text} />
+      <img className="self-end" src={stage} alt="Stage for showing the products" />
     </figure>
   );
 }
