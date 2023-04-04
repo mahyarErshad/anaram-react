a4799a9f3455449d0aa597d3a6e753260fe1c60d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 11:08:40 +0430

    Section is created

--- src/Pages/MainPage/DiscountSection/DiscountSection.jsx
+++ src/Pages/MainPage/DiscountSection/DiscountSection.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function DiscountSection() {
   return (
-    <section>DiscountSection</section>
+    <section className='flex px-4 mt-[5.625rem] w-full max-w-[82.5rem]'>
+
+    </section>
   )
 }
 
