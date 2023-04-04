525bb1ad570b93070cf4709d4e325fc5e50f2b13
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 09:16:13 +0430

    Images imported

--- src/lib/OtherProducts/OtherProductsData.js
+++ src/lib/OtherProducts/OtherProductsData.js
@@ -1,7 +1,12 @@
+import flashLight from "../../assets/images/data/other-products/flash-light.png"
+import charger from "../../assets/images/data/other-products/charger.png"
+import powerBank from "../../assets/images/data/other-products/Powerbank.png"
+import cable from "../../assets/images/data/other-products/cable.png"
 
 const OtherProducts = [
     {
-
+        image: flashLight,
+        text: "چراغ قوه"
     },
 ]
 export default OtherProducts
