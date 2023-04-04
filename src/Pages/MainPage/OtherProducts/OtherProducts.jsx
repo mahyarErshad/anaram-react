b340abc2d7b4f6983993276946e5ba0fad429fd0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 09:51:26 +0430

    Image has been relocated

--- src/Pages/MainPage/OtherProducts/OtherProducts.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProducts.jsx
@@ -8,7 +8,7 @@ function OtherProducts() {
       <h2 className="text-xl font-bold text-NeutralN500">
         محصولات دیگر <strong className="text-xl font-bold text-primary6">آنارام</strong>
       </h2>
-      <div className="w-full flex-center max-lg:flex-wrap gap-6">
+      <div className="w-full max-w-[82.5rem] flex-center max-lg:flex-wrap gap-6">
         {OtherProductsData.map((data, index) => {
           return <OtherProductsCards key={index} data={data} />;
         })}
--- src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
@@ -3,9 +3,9 @@ import stage from "../../../assets/images/data/other-products/stage.png"
 
 function OtherProductsCards({ data }) {
   return (
-    <figure className={`flex flex-col relative overflow-visible pr-4 pt-4 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
+    <figure className={`flex flex-col relative overflow-visible pr-4 pt-4 gap-2 rounded-2xl bg-gradient-to-br ${data.gradient}`}>
       <figcaption className="self-start text-2xl font-bold">{data.text}</figcaption>
-      <img className="self-end" src={stage} alt="Stage for showing the products" />
+      <img className="self-end mr-16 -translate-x-6" src={stage} alt="Stage for showing the products" />
     </figure>
   );
 }
