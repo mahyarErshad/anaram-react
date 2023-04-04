7cf3f12740db00dc91724dfd34c9296b00229441
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 15:04:50 +0430

    Changed swiper ref

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ReactComponent as PercentageIcon } from "../../assets/images/icons/percentage-icon.svg";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { ReactComponent as ButtonIcon } from "../../assets/images/icons/carousel-button-arrow.svg";
@@ -8,14 +8,7 @@ import "swiper/css";
 import "./carousel.css";
 
 function Carousel() {
-  const [carouselRef, setCarouselRef] = useState("carousel");
-  const prevHandler = () => {
-    carouselRef.slidePrev();
-  };
 
-  const nextHandler = () => {
-    carouselRef.slideNext();
-  };
   return (
     <section className="container mt-[5.625rem] gap-7">
       <div className="flex w-full justify-between items-center">
@@ -26,16 +19,15 @@ function Carousel() {
           </h3>
         </div>
         <div className="flex-center gap-2">
-          <button onClick={prevHandler} className="carousel-button-prev flex-center">
+          <button className="carousel-button-prev flex-center">
             <ButtonIcon className="mirror" />
           </button>
-          <button onClick={nextHandler} className="carousel-button-next flex-center">
+          <button className="carousel-button-next flex-center">
             <ButtonIcon />
           </button>
         </div>
       </div>
-      <Swiper onSwiper={(swiper) => setCarouselRef(swiper)}>
-        <SwiperSlide>sfsgdfgd</SwiperSlide>
+      <Swiper>
         <SwiperSlide>sfsgdfgd</SwiperSlide>
       </Swiper>
     </section>
