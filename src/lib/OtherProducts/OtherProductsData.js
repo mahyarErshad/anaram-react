f1f687db3918dc1340cf82cea2e2c61aa0a9fd80
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 09:17:37 +0430

    Data are added

--- src/lib/OtherProducts/OtherProductsData.js
+++ src/lib/OtherProducts/OtherProductsData.js
@@ -1,12 +1,24 @@
-import flashLight from "../../assets/images/data/other-products/flash-light.png"
-import charger from "../../assets/images/data/other-products/charger.png"
-import powerBank from "../../assets/images/data/other-products/Powerbank.png"
-import cable from "../../assets/images/data/other-products/cable.png"
+import flashLight from "../../assets/images/data/other-products/flash-light.png";
+import charger from "../../assets/images/data/other-products/charger.png";
+import powerBank from "../../assets/images/data/other-products/Powerbank.png";
+import cable from "../../assets/images/data/other-products/cable.png";
 
-const OtherProducts = [
-    {
-        image: flashLight,
-        text: "چراغ قوه"
-    },
-]
-export default OtherProducts
+const OtherProductsData = [
+  {
+    image: flashLight,
+    text: "چراغ قوه",
+  },
+  {
+    image: charger,
+    text: "شارژر",
+  },
+  {
+    image: powerBank,
+    text: "پاور بانک",
+  },
+  {
+    image: cable,
+    text: "لوازم جانبی",
+  },
+];
+export default OtherProductsData;
