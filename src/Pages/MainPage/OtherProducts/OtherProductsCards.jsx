9de2dc302482cbf2e2ee2ef74f6cbea736e730b9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 09:33:38 +0430

    Container is styled

--- src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProductsCards.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function OtherProductsCards() {
+function OtherProductsCards({data}) {
   return (
-    <figure>
+    <figure className={`flex flex-col relative overflow-visible px-4 pt-4 rounded-2xl bg-gradient-to-bl ${data.gradient}`}>
         
     </figure>
   )
