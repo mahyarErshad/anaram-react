f4c85d4ecef0695c14daae363fce352b2e9256d3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-04 10:39:37 +0430

    Added percentage icon and h3

--- src/Pages/MainPage/DiscountSection/DiscountSection.jsx
+++ src/Pages/MainPage/DiscountSection/DiscountSection.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import percentageIcon from "../../../assets/images/percentage-icon.svg"
+import React from "react";
+import { ReactComponent as PercentageIcon } from "../../../assets/images/icons/percentage-icon.svg";
 
 function DiscountSection() {
   return (
-    <section className='container mt-[5.625rem]'>
-    <div className='flex w-full justify-between items-center'>
-
-    </div>
+    <section className="container mt-[5.625rem]">
+      <div className="flex w-full justify-between items-center">
+        <div className="flex-center gap-[0.625rem]">
+          <PercentageIcon />
+          <h3 className="text-2xl text-NeutralN500">
+            تخفیفات این هفته <span className="text-primary6">آنارام</span>
+          </h3>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default DiscountSection
+export default DiscountSection;
