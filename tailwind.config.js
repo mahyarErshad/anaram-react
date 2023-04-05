855354e2564782bebb03b7ca17f891d715c2a0ab
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 11:10:01 +0430

    Defined N90 color

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         NeutralN10: "#FAFAFA",
         NeutralN30: "#EBEBEB",
         NeutralN70: "#A3A3A3",
+        NeutralN90: "#858585",
         NeutralN300: "#575757",
         NeutralN500: "#3B3B3B",
         NeutralN600: "#2E2E2E",
