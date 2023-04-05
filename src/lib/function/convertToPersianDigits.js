2ecea8d178d71700cebb7ebf3a67e5a7ed7d1f25
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 16:19:30 +0430

    Changed the name of functions

--- src/lib/function/convertToPersianDigits.js
+++ src/lib/function/convertToPersianDigits.js
@@ -1,8 +1,8 @@
-export const persian = (number) => {
+export const convertToPersian = (number) => {
   const persian = number.replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d]);
   return persian;
 };
-export function separateNumber(number) {
+export function separateNumberByThousand(number) {
   let newNumber = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   newNumber = persian(newNumber);
   return newNumber;
