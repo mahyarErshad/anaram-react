9ee1a0e54c7fa07fc854d2d1bf1b4cf10a8828a1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 09:28:31 +0430

    Image has been added

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import batteryImage from "../../../assets/images/data/product-card/battery.png"
+import batteryImage from "../../../assets/images/data/product-card/battery.png";
 
 function ProductCardWithDiscount() {
-  return <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col">
-    <div className="flex-center h-2/4">
-    
-    </div>
-  </figure>;
+  return (
+    <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col">
+      <div className="flex-center h-2/4">
+        <img className="object-contain" src={batteryImage} alt="battery" />
+      </div>
+    </figure>
+  );
 }
 
 export default ProductCardWithDiscount;
