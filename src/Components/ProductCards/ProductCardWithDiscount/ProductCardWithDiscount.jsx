72d9f515d029c2506bf5c126d412e740f8e311ab
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 09:57:21 +0430

    Percentage mark has been added

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import batteryImage from "../../../assets/images/data/product-card/battery.png";
-import styles from "../style/productCard.module.css"
+import styles from "../style/productCard.module.css";
 
 function ProductCardWithDiscount() {
   return (
     <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col">
-      <div className="flex-center h-2/4 mb-4">
+      <div className="flex-center h-2/4 mb-4 relative">
         <img className="object-contain" src={batteryImage} alt="battery" />
-        <span className={styles["discount-percentage"]}>۵۰٪</span>
+        <span className={`${styles["discount-percentage"]} text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal`}>۵۰٪</span>
       </div>
     </figure>
   );
