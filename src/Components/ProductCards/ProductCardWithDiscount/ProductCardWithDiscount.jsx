6b8c503247c16e3c92626f3badaf71f6b0d1049d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 10:31:54 +0430

    Aligned the price and the icon together

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import batteryImage from "../../../assets/images/data/product-card/battery.png";
 import starIcon from "../../../assets/images/icons/product-card-star.svg";
-import {ReactComponent as TomanIcon} from "../../../assets/images/icons/toman-icon.svg"
+import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon.svg";
 import styles from "../style/productCard.module.css";
 
 function ProductCardWithDiscount() {
@@ -17,9 +17,10 @@ function ProductCardWithDiscount() {
       </div>
       <figcaption className="text-xs font-semibold max-w-[11rem] leading-[1.125rem] text-NeutralN600 mb-4">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</figcaption>
       <div className="flex justify-between items-center w-full">
-      <span>
-
-      </span>
+        <span className="flex-center gap-1">
+          ۱۰۲,۰۰۰
+          <TomanIcon />
+        </span>
       </div>
     </figure>
   );
