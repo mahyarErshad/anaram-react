56dc7445cad4e9be922373cd580954ceb99dc42f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 15:46:15 +0430

    Text has 300ms transition

--- src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
@@ -5,7 +5,7 @@ function ProductCardTimeCounter({number , text}) {
   return (
     <div className={`${styles.counter} flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30`}>
       <span className="text-xs font-semibold text-Magenta">{number}</span>
-      <span className="text-[0.5rem] text-NeutralN50">{text}</span>
+      <span className="text-[0.5rem] text-NeutralN50 transition-all duration-300">{text}</span>
     </div>
   );
 }
