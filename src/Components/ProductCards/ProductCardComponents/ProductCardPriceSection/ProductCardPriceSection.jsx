6b55a0161b404596751b7696f39459d8a32f4110
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 11:26:46 +0430

    Imported TomanIconGray

--- src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { ReactComponent as TomanIcon } from "../../../../assets/images/icons/toman-icon-black.svg";
+import { ReactComponent as TomanIconBlack } from "../../../../assets/images/icons/toman-icon-black.svg";
+import { ReactComponent as TomanIconGray } from "../../../../assets/images/icons/toman-icon-gray.svg";
 
 function ProductCardPriceSection({ discountText }) {
   const textStyles = discountText ? "text-NeutralN90  text-[0.75rem]" : "text-NeutralN700 text-sm";
   return (
     <span className={`${textStyles} flex-center gap-1 font-semibold`}>
       ۱۰۲,۰۰۰
-      <TomanIcon className="w-[0.9375rem] h-[0.6875rem] -translate-y-[0.125rem]" />
+      <TomanIconBlack className="w-[0.9375rem] h-[0.6875rem] -translate-y-[0.125rem]" />
     </span>
   );
 }
