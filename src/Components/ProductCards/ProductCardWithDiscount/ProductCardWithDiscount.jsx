a8b772e8428759df48efa2f62fbd802c4038a1cb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 09:50:58 +0430

    Added the background image

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -7,7 +7,7 @@ function ProductCardWithDiscount() {
     <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col">
       <div className="flex-center h-2/4 mb-4">
         <img className="object-contain" src={batteryImage} alt="battery" />
-
+        <span className={styles["discount-percentage"]}>۵۰٪</span>
       </div>
     </figure>
   );
