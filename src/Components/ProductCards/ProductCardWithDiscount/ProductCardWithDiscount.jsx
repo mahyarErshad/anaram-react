38967bec920193fa2a12ab5884c2c25ba5e392a7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 11:07:10 +0430

    Passed discountText through props

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -17,7 +17,7 @@ function ProductCardWithDiscount() {
       </div>
       <figcaption className="text-xs font-semibold max-w-[11rem] leading-[1.125rem] text-NeutralN600 mb-4">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</figcaption>
       <div className="flex justify-between items-center w-full">
-        <ProductCardPriceSection />
+        <ProductCardPriceSection discountText />
         <ProductCardPriceSection />
       </div>
     </figure>
