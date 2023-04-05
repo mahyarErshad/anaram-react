ec4f04d5445108cae17f19a648a347ba45cfabed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 09:22:28 +0430

    Removed external styles

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import styles from "../style/productCard.module.css"
+import React from "react";
 
 function ProductCardWithDiscount() {
-  return (
-    <figure className={`${styles.card} pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30`}>
-      
-    </figure>
-  )
+  return <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30">
+    
+  </figure>;
 }
 
-export default ProductCardWithDiscount
+export default ProductCardWithDiscount;
