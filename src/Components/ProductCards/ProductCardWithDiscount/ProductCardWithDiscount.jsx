8cb42d642f064fb4696431847e1acef56274e4da
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 09:45:03 +0430

    Imported styles

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import batteryImage from "../../../assets/images/data/product-card/battery.png";
+import styles from "../style/productCard.module.css"
 
 function ProductCardWithDiscount() {
   return (
     <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col">
       <div className="flex-center h-2/4 mb-4">
         <img className="object-contain" src={batteryImage} alt="battery" />
-        
+
       </div>
     </figure>
   );
