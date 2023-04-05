a14077cf0fd992cb9013cfc46376a3fac9955ab2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 09:27:39 +0430

    Imported batteryImage

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import batteryImage from "../../../assets/images/data/product-card/battery.png"
 
 function ProductCardWithDiscount() {
   return <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col">
