c61035bfa7ef214e7c41cc959afcd176f1b55ca8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 16:29:23 +0430

    Converting numbers to persian digits using separateNumberByThousand function

--- src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "../../style/productCard.module.css"
+import { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits";
 
 function ProductCardTimeCounter({number , text}) {
   return (
     <div className={`${styles.counter} flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30`}>
-      <span className="text-xs font-semibold text-Magenta">{number}</span>
+      <span className="text-xs font-semibold text-Magenta">{separateNumberByThousand(number)}</span>
       <span className="text-[0.5rem] text-NeutralN50 transition-all duration-300">{text}</span>
     </div>
   );
