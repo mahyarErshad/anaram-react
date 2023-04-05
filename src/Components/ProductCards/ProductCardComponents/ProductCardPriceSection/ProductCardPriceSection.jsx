b5d74ecc2ab97f5a45b75f2d49fd8ff09b5d061b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 11:11:15 +0430

    textStyles is conditional

--- src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ReactComponent as TomanIcon } from "../../../../assets/images/icons/toman-icon.svg";
 
-function ProductCardPriceSection({discountText}) {
-    const textStyles = discountText ? "text-[0.75rem]" : "text-NeutralN700 text-sm"
+function ProductCardPriceSection({ discountText }) {
+  const textStyles = discountText ? "text-NeutralN90  text-[0.75rem]" : "text-NeutralN700 text-sm";
   return (
-    <span className="flex-center gap-[0.125rem] font-semibold">
+    <span className={`${textStyles} flex-center gap-[0.125rem] font-semibold`}>
       ۱۰۲,۰۰۰
       <TomanIcon className="w-[0.9375rem] h-[0.6875rem] -translate-y-[0.125rem]" />
     </span>
