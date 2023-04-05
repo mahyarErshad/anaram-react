e4215aa71b9e426802e6a8bca5afbca311f08402
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 10:17:02 +0430

    Figcaption styled

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -9,7 +9,12 @@ function ProductCardWithDiscount() {
       <div className="flex-center h-2/4 mb-4 relative">
         <img className="object-contain" src={batteryImage} alt="battery" />
         <span className={`${styles["discount-percentage"]} text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal`}>۵۰٪</span>
+        <div className="absolute -top-1 -left-1 flex-center gap-[0.125rem]">
+          <span className="font-semibold text-xs text-primary6 translate-y-[0.0625rem]">۴.۵</span>
+          <img src={starIcon} alt="star icon" />
+        </div>
       </div>
+      <figcaption className="text-xs font-semibold max-w-[11rem] text-center leading-[1.125rem]">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</figcaption>
     </figure>
   );
 }
