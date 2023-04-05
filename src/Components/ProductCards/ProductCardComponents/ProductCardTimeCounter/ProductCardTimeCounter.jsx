21d481be95f2d2e7b0dc0b858f84f80c14e03167
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 15:30:14 +0430

    number and text are this component's props

--- src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function ProductCardTimeCounter() {
+function ProductCardTimeCounter({number , text}) {
   return (
     <div className="flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30">
-      <span className="text-xs font-semibold text-Magenta">۲۴</span>
-      <span className="text-[0.5rem] text-NeutralN50">روز</span>
+      <span className="text-xs font-semibold text-Magenta">{number}</span>
+      <span className="text-[0.5rem] text-NeutralN50">{text}</span>
     </div>
   );
 }
