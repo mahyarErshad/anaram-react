c4042e9e2dbed3cda0bda0f8d5170a1f591422e5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 09:27:04 +0430

    Add a battery image for product card

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 function ProductCardWithDiscount() {
-  return <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30">
+  return <figure className=" pt-3 pb-[0.875rem] px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col">
+    <div className="flex-center h-2/4">
     
+    </div>
   </figure>;
 }
 
