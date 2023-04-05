9d1f6eb13b5663de93b9aa116f5f14da71cad849
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 16:02:22 +0430

    Product card is done

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -14,7 +14,7 @@ import ProductCardTimeCounter from "../ProductCardComponents/ProductCardTimeCoun
 
 function ProductCardWithDiscount() {
   return (
-    <figure className={`${styles.card} pt-3 pb-[0.875rem] relative px-3 rounded-t-lg bg-white border border-NeutralN30 flex flex-col cursor-pointer transition-all duration-300`}>
+    <figure className={`${styles.card} pt-3 pb-[0.875rem] relative px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col cursor-pointer transition-all duration-300`}>
       <div className="flex-center h-2/4 mb-4 relative">
         <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
         <ProductCardDiscountSection percentage="۵۰" />
