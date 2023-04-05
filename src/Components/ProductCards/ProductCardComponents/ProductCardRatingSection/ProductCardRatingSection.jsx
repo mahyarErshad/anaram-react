46f1b6d0601e9dc68fa0cd5e9340d0d477063954
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 13:03:05 +0430

    rating can be passed via props

--- src/Components/ProductCards/ProductCardComponents/ProductCardRatingSection/ProductCardRatingSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardRatingSection/ProductCardRatingSection.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { ReactComponent as StarIcon } from "../../../../assets/images/icons/product-card-star.svg";
 
-function ProductCardRatingSection() {
+function ProductCardRatingSection({rating}) {
   return (
     <div className="absolute -top-1 -left-1 flex-center gap-[0.125rem]">
-    <span className="font-semibold text-xs text-primary6 translate-y-[0.0625rem]">۴.۵</span>
+    <span className="font-semibold text-xs text-primary6 translate-y-[0.0625rem]">{rating}</span>
     <StarIcon />
   </div>
   )
