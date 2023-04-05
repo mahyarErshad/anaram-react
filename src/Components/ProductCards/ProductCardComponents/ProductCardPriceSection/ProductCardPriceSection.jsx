8ad3cbb02b68b675f3fe614a35ac5fd47cc63285
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 11:09:02 +0430

    Defined textStyles

--- src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { ReactComponent as TomanIcon } from "../../../../assets/images/icons/toman-icon.svg";
 
-function ProductCardPriceSection() {
+function ProductCardPriceSection({discountText}) {
+    const textStyles = discountText ? "text-[0.75rem]" : "text-NeutralN700 text-sm"
   return (
-    <span className="flex-center gap-[0.125rem] text-NeutralN700 text-sm font-semibold">
+    <span className="flex-center gap-[0.125rem] font-semibold">
       ۱۰۲,۰۰۰
       <TomanIcon className="w-[0.9375rem] h-[0.6875rem] -translate-y-[0.125rem]" />
     </span>
