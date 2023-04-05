e1611db74c89ab9c8497aa705158a831302eeb35
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 10:23:25 +0430

    Added toman icon

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -15,6 +15,10 @@ function ProductCardWithDiscount() {
         </div>
       </div>
       <figcaption className="text-xs font-semibold max-w-[11rem] leading-[1.125rem] text-NeutralN600 mb-4">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</figcaption>
+      <div className="flex justify-between items-center w-full">
+      <span></span>
+      <span></span>
+      </div>
     </figure>
   );
 }
