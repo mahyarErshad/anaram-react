e86656777471245e8d6d325c0a31d00aabb54063
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 12:01:46 +0430

    Primary Button for product card has been created

--- src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
+++ src/Components/ProductCards/ProductCardWithDiscount/ProductCardWithDiscount.jsx
@@ -23,7 +23,7 @@ function ProductCardWithDiscount() {
         <ProductCardPriceSection price="۱۰۲,۰۰۰" />
       </div>
       <div className="flex-center gap-1 w-full">
-        <PrimaryButton addToCartButton text="افزودن به سبد" icon={<CartIcon stroke="white" />} />
+        <PrimaryButton addToCartButton text="افزودن به سبد" icon={<CartIcon className="w-4 h-4" stroke="white" />} />
       </div>
     </figure>
   );
--- src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
+++ src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./styles.module.css";
 
 function PrimaryButton(props) {
-  const spaces = props.addToCartButton ? "py-[0.5625rem] px-[0.6875rem] gap-[0.625rem]" : "gap-3 px-4 py-3";
+  const spaces = props.addToCartButton ? "py-2 px-2 gap-[0.625rem] text-[0.625rem]" : "gap-3 px-4 py-3 text-sm";
   return (
-    <button onClick={props.onClick} className={`${props.className} ${styles.button} ${spaces} transition-all duration-300 rounded-5px bg-primary6 flex-center text-white text-sm`}>
+    <button onClick={props.onClick} className={`${props.className} ${styles.button} ${spaces} transition-all duration-300 rounded-5px bg-primary6 flex-center text-white`}>
       {props.icon}
       {props.text}
     </button>
