3079412714448fbe3b9d3cfd5b54c8e9e7ddc62b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 15:29:10 +0430

    Copied ProductCardTimeCounter

--- src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function ProductCardTimeCounter() {
   return (
-    <div>ProductCardTimeCounter</div>
-  )
+    <div className="flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30">
+      <span className="text-xs font-semibold text-Magenta">۲۴</span>
+      <span className="text-[0.5rem] text-NeutralN50">روز</span>
+    </div>
+  );
 }
 
-export default ProductCardTimeCounter
+export default ProductCardTimeCounter;
