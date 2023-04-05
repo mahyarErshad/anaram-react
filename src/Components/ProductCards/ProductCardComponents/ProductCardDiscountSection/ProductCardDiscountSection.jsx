a257bf07a68b2cf49c81d75a5bf8b2ce7bdc7dbd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 16:25:30 +0430

    Function converting number to persian string

--- src/Components/ProductCards/ProductCardComponents/ProductCardDiscountSection/ProductCardDiscountSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardDiscountSection/ProductCardDiscountSection.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "../../style/productCard.module.css";
-import  { convertToPersian } from "../../../../lib/function/convertToPersianDigits"
+import  { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits"
 
 function ProductCardDiscountSection({ percentage }) {
-  return <span className={`${styles["discount-percentage"]} text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal`}>{convertToPersian(percentage)}٪</span>;
+  return <span className={`${styles["discount-percentage"]} text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal`}>{separateNumberByThousand(percentage)}٪</span>;
 }
 
 export default ProductCardDiscountSection;
