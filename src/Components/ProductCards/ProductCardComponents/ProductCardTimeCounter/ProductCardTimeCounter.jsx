61458183cf249fb77e4484ec502a6c5f2d8228ed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 15:36:12 +0430

    Given counter class to this

--- src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
@@ -3,7 +3,7 @@ import styles from "../../style/productCard.module.css"
 
 function ProductCardTimeCounter({number , text}) {
   return (
-    <div className="flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30">
+    <div className={`${styles.counter} flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30`}>
       <span className="text-xs font-semibold text-Magenta">{number}</span>
       <span className="text-[0.5rem] text-NeutralN50">{text}</span>
     </div>
