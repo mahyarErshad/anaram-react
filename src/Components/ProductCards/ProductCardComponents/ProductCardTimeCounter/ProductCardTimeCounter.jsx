d205ef3dab1d999e81faa801237406534b46b0fc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-05 16:34:12 +0430

    Using padstart for single numbers

--- src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
@@ -5,7 +5,7 @@ import { separateNumberByThousand } from "../../../../lib/function/convertToPers
 function ProductCardTimeCounter({number , text}) {
   return (
     <div className={`${styles.counter} flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30`}>
-      <span className="text-xs font-semibold text-Magenta">{separateNumberByThousand(number)}</span>
+      <span className="text-xs font-semibold text-Magenta">{separateNumberByThousand(number).padStart(2, '0')}</span>
       <span className="text-[0.5rem] text-NeutralN50 transition-all duration-300">{text}</span>
     </div>
   );
