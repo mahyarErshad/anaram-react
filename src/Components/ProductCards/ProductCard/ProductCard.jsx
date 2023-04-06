64673b104aff2bbcf6fdfa6a0faeb2e93b0c67cc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-06 13:27:48 +0430

    discountCard shows the carousel which card to render

--- src/Components/ProductCards/ProductCard/ProductCard.jsx
+++ src/Components/ProductCards/ProductCard/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../style/productCard.module.css"
+import styles from "../style/productCard.module.css";
 import batteryImage from "../../../assets/images/data/product-card/battery.png";
 import ProductCardPriceSection from "../ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection";
 import PrimaryButton from "../../Utils/Buttons/PrimaryButton/PrimaryButton";
@@ -12,30 +12,32 @@ import ProductCardDiscountSection from "../ProductCardComponents/ProductCardDisc
 import ProductCardTitleSection from "../ProductCardComponents/ProductCardTitleSection/ProductCardTitleSection";
 import ProductCardTimeCounter from "../ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter";
 
-function ProductCard() {
+function ProductCard(props) {
   return (
     <figure className={`${styles.card} pt-3 pb-[0.875rem] relative px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col cursor-pointer transition-all duration-300`}>
       <div className="flex-center h-2/4 mb-4 relative">
         <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
-        <ProductCardDiscountSection percentage={50} />
+        {props.discountCard && <ProductCardDiscountSection percentage={50} />}
         <ProductCardRatingSection rating={4.5} />
       </div>
       <ProductCardTitleSection title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" />
       <div className="flex justify-between items-center w-full mb-3">
         <ProductCardPriceSection price={200000} discountText />
         <ProductCardPriceSection price={100000} />
-      </div>     
+      </div>
       <div className="flex-center gap-1 w-full mb-3">
         <PrimaryButton addToCartButton text="افزودن به سبد" icon={<CartIcon className="w-4 h-4" stroke="white" />} />
         <SmallButton className="p-2 bg-NeutralN20" icon={<HeartIcon />} />
         <SmallButton className="p-2 bg-NeutralN20" icon={<CompareArrowsIcon />} />
       </div>
-      <div className="flex-center gap-0 bg-NeutralN20 rounded-[4px] py-1 mb-1">
-      <ProductCardTimeCounter number={6} text="ثانیه" />
-      <ProductCardTimeCounter number={24} text="دقیقه" />
-      <ProductCardTimeCounter number={12} text="ساعت" />
-      <ProductCardTimeCounter number={2} text="روز" />
-      </div>
+      {props.discountCard && (
+        <div className="flex-center gap-0 bg-NeutralN20 rounded-[4px] py-1 mb-1">
+          <ProductCardTimeCounter number={6} text="ثانیه" />
+          <ProductCardTimeCounter number={24} text="دقیقه" />
+          <ProductCardTimeCounter number={12} text="ساعت" />
+          <ProductCardTimeCounter number={2} text="روز" />
+        </div>
+      )}
     </figure>
   );
 }
