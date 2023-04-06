e2a08574f7d8a6183b3bee4b84348061a86c32af
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-06 13:46:52 +0430

    Text align is right

--- src/Components/ProductCards/ProductCardComponents/ProductCardTitleSection/ProductCardTitleSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTitleSection/ProductCardTitleSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ProductCardTitleSection({ title }) {
-  return <figcaption className="text-xs font-semibold max-w-[11rem] leading-[1.125rem] text-NeutralN600 mb-4">{title}</figcaption>;
+  return <figcaption className="text-xs font-semibold max-w-[11rem] leading-[1.125rem] text-NeutralN600 mb-4 text-right">{title}</figcaption>;
 }
 
 export default ProductCardTitleSection;
