7f2a3dee2326bf6f0baf9faffcfdf5c4d1ff9cd7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 13:52:52 +0430

    Images are placed

--- src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
+++ src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import banner1 from "../../../assets/images/data/small-banners/banner1.jpg"
-import banner2 from "../../../assets/images/data/small-banners/banner2.jpg"
+import React from "react";
+import banner1 from "../../../assets/images/data/small-banners/banner1.jpg";
+import banner2 from "../../../assets/images/data/small-banners/banner2.jpg";
 
 function MainPageSmallBanners() {
   return (
-    <section className='container bg-transparent flex-center gap-6'>
-    <img className='rounded-2xl' src={banner1} alt='Banner for advertising batteries for customers' />
-    <img className='rounded-2xl' src={banner2} alt='Banner for advertising batteries for customers' />
+    <section className="w-full max-w-[82.5rem] mx-auto mt-[6.5rem] bg-transparent flex-center gap-6">
+      <img className="rounded-2xl lg:max-w-[40.5rem]" src={banner1} alt="Banner for advertising batteries for customers" />
+      <img className="rounded-2xl lg:max-w-[40.5rem]" src={banner2} alt="Banner for advertising batteries for customers" />
     </section>
-  )
+  );
 }
 
-export default MainPageSmallBanners
+export default MainPageSmallBanners;
