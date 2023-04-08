d4654b10e28965a5932bd4e1d56ceadcfe22c820
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 11:16:59 +0430

    Number styled

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -10,7 +10,7 @@ function AnaramAssistant() {
       <p className="max-w-[71.75rem] text-base text-NeutralN300">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد</p>
       <div className="flex-center gap-6 mt-[2.5rem]">
         <div className="bg-gradient-to-b from-[#5F34CC] to-[#22134A] rounded-2xl py-3 px-6 z-20 flex flex-col gap-4">
-        <span>1</span>
+        <span className="poppins text-2xl font-light text-white opacity-50">1</span>
         </div>
       </div>
     </section>
