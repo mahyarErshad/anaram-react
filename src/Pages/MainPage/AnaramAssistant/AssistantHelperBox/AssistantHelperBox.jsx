cca147b08209d0acad1799dd3203b40199333e4f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 12:07:10 +0430

    Copied the lines

--- src/Pages/MainPage/AnaramAssistant/AssistantHelperBox/AssistantHelperBox.jsx
+++ src/Pages/MainPage/AnaramAssistant/AssistantHelperBox/AssistantHelperBox.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function AssistantHelperBox() {
   return (
-    <div>AssistantHelperBox</div>
-  )
+    <div className={`bg-gradient-to-b from-[#5F34CC] to-[#22134A] rounded-2xl py-3 px-6 z-10 flex justify-start items-center flex-col gap-2 min-h-[11.4375rem] relative ${styles["background-shape"]}`}>
+      <span className="poppins text-2xl font-light text-white opacity-50">1</span>
+      <span className="text-2xl font-semibold text-white ">انتخاب دستگاه</span>
+      <p className="text-[9px] text-NeutralN30 max-w-[8.9375rem]">دستگاهی که میخواهی برایش باتری بخری را انتخاب کن</p>
+    </div>
+  );
 }
 
-export default AssistantHelperBox
+export default AssistantHelperBox;
