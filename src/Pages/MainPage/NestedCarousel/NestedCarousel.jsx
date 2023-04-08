ae1e9e9cf28cdb7bb6dfa5c7bb81bf6ad1bfb830
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 16:03:02 +0430

    Single Carousel is added

--- src/Pages/MainPage/NestedCarousel/NestedCarousel.jsx
+++ src/Pages/MainPage/NestedCarousel/NestedCarousel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { ReactComponent as ButtonIcon } from "../../assets/images/icons/carousel-button-arrow.svg";
-import ProductCard from "../ProductCards/ProductCard/ProductCard";
+import { ReactComponent as ButtonIcon } from "../../../assets/images/icons/carousel-button-arrow.svg";
 
 // Import Swiper styles
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,20 +8,20 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/free-mode";
-import "./carousel.css";
+import "../../../Components/Carousel/carousel.css";
 
 // import required modules
 import { Navigation, FreeMode } from "swiper";
+import ProductCard from "../../../Components/ProductCards/ProductCard/ProductCard";
 
 function NestedCarousel() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <section className="container mt-[5.625rem] gap-7">
       <div className="flex w-full justify-between items-center">
-        <div className="flex-center gap-2">
-          {props.icon}
+        <div className="flex-center">
           <h3 className="text-xl text-NeutralN500 font-bold">
-            {props.title} <strong className="text-primary6">آنارام</strong>
+            ویترین <strong className="text-primary6">آنارام</strong>
           </h3>
         </div>
         <div className="flex-center gap-2">
@@ -48,7 +47,7 @@ function NestedCarousel() {
         {arr.map((item, index) => {
           return (
             <SwiperSlide key={index}>
-              <ProductCard discountCard={props.discountCard} />
+              <ProductCard />
             </SwiperSlide>
           );
         })}
