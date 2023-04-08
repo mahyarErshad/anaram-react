a65b57e4326f5f2f0fc4f2117d3d6b7c49d7b1fd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 11:04:03 +0430

    Added p tag

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -4,9 +4,10 @@ import styles from "../styles/style.module.css";
 function AnaramAssistant() {
   return (
     <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-2xl relative py-12`}>
-        <span className="font-bold text-[1.75rem] text-NeutralN500 bg-background absolute top-0 lg:right-12 -translate-y-[50%] px-10 py-4 rounded-b-2xl">
-          دستیار <strong className="text-primary6">آنارام</strong>
-        </span>
+      <span className="font-bold text-[1.75rem] text-NeutralN500 bg-background absolute top-0 lg:right-12 -translate-y-[50%] px-10 py-4 rounded-b-2xl">
+        دستیار <strong className="text-primary6">آنارام</strong>
+      </span>
+      <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد</p>
     </section>
   );
 }
