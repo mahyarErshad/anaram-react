8cae404e7636f5d35e09b576f41ccc376a6e49fc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 10:50:02 +0430

    Remove anaram assistant svg

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -3,13 +3,11 @@ import styles from "../styles/style.module.css";
 
 function AnaramAssistant() {
   return (
-    <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-b-2xl relative`}>
-      <div className="flex gap-0 absolute top-0 right-0">
-        <span className="lg:w-12 rounded-t-2xl"></span>
-        <span className="font-bold text-[1.75rem] text-NeutralN500 lg:right-12 -translate-y-[50%] rounded-b-2xl">
+    <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-2xl relative`}>
+      <div className="flex gap-0 absolute top-0 lg:right-12">
+        <span className="font-bold text-[1.75rem] text-NeutralN500 bg-background lg:right-12 -translate-y-[50%] px-10 py-4 rounded-b-2xl flex-shrink-0">
           دستیار <strong className="text-primary6">آنارام</strong>
         </span>
-        <span></span>
       </div>
     </section>
   );
