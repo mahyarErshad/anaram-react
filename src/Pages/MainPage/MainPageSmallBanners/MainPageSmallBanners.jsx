382e08992c7a4753008849996fca33e7d91438d8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 13:46:37 +0430

    Imported 2 banners

--- src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
+++ src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import banner1 from "../../../assets/images/data/small-banners/banner1.jpg"
+import banner2 from "../../../assets/images/data/small-banners/banner2.jpg"
 
 function MainPageSmallBanners() {
   return (
     <section className='container bg-transparent flex-center gap-6'>
-        
+
     </section>
   )
 }
