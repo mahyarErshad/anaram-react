744bdb5547c59e3401971abcbab914b601bab8e1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 11:03:06 +0430

    Title has been added

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -3,9 +3,9 @@ import styles from "../styles/style.module.css";
 
 function AnaramAssistant() {
   return (
-    <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-2xl relative`}>
+    <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-2xl relative py-12`}>
       <div className="flex gap-0 absolute top-0 lg:right-12">
-        <span className="font-bold text-[1.75rem] text-NeutralN500 bg-background lg:right-12 -translate-y-[50%] px-10 py-4 rounded-b-2xl flex-shrink-0">
+        <span className="font-bold text-[1.75rem] text-NeutralN500 bg-background lg:right-12 -translate-y-[50%] px-10 py-4 rounded-b-2xl">
           دستیار <strong className="text-primary6">آنارام</strong>
         </span>
       </div>
