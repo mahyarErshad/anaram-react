86aaca105dc0f918b8c3190b722be0fc3a0f9856
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 12:38:06 +0430

    After is placed

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -5,7 +5,7 @@ import PrimaryButton from "../../../../Components/Utils/Buttons/PrimaryButton/Pr
 
 function AnaramAssistant() {
   return (
-    <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-2xl relative py-12`}>
+    <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-2xl relative py-12 after:bg-primary6`}>
       <h2 className="font-bold text-[1.75rem] text-NeutralN500 bg-background absolute top-0 lg:right-12 -translate-y-[50%] px-10 py-4 rounded-b-2xl">
         دستیار <strong className="text-primary6">آنارام</strong>
       </h2>
