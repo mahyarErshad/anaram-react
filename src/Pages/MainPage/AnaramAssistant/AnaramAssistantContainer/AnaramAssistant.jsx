7ed16d82424f8fcdd21debb1c6bab286521ef07e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 09:21:52 +0430

    Added a span

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import styles from "../styles/style.module.css"
+import React from "react";
+import styles from "../styles/style.module.css";
 
 function AnaramAssistant() {
   return (
     <section className={`container ${styles.container} lg:min-h-[36.0625rem] py-4 mt-28 bg-white rounded-2xl`}>
-
+      <span>
+        دستیار <strong>آنارام</strong>
+      </span>
     </section>
-  )
+  );
 }
 
-export default AnaramAssistant
+export default AnaramAssistant;
