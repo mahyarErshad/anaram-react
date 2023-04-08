39c28af0dd66f6dd021d5e8ef8fe381dd95b4254
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 13:45:44 +0430

    Added 2 banner images

--- src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
+++ src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MainPageSmallBanners() {
   return (
-    <section className='container bg-transparent flex-center gap-6'>MainPageSmallBanners</section>
+    <section className='container bg-transparent flex-center gap-6'>
+        
+    </section>
   )
 }
 
