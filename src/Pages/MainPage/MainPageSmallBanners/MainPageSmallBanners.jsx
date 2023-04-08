3e06ea3344413aad74dbde1cf18b88f775f73b2d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 13:57:12 +0430

    Banners section is completed

--- src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
+++ src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
@@ -3,10 +3,11 @@ import banner1 from "../../../assets/images/data/small-banners/banner1.jpg";
 import banner2 from "../../../assets/images/data/small-banners/banner2.jpg";
 
 function MainPageSmallBanners() {
+  const imageClassName = "rounded-2xl lg:max-w-[40.5rem] cursor-pointer";
   return (
     <section className="w-full max-w-[82.5rem] mx-auto mt-[6.5rem] bg-transparent flex-center gap-6">
-      <img className="rounded-2xl lg:max-w-[40.5rem]" src={banner1} alt="Banner for advertising batteries for customers" />
-      <img className="rounded-2xl lg:max-w-[40.5rem]" src={banner2} alt="Banner for advertising batteries for customers" />
+      <img className={imageClassName} src={banner1} alt="Banner for advertising batteries for customers" />
+      <img className={imageClassName} src={banner2} alt="Banner for advertising batteries for customers" />
     </section>
   );
 }
