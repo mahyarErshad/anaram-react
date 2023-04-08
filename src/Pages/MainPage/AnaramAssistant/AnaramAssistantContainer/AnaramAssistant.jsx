fcd2af283291a989a4992718a0977839cdbe8511
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 09:14:41 +0430

    Styled the section

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AnaramAssistant() {
   return (
-    <section>AnaramAssistant</section>
+    <section className='container mt-28 bg-white rounded-2xl'>AnaramAssistant</section>
   )
 }
 
