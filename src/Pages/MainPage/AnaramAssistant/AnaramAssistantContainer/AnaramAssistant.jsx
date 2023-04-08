22a8f62d93b88120efde18f9eaa807d77d91ad23
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-08 10:31:26 +0430

    Background image is a variable

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -3,10 +3,14 @@ import styles from "../styles/style.module.css";
 
 function AnaramAssistant() {
   return (
-    <section className={`container ${styles.container} lg:min-h-[36.0625rem] py-4 mt-28 bg-white rounded-2xl`}>
-      <span className="font-bold text-[1.75rem] text-NeutralN500">
-        دستیار <strong className="text-primary6">آنارام</strong>
-      </span>
+    <section className={`container ${styles.container} lg:min-h-[36.0625rem] mt-28 bg-white rounded-b-2xl relative`}>
+      <div className="flex gap-0 absolute top-0 right-0">
+        <span className="lg:w-12 rounded-t-2xl"></span>
+        <span className="font-bold text-[1.75rem] text-NeutralN500 lg:right-12 -translate-y-[50%] rounded-b-2xl">
+          دستیار <strong className="text-primary6">آنارام</strong>
+        </span>
+        <span></span>
+      </div>
     </section>
   );
 }
