6b5b73234e57dcd79da93c1199c6fbfb3616eae7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:32:27 +0430

    Image is styled

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -4,7 +4,7 @@ import image from "../../../assets/images/data/blog-cards/battery.jpg";
 function BlogCards() {
   return (
     <figure className="rounded-2xl p-[1.125rem] bg-white border border-primary1">
-      <img src={image} alt="" />
+      <img className="h-2/4 rounded-lg" src={image} alt="" />
     </figure>
   );
 }
