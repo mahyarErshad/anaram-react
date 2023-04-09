fc3b631938be960dbdcc2961d76e145ca4f291ae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 12:42:13 +0430

    Added icons of MainPageFeatures section

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MainPageFeatures() {
   return (
     <section className='container bg-white rounded-[0.625rem] py-2 px-3 flex-center gap-0 mt-[5.625rem]'>
-
+    
     </section>
   )
 }
