af8ee4b818385b5805add71008a25ed8e866fed7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 16:21:58 +0430

    FooterNavLinks are dynamic

--- src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
+++ src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function FooterNavLinks() {
+function FooterNavLinks({ data }) {
   return (
-    <div>FooterNavLinks</div>
-  )
+    <div className="flex flex-col gap-4">
+      <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold">{data.title}</span>
+      <ul className="flex flex-col justify-start items-start gap-3">
+        {data.navLinks.map((item, index) => {
+          return (
+            <li key={index} className="text-sm text-NeutralN200 font-semibold">
+              {item.text}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default FooterNavLinks
+export default FooterNavLinks;
