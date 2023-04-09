fca30b3d19fa01417fae57a411c7f7701fc707f5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:29:13 +0430

    img tag is added

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import image from "../../../assets/images/data/blog-cards/battery.jpg"
+import React from "react";
+import image from "../../../assets/images/data/blog-cards/battery.jpg";
 
 function BlogCards() {
   return (
-    <figure className='rounded-2xl p-[1.125rem] bg-white border border-primary1'>
-
+    <figure className="rounded-2xl p-[1.125rem] bg-white border border-primary1">
+      <img src={image} alt="" />
     </figure>
-  )
+  );
 }
 
-export default BlogCards
+export default BlogCards;
