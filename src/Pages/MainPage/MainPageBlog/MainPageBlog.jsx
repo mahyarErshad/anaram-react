09565a837d42d7e9144f9084203841ec8a4d83cb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:18:57 +0430

    h2 added

--- src/Pages/MainPage/MainPageBlog/MainPageBlog.jsx
+++ src/Pages/MainPage/MainPageBlog/MainPageBlog.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function MainPageBlog() {
   return (
-    <section className='container flex-center mt-[5.625rem] gap-6'>
-
+    <section className="container flex-center mt-[5.625rem] gap-6">
+      <h2 className="font-bold text-xl text-NeutralN500">
+        مجله <strong className="text-primary6">آنارام</strong>
+      </h2>
     </section>
-  )
+  );
 }
 
-export default MainPageBlog
+export default MainPageBlog;
