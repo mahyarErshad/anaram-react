1a536467f0b6cdc9ba43362ce2a4f16151fe61db
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 12:46:25 +0430

    Second Card added

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
@@ -9,6 +9,7 @@ function MainPageFeatures() {
   return (
     <section className="container bg-white rounded-[0.625rem] py-2 px-3 flex-center gap-0 mt-[5.625rem]">
       <MainPageFeatureCards icon={<MedalIcon />} text="ضمانت اصلات کالا" />
+      <MainPageFeatureCards icon={<TruckIcon />} text="ارسال به سراسر کشور" />
     </section>
   );
 }
