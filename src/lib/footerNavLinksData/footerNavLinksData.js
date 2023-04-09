77bd27f6e79542b156818d500abb41bf568d0350
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 16:34:58 +0430

    customerServicesData is created

--- src/lib/footerNavLinksData/footerNavLinksData.js
+++ src/lib/footerNavLinksData/footerNavLinksData.js
@@ -4,3 +4,9 @@ export const helpShopingData = [
     navLinks: ["شیوه های پرداخت", "نحوه ثبت سفارش", "پیگیری سفارش", "رویه ارسال"],
   },
 ];
+export const customerServicesData = [
+  {
+    title: "خدمات مشتریان",
+    navLinks: ["پرسش های متداول", "حریم خصوصی", "محصولات"],
+  },
+];
