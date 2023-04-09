194cdec313e47bae0b811e11ddf06b74a8543175
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 15:47:12 +0430

    Created a component for footer address

---
+++ src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FooterAddress() {
+  return (
+    <div>FooterAddress</div>
+  )
+}
+
+export default FooterAddress
--- src/Components/Footer/FooterContainer/FooterContainer.jsx
+++ src/Components/Footer/FooterContainer/FooterContainer.jsx
@@ -8,7 +8,9 @@ function FooterContainer() {
       <section className={`${styles.footer} w-full max-w-[101.875rem] mx-auto`}>
         <Logo className={styles.logo} />
         <p className={`${styles.description} text-sm text-NeutralN90`}>فروشگاه آنارام نماینده فروش محصولات برند وارتا (Varta) آلمان (دارای ۱۳۰ سال سابقه در تولید انواع باتری) است که فعالیت خود را در سال 1400 آغاز نمود. آنارام دارای نمایندگی باتری وارتا و دیگر محصولات این شرکت است و در این فروشگاه انواع باتری قلمی و نیم قلمی ، باتری شارژی ، باتری سکه ای ، باتری مخصوص مانند باتری دوربین و تلفن و دیگر محصولات برند وارتا از جمله شارژر باتری و چراغ قوه عرضه می گردد.</p>
-        <address></address>
+        <address>
+
+        </address>
       </section>
     </footer>
   );
