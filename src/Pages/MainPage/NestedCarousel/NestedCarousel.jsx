d6ee12d7858d54974a8b8fca021ffc2c8b733f5f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 09:51:44 +0430

    Uninstalled react-tabs

--- src/Pages/MainPage/NestedCarousel/NestedCarousel.jsx
+++ src/Pages/MainPage/NestedCarousel/NestedCarousel.jsx
@@ -10,30 +10,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/free-mode";
 import "../../../Components/Carousel/carousel.css";
-import styles from "./styles.module.css"
 
 // import required modules
 import { Navigation, FreeMode } from "swiper";
 import ProductCard from "../../../Components/ProductCards/ProductCard/ProductCard";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 function NestedCarousel() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   const nextButton = randomizeClassName();
   const prevButton = randomizeClassName();
   return (
-    <Tabs className="container mt-[5.625rem] gap-7">
-      <div className="flex justify-between items-center w-full">
-        <div className="flex-center gap-12 mr-2">
+    <section className="container mt-[5.625rem] gap-7">
+      <div className="flex w-full justify-between items-center">
+        <div className="flex-center">
           <h3 className="text-xl text-NeutralN500 font-bold">
             ویترین <strong className="text-primary6">آنارام</strong>
           </h3>
-          <div>
-            <TabList>
-              <Tab>Title 1</Tab>
-              <Tab>Title 2</Tab>
-            </TabList>
-          </div>
         </div>
         <div className="flex-center gap-2">
           <button className={`carousel-button ${prevButton}`}>
@@ -44,31 +36,26 @@ function NestedCarousel() {
           </button>
         </div>
       </div>
-      <TabPanel>
-        <Swiper
-          navigation={{
-            nextEl: `.${nextButton}`,
-            prevEl: `.${prevButton}`,
-          }}
-          slidesPerView={"auto"}
-          spaceBetween={16}
-          modules={[Navigation, FreeMode]}
-          freeMode={true}
-          className="mySwiper"
-        >
-          {arr.map((item, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <ProductCard />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </TabPanel>
-      <TabPanel>
-        <h2>Any content 2</h2>
-      </TabPanel>
-    </Tabs>
+      <Swiper
+        navigation={{
+          nextEl: `.${nextButton}`,
+          prevEl: `.${prevButton}`,
+        }}
+        slidesPerView={"auto"}
+        spaceBetween={16}
+        modules={[Navigation, FreeMode]}
+        freeMode={true}
+        className="mySwiper"
+      >
+        {arr.map((item, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <ProductCard />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
   );
 }
 
