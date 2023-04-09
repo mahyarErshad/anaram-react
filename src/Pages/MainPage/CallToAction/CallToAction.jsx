637a1b605a8ff47a44019633e0985be89ba6b80e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 13:58:08 +0430

    Container has background image and color

--- src/Pages/MainPage/CallToAction/CallToAction.jsx
+++ src/Pages/MainPage/CallToAction/CallToAction.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.css"
 
 function CallToAction() {
-  return <section className="w-full h-[12.25rem] mt-16 flex-center flex-col gap-8 bg-primary6">
+  return <section className={`${styles.container} w-full h-[12.25rem] mt-16 flex-center flex-col gap-8`}>
 
   </section>;
 }
