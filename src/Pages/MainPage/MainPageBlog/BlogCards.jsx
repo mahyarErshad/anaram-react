0b562be228dfff30d13321ce444295f62950851f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:38:11 +0430

    Changed heading to figcaption

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -5,7 +5,7 @@ function BlogCards() {
   return (
     <figure className="rounded-2xl p-[1.125rem] bg-white border border-primary1">
       <img className="h-2/4 rounded-lg" src={image} alt="" />
-      <h3 className="text-right text-sm font-semibold text-NeutralN600 mt-4">علت شارژ نشدن باتری های موبایل</h3>
+      <figcaption className="text-right text-sm font-semibold text-NeutralN600 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
     </figure>
   );
 }
