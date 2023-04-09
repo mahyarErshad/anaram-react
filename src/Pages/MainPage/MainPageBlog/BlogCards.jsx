c6f48ca82d73ddc6ba62feabffe73e096382ce4a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:47:20 +0430

    P tag has N100 color

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -6,7 +6,7 @@ function BlogCards() {
     <figure className="rounded-2xl p-4 bg-white border border-primary1">
       <img className="h-2/4 rounded-lg" src={image} alt="" />
       <figcaption className="text-right text-sm font-semibold text-NeutralN600 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
-      <p className="text-[0.6875rem] text-right w-full leading-6 mt-4 max-w-[17.5rem]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
+      <p className="text-[0.6875rem] text-right w-full leading-6 mt-4 max-w-[17.5rem] text-NeutralN100">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
     </figure>
   );
 }
