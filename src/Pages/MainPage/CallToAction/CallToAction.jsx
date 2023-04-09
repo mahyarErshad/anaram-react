93008f73adf5afbb38d9fcb3c6bd5379587df0b5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 13:53:35 +0430

    Imported call-to-action-background image

--- src/Pages/MainPage/CallToAction/CallToAction.jsx
+++ src/Pages/MainPage/CallToAction/CallToAction.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CallToAction() {
-  return <section className="w-full h-[12.25rem] mt-16 flex-center flex-col gap-8">
+  return <section className="w-full h-[12.25rem] mt-16 flex-center flex-col gap-8 bg-primary6">
 
   </section>;
 }
