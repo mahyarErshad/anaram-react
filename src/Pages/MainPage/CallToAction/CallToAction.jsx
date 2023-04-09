6db0825516e36e13f943934bdf53435838aaa0e0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 14:00:10 +0430

    Heading has been added

--- src/Pages/MainPage/CallToAction/CallToAction.jsx
+++ src/Pages/MainPage/CallToAction/CallToAction.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
 function CallToAction() {
-  return <section className={`${styles.container} w-full h-[12.25rem] mt-16 flex-center flex-col gap-8`}>
-
-  </section>;
+  return (
+    <section className={`${styles.container} w-full h-[12.25rem] mt-16 flex-center flex-col gap-8`}>
+      <h4 className="text-white font-semibold text-lg">از تخفیف ها و جدیدترین های آنارام با خبر شوید.</h4>
+    </section>
+  );
 }
 
 export default CallToAction;
