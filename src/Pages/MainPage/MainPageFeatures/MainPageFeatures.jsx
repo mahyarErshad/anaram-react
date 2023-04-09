00414a8d043eaa98d85d43f498e98b04eaaa830e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 12:42:35 +0430

    Imported MainPageFeatureCards

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import MainPageFeatureCards from "./MainPageFeatureCards";
 
 function MainPageFeatures() {
   return (
-    <section className='container bg-white rounded-[0.625rem] py-2 px-3 flex-center gap-0 mt-[5.625rem]'>
-    
+    <section className="container bg-white rounded-[0.625rem] py-2 px-3 flex-center gap-0 mt-[5.625rem]">
+      <MainPageFeatureCards />
     </section>
-  )
+  );
 }
 
-export default MainPageFeatures
+export default MainPageFeatures;
