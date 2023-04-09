2ec04eba0c146da5fbbf21be278d7da2166f892a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:52:51 +0430

    Defined N80 color

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -7,6 +7,9 @@ function BlogCards() {
       <img className="h-2/4 rounded-lg" src={image} alt="" />
       <figcaption className="text-right text-sm font-semibold text-NeutralN600 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
       <p className="text-[0.6875rem] text-right w-full leading-6 mt-4 max-w-[17.5rem] text-NeutralN100">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
+      <div className="mt-8 w-full flex justify-between items-center">
+        <span className="text-[0.5625rem]">۱۴۰۱/۰۷/۱۹</span>
+      </div>
     </figure>
   );
 }
