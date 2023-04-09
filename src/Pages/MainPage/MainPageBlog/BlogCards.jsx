4d4d7451da2cf3670f124025242db966c04cbc49
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:45:53 +0430

    Fixed a bug

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -3,10 +3,10 @@ import image from "../../../assets/images/data/blog-cards/battery.jpg";
 
 function BlogCards() {
   return (
-    <figure className="rounded-2xl p-[1.125rem] bg-white border border-primary1">
+    <figure className="rounded-2xl p-4 bg-white border border-primary1">
       <img className="h-2/4 rounded-lg" src={image} alt="" />
       <figcaption className="text-right text-sm font-semibold text-NeutralN600 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
-      <p className="text-[0.6875rem] text-right w-full leading-6 mt-4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
+      <p className="text-[0.6875rem] text-right w-full leading-6 mt-4 max-w-[17.5rem]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
     </figure>
   );
 }
