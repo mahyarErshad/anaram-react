02ec93925cb6f140d6f05e50d5ac96f5e4b1bde9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 15:37:46 +0430

    Logo is placed

--- src/Components/Footer/FooterContainer/FooterContainer.jsx
+++ src/Components/Footer/FooterContainer/FooterContainer.jsx
@@ -6,7 +6,7 @@ function FooterContainer() {
   return (
     <footer className=" bg-NeutralN20 pt-10 pb-16 px-4">
       <section className={`${styles.footer} w-full max-w-[101.875rem] mx-auto`}>
-        <Logo />
+        <Logo className={styles.logo} />
       </section>
     </footer>
   );
