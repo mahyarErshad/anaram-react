f7d66a7a25b03c437c0524992758d9d01dd9ddf9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 11:34:26 +0430

    margin top of heading is 1 rem

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -5,7 +5,7 @@ function BlogCards() {
   return (
     <figure className="rounded-2xl p-[1.125rem] bg-white border border-primary1">
       <img className="h-2/4 rounded-lg" src={image} alt="" />
-      <h3 className="text-right text-sm text-NeutralN600">زمان شارژ نگه داشتن باتری ها را افزایش دهید</h3>
+      <h3 className="text-right text-sm text-NeutralN600 mt-4">زمان شارژ نگه داشتن باتری ها را افزایش دهید</h3>
     </figure>
   );
 }
