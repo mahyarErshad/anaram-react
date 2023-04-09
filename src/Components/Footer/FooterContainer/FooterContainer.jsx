ac86ecff7bff1f7cf7aa012a4849a1b2f4bcc602
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 15:45:00 +0430

    description is placed

--- src/Components/Footer/FooterContainer/FooterContainer.jsx
+++ src/Components/Footer/FooterContainer/FooterContainer.jsx
@@ -7,6 +7,7 @@ function FooterContainer() {
     <footer className=" bg-NeutralN20 pt-10 pb-16 px-4">
       <section className={`${styles.footer} w-full max-w-[101.875rem] mx-auto`}>
         <Logo className={styles.logo} />
+        <p className={`${styles.description} text-sm text-NeutralN90`}>فروشگاه آنارام نماینده فروش محصولات برند وارتا (Varta) آلمان (دارای ۱۳۰ سال سابقه در تولید انواع باتری) است که فعالیت خود را در سال 1400 آغاز نمود. آنارام دارای نمایندگی باتری وارتا و دیگر محصولات این شرکت است و در این فروشگاه انواع باتری قلمی و نیم قلمی ، باتری شارژی ، باتری سکه ای ، باتری مخصوص مانند باتری دوربین و تلفن و دیگر محصولات برند وارتا از جمله شارژر باتری و چراغ قوه عرضه می گردد.</p>
       </section>
     </footer>
   );
