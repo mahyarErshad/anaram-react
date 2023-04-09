4e0b72552452a7cf2ecbe96d72be6517501ba72a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 12:44:37 +0430

    Card is dynamically working

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
@@ -4,7 +4,7 @@ function MainPageFeatureCards(props) {
   return (
     <div className="flex justify-start items-center gap-6">
       <div className="flex-center bg-NeutralN10 rounded-[0.3125rem] p-4 w-[4.5rem] h-[4.5rem]">{props.icon}</div>
-      <span className="text-sm text-NeutralN500">{props.text}</span>
+      <span className="text-sm font-semibold text-NeutralN500">{props.text}</span>
     </div>
   );
 }
--- src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MainPageFeatureCards from "./MainPageFeatureCards";
+import { ReactComponent as MedalIcon } from "../../../assets/images/main-page-features/medal.svg";
 
 function MainPageFeatures() {
   return (
     <section className="container bg-white rounded-[0.625rem] py-2 px-3 flex-center gap-0 mt-[5.625rem]">
-      <MainPageFeatureCards />
+      <MainPageFeatureCards icon={<MedalIcon />} text="ضمانت اصلات کالا" />
     </section>
   );
 }
