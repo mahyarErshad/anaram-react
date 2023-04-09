d9ec11c7ee5776b1a1b997afb78cfc1dcd717964
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 15:56:49 +0430

    FooterAddresses are styled

--- src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
+++ src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FooterAddress(props) {
   return (
-    <div className="flex-center gap-2 not-italic">
+    <div className="flex justify-start items-center gap-2 not-italic">
       {props.icon}
       <p className="text-sm text-NeutralN90">{props.text}</p>
     </div>
