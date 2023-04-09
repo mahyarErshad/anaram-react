e70fe22cfe29e763e8ecf29dc8081da80f5af80b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 15:29:59 +0430

    Imported styles

--- src/Components/Footer/FooterContainer/FooterContainer.jsx
+++ src/Components/Footer/FooterContainer/FooterContainer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "../styles/styles.module.css"
 
 function FooterContainer() {
   return <footer className="bg-NeutralN20 pt-10 pb-16 px-4">
-    
+
   </footer>;
 }
 
