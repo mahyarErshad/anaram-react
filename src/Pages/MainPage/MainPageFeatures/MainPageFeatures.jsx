74c75b1e9cb514f9291e21a922afef7175989e3b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 12:35:11 +0430

    Added padding to the wrapper

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MainPageFeatures() {
   return (
-    <div>MainPageFeatures</div>
+    <section className='container bg-white rounded-[0.625rem] py-2 px-3 flex-center gap-0 mt-[5.625rem]'>
+
+    </section>
   )
 }
 
