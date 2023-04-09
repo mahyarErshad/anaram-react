adee5fd3777a07b00c5a252ff64cab53ae7a49e2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 12:40:13 +0430

    Text is added

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 function MainPageFeatureCards(props) {
   return (
-    <div className="flex-center gap-6">
-      <div className="flex-center bg-NeutralN10 rounded-[0.3125rem] p-4 w-[4.5rem] h-[4.5rem]">
-        {props.icon}
-      </div>
+    <div className="flex justify-start items-center gap-6">
+      <div className="flex-center bg-NeutralN10 rounded-[0.3125rem] p-4 w-[4.5rem] h-[4.5rem]">{props.icon}</div>
+      <span className="text-sm text-NeutralN500">{props.text}</span>
     </div>
   );
 }
