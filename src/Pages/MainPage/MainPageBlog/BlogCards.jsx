24098138f05929e83b8f9946540aa6b8a52be9c8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 12:20:49 +0430

    Imported styles

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { ReactComponent as Icon } from "../../../assets/images/icons/carousel-button-arrow.svg";
+import styles from "./style.module.css"
 import image from "../../../assets/images/data/blog-cards/battery.jpg";
 
 function BlogCards() {
   return (
-    <figure className="rounded-2xl p-4 bg-white border border-primary1 cursor-pointer">
+    <figure className={`rounded-2xl p-4 bg-white border border-primary1 cursor-pointer`}>
       <img className="h-2/4 rounded-lg" src={image} alt="" />
       <figcaption className="text-right text-sm font-semibold text-NeutralN600 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
       <p className="text-[0.6875rem] text-right w-full leading-6 mt-4 max-w-[17.5rem] text-NeutralN100">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
