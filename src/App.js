83d0b93471ce8229b5141c42365236f29f135091
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 15:21:59 +0430

    Imported FooterContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderWrapper from "./Components/Header/HeaderWrapper/HeaderWrapper";
 import MainPageWrapper from "./Pages/MainPage/MainPageWrapper/MainPageWrapper";
+import FooterContainer from "./Components/Footer/FooterContainer/FooterContainer";
 
 function App() {
   return (
     <>
       <HeaderWrapper />
       <MainPageWrapper />
+      <FooterContainer />
     </>
   );
 }
