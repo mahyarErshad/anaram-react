b69e48dbf8d2220acfc2657c4bd5171caac051f2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-09 15:36:25 +0430

    Logo has been added

--- src/Components/Footer/FooterContainer/FooterContainer.jsx
+++ src/Components/Footer/FooterContainer/FooterContainer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import styles from "../styles/styles.module.css"
+import styles from "../styles/styles.module.css";
+import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
 
 function FooterContainer() {
-  return <footer className="bg-NeutralN20 pt-10 pb-16 px-4">
-
-  </footer>;
+  return (
+    <footer className=" bg-NeutralN20 pt-10 pb-16 px-4">
+      <section className={`${styles.footer} w-full max-w-[101.875rem] mx-auto`}>
+        <Logo />
+      </section>
+    </footer>
+  );
 }
 
 export default FooterContainer;
