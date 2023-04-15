7b18d50cd35bd28b6cf1ac7fbe5d2ddb89fc3bcd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-15 12:27:51 +0430

    Search box and phone section are responsive

--- src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
+++ src/Components/Header/HeaderNavBars/HeaderNavBars.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavLinks from "./NavLinks";
 import { ReactComponent as PhoneIcon } from "../../../assets/images/icons/phone-icon.svg";
+import SearchBox from "../../Utils/SearchBox/SearchBox";
 
 function HeaderNavBars() {
   return (
-    <section className="flex items-center justify-between max-w-[101.5625rem] self-center w-full">
-      <nav>
-        <ul className="max-lg:hidden lg:visible flex justify-start">
+    <section className="flex items-center justify-between max-w-[101.5625rem] self-center w-full gap-2">
+      <SearchBox className="flex-grow lg:hidden max-lg:mb-4" />
+      <nav className="max-lg:hidden lg:visible">
+        <ul className="flex justify-start">
           <NavLinks hasIcon text="باتری بر اساس سایز " />
           <NavLinks hasIcon text="باتری بر اساس کاربرد " />
           <NavLinks hasIcon text="باتری بر اساس تکنولوژی " />
@@ -15,7 +17,7 @@ function HeaderNavBars() {
           <NavLinks textColor="text-primary6" text="وارتا" />
         </ul>
       </nav>
-      <a href="tel:02177989898" className="flex-center pb-7 gap-3 hover:text-primary6">
+      <a href="tel:02177989898" className="flex-center max-md:hidden md:mt-3 lg:mt-0 pb-7 gap-3 hover:text-primary6">
         <span className="text-sm font-semibold transition-all">۷۷۹۸۹۸۹۸ - ۰۲۱</span>
         <PhoneIcon />
       </a>
