36978848f50381e729e169fc410f9ac65861080b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-15 10:27:51 +0430

    Created module css file

--- src/Components/ProductCards/SideBySideProductCard/SideBySideProductCard.jsx
+++ src/Components/ProductCards/SideBySideProductCard/SideBySideProductCard.jsx
@@ -4,7 +4,7 @@ import toman from "../../../assets/images/icons/toman-icon-black.svg";
 
 function SideBySideProductCard(props) {
   return (
-    <figure className="flex py-5 pr-3 pl-6 border-y border-NeutralN20 w-[21.875rem]">
+    <figure className={`flex py-5 pr-3 pl-6 border-y border-NeutralN20 w-[21.875rem]`}>
       <img className="w-[6.6875rem]" src={props.image} alt={props.title} />
       <div className="flex flex-col gap-5">
         <figcaption className="text-xs leading-5 font-semibold">{props.title}</figcaption>
