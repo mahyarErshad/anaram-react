d6d2bc84e2cb545de609a6917648c6f7575dfdaf
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-15 11:45:33 +0430

    Passed open state to HeaderShoppingCartModal

--- src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
+++ src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
@@ -8,7 +8,7 @@ import SmallButton from "../../Utils/Buttons/SmallButton/SmallButton";
 import HeaderShoppingCartModal from "../HeaderShoppingCartModal/HeaderShoppingCartModal";
 
 function HeaderActionSection() {
-  const [openCart, setOpenCart] = useState(false)
+  const [openCart, setOpenCart] = useState(false);
   return (
     <div className="flex justify-between items-center w-full max-w-[101.5625rem] self-center">
       <div className="flex-center gap-6 w-2/4">
@@ -18,7 +18,7 @@ function HeaderActionSection() {
       <div className="flex-center gap-4 relative">
         <PrimaryButton text="ورود / ثبت نام" icon={<UserIcon />} />
         <SmallButton className="p-3 bg-NeutralN10" icon={<BagIcon stroke="#575757" />} />
-        <HeaderShoppingCartModal />
+        <HeaderShoppingCartModal open={openCart} setOpen={setOpenCart} />
       </div>
     </div>
   );
