5318ac426f39108d113b94cdf4010ebd9a21af06
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-15 11:44:41 +0430

    Defined open state for cart

--- src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
+++ src/Components/Header/HeaderActionSection/HeaderActionSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
 import PrimaryButton from "../../Utils/Buttons/PrimaryButton/PrimaryButton";
 import SearchBox from "../../Utils/SearchBox/SearchBox";
@@ -8,6 +8,7 @@ import SmallButton from "../../Utils/Buttons/SmallButton/SmallButton";
 import HeaderShoppingCartModal from "../HeaderShoppingCartModal/HeaderShoppingCartModal";
 
 function HeaderActionSection() {
+  const [openCart, setOpenCart] = useState(false)
   return (
     <div className="flex justify-between items-center w-full max-w-[101.5625rem] self-center">
       <div className="flex-center gap-6 w-2/4">
