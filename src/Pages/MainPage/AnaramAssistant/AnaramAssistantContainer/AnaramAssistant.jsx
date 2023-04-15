bbf5bb039648420363b3ed5e3da764a68d7007d6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-15 13:55:27 +0430

    Component is responsive using grid

--- src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
+++ src/Pages/MainPage/AnaramAssistant/AnaramAssistantContainer/AnaramAssistant.jsx
@@ -10,10 +10,10 @@ function AnaramAssistant() {
         دستیار <strong className="text-primary6">آنارام</strong>
       </h2>
       <p className="max-w-[71.75rem] text-base text-NeutralN300">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد</p>
-      <div className="grid lg:grid-cols-3 max-lg:grid-cols-2 max-lg:grid-rows-2 gap-6 mt-[2.5rem]">
-        <AssistantHelperBox number="1" title="انتخاب دستگاه" description="دستگاهی که میخواهی برایش باتری بخری را انتخاب کن" gradientFrom="from-[#5F34CC]" gradientTo="to-[#22134A]" />
-        <AssistantHelperBox number="2" title="انتخاب باتری" description="سایز باتری را انتخاب کن (قلمی ُ نیم قلمی ُ‌کتابی و ...)" gradientFrom="from-[#47A8BD]" gradientTo="to-[#00829E]" />
-        <AssistantHelperBox number="3" title="بقیش باما" description="دستیار آنارام به صورت هوشمند لیستی از باتری هایی که مناسب شما هست را نمایش میده" gradientFrom="from-[#C53950]" gradientTo="to-[#790013]" />
+      <div className="grid lg:grid-cols-3 max-lg:grid-cols-2 max-lg:grid-rows-2 justify-items-center gap-6 mt-[2.5rem]">
+        <AssistantHelperBox number="1" title="انتخاب دستگاه" description="دستگاهی که میخواهی برایش باتری بخری را انتخاب کن" gradientFrom="from-[#5F34CC]" gradientTo="to-[#22134A]" className="max-lg:col-span-full" />
+        <AssistantHelperBox number="2" title="انتخاب باتری" description="سایز باتری را انتخاب کن (قلمی ُ نیم قلمی ُ‌کتابی و ...)" gradientFrom="from-[#47A8BD]" gradientTo="to-[#00829E]" className="max-lg:row-start-2" />
+        <AssistantHelperBox number="3" title="بقیش باما" description="دستیار آنارام به صورت هوشمند لیستی از باتری هایی که مناسب شما هست را نمایش میده" gradientFrom="from-[#C53950]" gradientTo="to-[#790013]" className="max-lg:row-start-2" />
       </div>
       <PrimaryButton text="بزن بریم" className="mt-16 py-3 px-[4.625rem]" />
     </section>
