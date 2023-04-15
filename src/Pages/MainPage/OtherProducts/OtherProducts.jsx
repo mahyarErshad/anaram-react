ded5861b2614499e317628bcaa947b479fcd06ce
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-15 13:40:36 +0430

    Component is responsive

--- src/Pages/MainPage/OtherProducts/OtherProducts.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProducts.jsx
@@ -8,7 +8,7 @@ function OtherProducts() {
       <h2 className="text-xl font-bold text-NeutralN500">
         محصولات دیگر <strong className="text-xl font-bold text-primary6">آنارام</strong>
       </h2>
-      <div className="w-full max-w-[82.5rem]">
+      <div className="w-full max-w-[82.5rem] grid lg:grid-cols-4 max-lg:grid-cols-2 lg:gap-6 max-lg:gap-3">
         {OtherProductsData.map((data, index) => {
           return <OtherProductsCards key={index} data={data} />;
         })}
