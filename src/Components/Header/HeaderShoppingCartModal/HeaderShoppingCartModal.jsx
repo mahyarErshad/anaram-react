064ea6984ca89034980d9d3c2fa921e5b2f97f77
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-15 12:51:12 +0430

    Overflow of cards is set to scroll

--- src/Components/Header/HeaderShoppingCartModal/HeaderShoppingCartModal.jsx
+++ src/Components/Header/HeaderShoppingCartModal/HeaderShoppingCartModal.jsx
@@ -8,7 +8,7 @@ import styles from "./styles.module.css";
 
 function HeaderShoppingCartModal() {
   return (
-    <div className="max-lg:hidden scale-y-0 origin-top opacity-0 duration-300 flex flex-col absolute left-0 top-12 bg-white border border-NeutralN10 rounded-2xl z-10">
+    <div className="max-lg:hidden max-h-[90vh] scale-y-0 origin-top opacity-0 duration-300 flex flex-col absolute left-0 top-12 bg-white border border-NeutralN10 rounded-2xl z-10">
       <div className="p-4 flex justify-between items-center w-full">
         <span className="text-[0.5625rem] text-NeutralN60">۳ محصول</span>
         <div className="flex-center gap-3 text-primary6">
@@ -16,9 +16,11 @@ function HeaderShoppingCartModal() {
           <ArrowIcon />
         </div>
       </div>
-      <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={150000} amount={2} />
-      <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={102000} discountedPrice={80000} amount={3} discountCard />
-      <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={250000} amount={4} />
+      <div className="flex flex-col overflow-y-scroll">
+        <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={150000} amount={2} />
+        <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={102000} discountedPrice={80000} amount={3} discountCard />
+        <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={250000} amount={4} />
+      </div>
       <div className="py-5 pr-6 pl-4 flex justify-between flex-wrap">
         <PrimaryButton className="py-4 px-[3.125rem]" text="ثبت سفارش" />
         <div className={`${styles["price-section"]} flex flex-col gap-4`}>
