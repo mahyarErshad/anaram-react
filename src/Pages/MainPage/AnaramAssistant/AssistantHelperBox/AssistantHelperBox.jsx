e338c6d0aa2888ca627329be7ee66e62f8050239
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 09:24:02 +0430

    Line height of description is 1rem

--- src/Pages/MainPage/AnaramAssistant/AssistantHelperBox/AssistantHelperBox.jsx
+++ src/Pages/MainPage/AnaramAssistant/AssistantHelperBox/AssistantHelperBox.jsx
@@ -6,7 +6,7 @@ function AssistantHelperBox(props) {
     <div className={`bg-gradient-to-b ${props.className} ${props.gradientFrom} ${props.gradientTo} after:bg-gradient-to-b rounded-2xl py-3 px-6 z-10 flex justify-start items-center flex-col gap-2 min-h-[11.4375rem] relative ${styles["background-shape"]}`}>
       <span className="poppins text-2xl font-light text-white opacity-50">{props.number}</span>
       <span className="text-2xl font-semibold text-white ">{props.title}</span>
-      <p className="text-[9px] text-NeutralN30 text-center max-w-[8.9375rem]">{props.description}</p>
+      <p className="text-[9px] leading-4 text-NeutralN30 text-center max-w-[8.9375rem]">{props.description}</p>
     </div>
   );
 }
