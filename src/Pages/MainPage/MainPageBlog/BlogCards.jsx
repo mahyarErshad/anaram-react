3cd49f375d359e8550c3171eb5a6f4915efad1b3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 11:06:47 +0430

    Component is responsive

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -5,11 +5,11 @@ import image from "../../../assets/images/data/blog-cards/battery.jpg";
 
 function BlogCards() {
   return (
-    <figure className={`${styles.card} transition-all duration-300 rounded-2xl p-4 bg-white border border-primary1 cursor-pointer`}>
+    <figure className={`${styles.card} transition-all duration-300 rounded-2xl max-lg:p-3 lg:p-4 bg-white border border-primary1 cursor-pointer`}>
       <img className="h-2/4 rounded-lg" src={image} alt="" />
-      <figcaption className="text-right text-sm font-semibold text-NeutralN600 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
-      <p className="text-[0.6875rem] text-right w-full leading-6 mt-4 max-w-[17.5rem] text-NeutralN100">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
-      <div className="mt-8 w-full flex justify-between items-center">
+      <figcaption className="text-right text-sm font-semibold text-NeutralN600 max-lg:mt-3 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
+      <p className="text-[0.6875rem] text-right w-full leading-6 max-lg:mt-3 mt-4 max-w-[17.5rem] text-NeutralN100">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
+      <div className="max-lg:mt-5 mt-8 w-full flex justify-between items-center">
         <span className="text-[0.5625rem] text-NeutralN80">۱۴۰۱/۰۷/۱۹</span>
         <div className="flex-center gap-2">
           <span className="text-sm text-NeutralN80 transition-all duration-300">مطالعه مقاله</span>
--- src/Pages/MainPage/MainPageBlog/MainPageBlog.jsx
+++ src/Pages/MainPage/MainPageBlog/MainPageBlog.jsx
@@ -6,7 +6,7 @@ import { FreeMode } from "swiper";
 function MainPageBlog() {
   const arr = [1, 2, 3, 4];
   return (
-    <section className="container flex-center mt-[5.625rem] gap-6">
+    <section className="container flex-center max-lg:mt-16 mt-[5.625rem] gap-6">
       <h2 className="font-bold text-xl text-NeutralN500">
         مجله <strong className="text-primary6">آنارام</strong>
       </h2>
