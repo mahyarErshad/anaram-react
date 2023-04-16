0e8169e18393d16c99f3025e8f96700557d6094e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 11:38:26 +0430

    Imported SideBarMenu

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderActionSection from "../HeaderActionSection/HeaderActionSection";
 import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
+import SideBarMenu from "../SideBarMenu/SideBarMenu";
 
 function HeaderWrapper() {
   return (
     <header className="pt-4 px-4 2xl:px-16 flex flex-col max-md:gap-0 md:gap-6 bg-white">
       <HeaderActionSection />
       <HeaderNavBars />
+      <SideBarMenu />
     </header>
   );
 }
