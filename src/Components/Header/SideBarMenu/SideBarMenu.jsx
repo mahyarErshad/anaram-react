938c68bc6bd3f55c0235b8a029ed6fe4cf55eb5e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 15:04:37 +0430

    Added 3 new icons

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -5,19 +5,11 @@ function SideBarMenu(props) {
   const { sideBarOpen, setSideBarOpen } = props;
   return (
     <section onClick={() => setSideBarOpen(false)} className={`fixed top-0 right-0 w-screen h-screen bg-black ${sideBarOpen ? "bg-opacity-[0.25]" : "bg-opacity-0 pointer-events-none"} lg:hidden duration-300 z-50`}>
-      <aside onClick={(e) => e.stopPropagation()} className={`absolute top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white pt-16 px-8 flex justify-start items-center flex-col gap-2 h-screen min-w-2/4 duration-300`}>
-        <Accordion>
+      <aside onClick={(e) => e.stopPropagation()} className={`absolute top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white pt-16 px-8 flex justify-center h-screen min-w-2/4 duration-300`}>
+        <Accordion className="flex flex-col items-center gap-2">
           <AccordionItem>
-            <AccordionHeader>
+            <AccordionHeader className="bg-NeutralN10 rounded-2xl p-5 flex-center gap-3">
               <h3 className={`accordion-title`}>Title 1</h3>
-            </AccordionHeader>
-            <AccordionBody>
-              <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
-            </AccordionBody>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionHeader>
-              <h3 className={`accordion-title`}>Title 2</h3>
             </AccordionHeader>
             <AccordionBody>
               <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
