2ece82b8053863f263684887fa5096b832383925
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 15:08:20 +0430

    Added icon property to the data

--- src/lib/HeaderNavLinksData/HeaderNavLinksData.js
+++ src/lib/HeaderNavLinksData/HeaderNavLinksData.js
@@ -5,26 +5,32 @@ const headerNavLinksData = [
   {
     text: "باتری بر اساس سایز ",
     hasSubMenu: true,
+    icon: batteryIcon,
   },
   {
     text: "باتری بر اساس کاربرد ",
     hasSubMenu: true,
+    icon: batteryIcon,
   },
   {
     text: "باتری بر اساس تکنولوژی ",
     hasSubMenu: true,
+    icon: batteryIcon,
   },
   {
     text: "درباره آنارام",
     hasSubMenu: false,
+    icon: userIcon,
   },
   {
     text: "تماس با آنارام",
     hasSubMenu: false,
+    icon: userIcon,
   },
   {
     text: "وارتا",
     hasSubMenu: false,
+    icon: batteryIcon,
   },
 ];
 export default headerNavLinksData;
