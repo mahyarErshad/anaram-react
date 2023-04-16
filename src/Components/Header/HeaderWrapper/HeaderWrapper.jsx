94a7ae3d2ea5bb54580eb0c514fb670481a1381f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 09:21:52 +0430

    On 2xl screens px will be 64px

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -4,7 +4,7 @@ import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
 
 function HeaderWrapper() {
   return (
-    <header className="pt-4 px-4 flex flex-col max-md:gap-0 md:gap-6 bg-white">
+    <header className="pt-4 px-4 2xl:px-16 flex flex-col max-md:gap-0 md:gap-6 bg-white">
       <HeaderActionSection />
       <HeaderNavBars />
     </header>
