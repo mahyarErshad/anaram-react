adf83f4d338532c01d4a419827848fa310beb10b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 10:30:43 +0430

    Component is responsive

--- src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
+++ src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
@@ -3,9 +3,9 @@ import banner1 from "../../../assets/images/data/small-banners/banner1.jpg";
 import banner2 from "../../../assets/images/data/small-banners/banner2.jpg";
 
 function MainPageSmallBanners() {
-  const imageClassName = "rounded-2xl lg:max-w-[40.5rem] cursor-pointer max-md:max-h-[11.3125rem]";
+  const imageClassName = "rounded-2xl cursor-pointer";
   return (
-    <section className="w-full max-w-[82.5rem] mx-auto max-lg:mt-12 lg:mt-[6.5rem] bg-transparent flex-center gap-6 flex-wrap">
+    <section className="w-full max-w-[82.5rem] px-4 mx-auto max-lg:mt-12 lg:mt-[6.5rem] bg-transparent grid max-md:grid-cols-1 md:grid-cols-2 gap-6 justify-items-center">
       <img className={imageClassName} src={banner1} alt="Banner for advertising batteries for customers" />
       <img className={imageClassName} src={banner2} alt="Banner for advertising batteries for customers" />
     </section>
