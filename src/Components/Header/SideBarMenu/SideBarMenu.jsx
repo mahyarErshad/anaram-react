ba4ffa7af2e7630d7009b4526b5ecc915299a75a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 16:02:21 +0430

    Spaces are corrected

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -12,18 +12,18 @@ function SideBarMenu(props) {
       <aside onClick={(e) => e.stopPropagation()} className={`absolute top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white pt-16 px-8 flex justify-center h-screen min-w-2/4 duration-300`}>
         <Accordion className="flex flex-col items-center gap-2">
           <AccordionItem>
-            <AccordionHeader className={`${styles["accordion-header"]} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 rounded-2xl p-5 flex-center w-[13.5rem] border border-transparent hover:border-neutral-300 duration-300`}>
+            <AccordionHeader className={`${styles["accordion-header"]} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] border border-transparent hover:border-neutral-300 duration-300`}>
               <HomeIcon className="ml-[0.625rem] stroke-inherit" />
-              <h3 className="text-sm text-inherit font-medium">صفحه اصلی</h3>
+              <h3 className="text-xs text-inherit font-semibold">صفحه اصلی</h3>
             </AccordionHeader>
           </AccordionItem>
           {headerNavLinksData.map((item, index) => {
             return (
               <AccordionItem key={index}>
-                <AccordionHeader className={`${styles["accordion-header"]} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 rounded-2xl p-5 flex-center w-[13.5rem] border border-transparent hover:border-neutral-300 duration-300`}>
+                <AccordionHeader className={`${styles["accordion-header"]} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 rounded-2xl p-5 flex justify-start items-center w-[13.5rem] border border-transparent hover:border-neutral-300 duration-300`}>
                   {item.icon}
-                  <h3 className="text-sm text-inherit font-medium">{item.text}</h3>
-                  {item.hasSubMenu && <ArrowIcon />}
+                  <h3 className="text-xs text-inherit font-semibold mr-[0.625rem]">{item.text}</h3>
+                  {item.hasSubMenu && <ArrowIcon className="mr-[0.375rem] stroke-inherit" />}
                 </AccordionHeader>
               </AccordionItem>
             );
