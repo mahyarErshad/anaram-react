1ab7d60cd6ef2649c0a8e27c95734bc8f4f1e56a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 15:34:26 +0430

    Home page navigation is created

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
-import homeIcon from "../../../assets/images/data/sidebar-navlinks/home.svg";
+import { ReactComponent as HomeIcon } from "../../../assets/images/data/sidebar-navlinks/home.svg";
+import styles from "./styles.module.css";
 
 function SideBarMenu(props) {
   const { sideBarOpen, setSideBarOpen } = props;
   return (
     <section onClick={() => setSideBarOpen(false)} className={`fixed top-0 right-0 w-screen h-screen bg-black ${sideBarOpen ? "bg-opacity-[0.25]" : "bg-opacity-0 pointer-events-none"} lg:hidden duration-300 z-50`}>
       <aside onClick={(e) => e.stopPropagation()} className={`absolute top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white pt-16 px-8 flex justify-center h-screen min-w-2/4 duration-300`}>
-        <Accordion className="flex flex-col items-center">
+        <Accordion className="flex flex-col items-center gap-2">
           <AccordionItem>
-            <AccordionHeader className="bg-NeutralN10 rounded-2xl p-5 flex-center w-[13.5rem]">
-              <img className="ml-[0.625rem]" src={homeIcon} alt="Icon of home page" />
-              <h3 className={`accordion-title text-sm text-NeutralN90`}>صفحه اصلی</h3>
+            <AccordionHeader className={`${styles["accordion-header"]} bg-NeutralN10 rounded-2xl p-5 flex-center w-[13.5rem]`}>
+              <HomeIcon className="ml-[0.625rem] stroke-NeutralN90" />
+              <h3 className="text-sm text-NeutralN90">صفحه اصلی</h3>
             </AccordionHeader>
           </AccordionItem>
-
           <AccordionItem>
             <AccordionHeader className="bg-NeutralN10 rounded-2xl p-5 flex-center gap-3">
               <h3 className={`accordion-title`}>Title 1</h3>
