f7558f3779b71d58c7240ddfc89ae197b5e15d8b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 12:54:47 +0430

    Catching sideBarOpen , setSideBarOpen

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
 
-function SideBarMenu() {
+function SideBarMenu(props) {
+    const {sideBarOpen , setSideBarOpen} = props
   return (
     <section className="fixed top-0 right-0 w-screen h-screen bg-black bg-opacity-[0.25] z-50 lg:hidden">
       <aside className="absolute top-0 right-0 bg-white pt-16 px-8 flex justify-start items-center flex-col gap-2 h-screen z-50 min-w-2/4">
