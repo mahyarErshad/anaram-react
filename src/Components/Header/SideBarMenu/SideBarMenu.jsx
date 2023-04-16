ccaf56c8112a74efa4163b739487d47fcf0e1031
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 11:55:17 +0430

    Aside is created

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function SideBarMenu() {
-  return (
-    <aside className='fixed top-0 right-0 '>
-sidebar
-    </aside>
-  )
+  return <aside className="fixed top-0 right-0 bg-white pt-16 px-8">
+    
+  </aside>;
 }
 
-export default SideBarMenu
+export default SideBarMenu;
