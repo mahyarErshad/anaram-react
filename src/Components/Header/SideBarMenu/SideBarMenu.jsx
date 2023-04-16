a5b7a1c8f41d0008fb568a1339617befdf8cef9c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 13:55:16 +0430

    Fixed a bug using stopPropagation

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
 
 function SideBarMenu(props) {
-    const {sideBarOpen , setSideBarOpen} = props
+  const { sideBarOpen, setSideBarOpen } = props;
   return (
-    <section className="fixed top-0 right-0 w-screen h-screen bg-black bg-opacity-[0.25] z-50 lg:hidden">
-      <aside className="absolute top-0 right-0 bg-white pt-16 px-8 flex justify-start items-center flex-col gap-2 h-screen z-50 min-w-2/4">
+    <section onClick={() => setSideBarOpen(false)} className={`fixed top-0 right-0 w-screen h-screen bg-black ${sideBarOpen ? "bg-opacity-[0.25]" : "bg-opacity-0 pointer-events-none"} lg:hidden duration-300 z-50`}>
+      <aside onClick={(e) => e.stopPropagation()} className={`absolute top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white pt-16 px-8 flex justify-start items-center flex-col gap-2 h-screen min-w-2/4 duration-300`}>
         <Accordion>
           <AccordionItem>
             <AccordionHeader>
