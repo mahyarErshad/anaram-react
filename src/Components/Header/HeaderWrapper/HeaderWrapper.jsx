b520c9bc92368701b97f4679f683dd94c51a3789
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 12:51:52 +0430

    Created a state for sidebar open

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderActionSection from "../HeaderActionSection/HeaderActionSection";
 import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
 import SideBarMenu from "../SideBarMenu/SideBarMenu";
 
 function HeaderWrapper() {
+  const [sideBarOpen, setSideBarOpen] = useState(false)
   return (
     <header className="pt-4 px-4 2xl:px-16 flex flex-col max-md:gap-0 md:gap-6 bg-white">
       <HeaderActionSection />
