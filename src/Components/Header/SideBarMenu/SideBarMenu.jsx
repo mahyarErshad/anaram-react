f26f1ee5e17f8b0d4a8d70b6d40f289658f12a38
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 15:09:42 +0430

    Imported home Icon

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
+import homeIcon from "../../../assets/images/data/sidebar-navlinks/home.svg"
 
 function SideBarMenu(props) {
   const { sideBarOpen, setSideBarOpen } = props;
@@ -7,6 +8,7 @@ function SideBarMenu(props) {
     <section onClick={() => setSideBarOpen(false)} className={`fixed top-0 right-0 w-screen h-screen bg-black ${sideBarOpen ? "bg-opacity-[0.25]" : "bg-opacity-0 pointer-events-none"} lg:hidden duration-300 z-50`}>
       <aside onClick={(e) => e.stopPropagation()} className={`absolute top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white pt-16 px-8 flex justify-center h-screen min-w-2/4 duration-300`}>
         <Accordion className="flex flex-col items-center gap-2">
+
           <AccordionItem>
             <AccordionHeader className="bg-NeutralN10 rounded-2xl p-5 flex-center gap-3">
               <h3 className={`accordion-title`}>Title 1</h3>
