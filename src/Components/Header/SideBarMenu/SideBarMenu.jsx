b282e252e0c42bcd3d2612eeb2a635ff7223be3a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 12:51:01 +0430

    Sidebar and overlay are placed in the dom

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
 
 function SideBarMenu() {
-  return <aside className="fixed top-0 right-0 bg-white pt-16 px-8">
-    
-  </aside>;
+  return (
+    <section className="fixed top-0 right-0 w-screen h-screen bg-black bg-opacity-[0.25] z-50 lg:hidden">
+      <aside className="absolute top-0 right-0 bg-white pt-16 px-8 flex justify-start items-center flex-col gap-2 h-screen z-50 min-w-2/4">
+        <Accordion>
+          <AccordionItem>
+            <AccordionHeader>
+              <h3 className={`accordion-title`}>Title 1</h3>
+            </AccordionHeader>
+            <AccordionBody>
+              <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
+            </AccordionBody>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionHeader>
+              <h3 className={`accordion-title`}>Title 2</h3>
+            </AccordionHeader>
+            <AccordionBody>
+              <div className="accordion-body">Lorem ipsum dolor sit amet.</div>
+            </AccordionBody>
+          </AccordionItem>
+        </Accordion>
+      </aside>
+    </section>
+  );
 }
 
 export default SideBarMenu;
