4d8b692c6c69242e51c7b6c129511fedb3b2ea37
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 09:38:02 +0430

    Component is responsive

--- src/Pages/MainPage/AnaramAssistant/AssistantHelperBox/AssistantHelperBox.jsx
+++ src/Pages/MainPage/AnaramAssistant/AssistantHelperBox/AssistantHelperBox.jsx
@@ -5,7 +5,7 @@ function AssistantHelperBox(props) {
   return (
     <div className={`bg-gradient-to-b ${props.className} ${props.gradientFrom} ${props.gradientTo} after:bg-gradient-to-b rounded-2xl py-3 px-6 z-10 flex justify-start items-center flex-col gap-2 min-h-[11.4375rem] relative ${styles["background-shape"]}`}>
       <span className="poppins text-2xl font-light text-white opacity-50">{props.number}</span>
-      <span className="text-2xl font-semibold text-white ">{props.title}</span>
+      <span className="text-2xl font-semibold text-white text-center">{props.title}</span>
       <p className="text-[9px] leading-4 text-NeutralN30 text-center max-w-[8.9375rem]">{props.description}</p>
     </div>
   );
