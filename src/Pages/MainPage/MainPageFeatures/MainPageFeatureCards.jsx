adb3c107a5a7b0d1325ebb2d81f75c9d2982db9d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-16 11:01:09 +0430

    Component is responsive

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
@@ -4,7 +4,7 @@ function MainPageFeatureCards(props) {
   return (
     <div className="flex max-md:flex-col justify-start items-center max-md:gap-2 md:gap-6 flex-grow">
       <div className="flex-center bg-NeutralN10 rounded-[0.3125rem] max-md:p-3 md:p-4 max-md:w-12 max-md:h-12 md:w-[4.5rem] md:h-[4.5rem]">{props.icon}</div>
-      <span className="text-sm text-center font-semibold text-NeutralN500">{props.text}</span>
+      <span className="text-sm text-center lg:font-semibold text-NeutralN500">{props.text}</span>
     </div>
   );
 }
