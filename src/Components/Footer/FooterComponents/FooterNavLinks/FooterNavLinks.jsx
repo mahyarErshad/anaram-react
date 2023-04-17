10c56e1e1b1634c90e6ff3aeab1d4f5dfd5263b1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 10:46:33 +0430

    Set whitespace to no-wrap

--- src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
+++ src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function FooterNavLinks({ data }) {
   return (
     <div className="flex flex-col gap-4">
-      <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold">{data[0].title}</span>
+      <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">{data[0].title}</span>
       <ul className="flex flex-col justify-start items-start gap-3">
         {data[0].navLinks.map((item, index) => {
           return (
