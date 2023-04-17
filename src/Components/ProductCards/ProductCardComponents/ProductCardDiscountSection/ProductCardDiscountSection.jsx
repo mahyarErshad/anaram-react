e8c2341c40e0cd305611d92003f0bb5b39a31a7e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 16:09:55 +0430

    Cleaned and optimized the code

--- src/Components/ProductCards/ProductCardComponents/ProductCardDiscountSection/ProductCardDiscountSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardDiscountSection/ProductCardDiscountSection.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import styles from "../../style/productCard.module.css";
-import  { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits"
+import { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits";
 
-function ProductCardDiscountSection({ percentage }) {
-  return <span className={`${styles["discount-percentage"]} text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal`}>{separateNumberByThousand(percentage)}٪</span>;
-}
+const ProductCardDiscountSection = ({ percentage }) => <span className={`${styles["discount-percentage"]} text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal`}>{separateNumberByThousand(percentage)}٪</span>;
 
 export default ProductCardDiscountSection;
