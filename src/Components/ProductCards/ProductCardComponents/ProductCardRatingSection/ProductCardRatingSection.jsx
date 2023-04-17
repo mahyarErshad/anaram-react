522b5028f34cc20d4219b59a9a648a05bee7f4ad
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 16:12:30 +0430

    Cleaned the code

--- src/Components/ProductCards/ProductCardComponents/ProductCardRatingSection/ProductCardRatingSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardRatingSection/ProductCardRatingSection.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from "react";
 import { ReactComponent as StarIcon } from "../../../../assets/images/icons/product-card-star.svg";
-import { separateNumberByThousand } from '../../../../lib/function/convertToPersianDigits';
+import { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits";
 
-function ProductCardRatingSection({rating}) {
-  return (
-    <div className="absolute -top-1 -left-1 flex-center gap-[0.125rem]">
-    <span className="font-semibold text-xs text-primary6 translate-y-[0.0625rem]">{separateNumberByThousand(rating)}</span>
+const ProductCardRatingSection = ({ rating }) => (
+  <div className="absolute -top-1 -left-1 flex-center gap-1">
+    <span className="font-semibold text-xs text-primary6">{separateNumberByThousand(rating)}</span>
     <StarIcon />
   </div>
-  )
-}
+);
 
-export default ProductCardRatingSection
+export default ProductCardRatingSection;
