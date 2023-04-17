72a10106baa0a1775718e826ed2beb5bd3608a81
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 13:36:51 +0430

    Imported Battery

--- src/Pages/MainPage/BatteryChargingComponent/BatteryChargingComponent.jsx
+++ src/Pages/MainPage/BatteryChargingComponent/BatteryChargingComponent.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import { ReactComponent as Battery } from "../../../assets/images/battery-component/battery-full.svg";
 
 function BatteryChargingComponent() {
-  return (
-    <div>BatteryChargingComponent</div>
-  )
+  return <div>BatteryChargingComponent</div>;
 }
 
-export default BatteryChargingComponent
+export default BatteryChargingComponent;
