f4589244384c4cb5b1f64c844de4d6bfcc7863d5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 16:11:29 +0430

    Cleaned the code

--- src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardPriceSection/ProductCardPriceSection.jsx
@@ -4,15 +4,16 @@ import { ReactComponent as TomanIconGray } from "../../../../assets/images/icons
 import styles from "../../style/productCard.module.css";
 import { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits";
 
-function ProductCardPriceSection({ discountText, price }) {
-  const textStyles = discountText ? "text-NeutralN90  text-[0.75rem]" : "text-NeutralN700 text-sm";
+const ProductCardPriceSection = ({ discountText, price }) => {
+  const textStyles = discountText ? "text-NeutralN90 text-sm" : "text-NeutralN700 text-sm";
   const iconStyles = "w-[0.9375rem] h-[0.6875rem] -translate-y-[0.125rem]";
+
   return (
-    <span className={`${textStyles} ${discountText && styles.lineThrough} flex-center gap-1 font-semibold relative`}>
+    <span className={`${textStyles} ${discountText ? styles.lineThrough : ""} flex-center gap-1 font-semibold relative`}>
       {separateNumberByThousand(price)}
       {discountText ? <TomanIconGray className={iconStyles} /> : <TomanIconBlack className={iconStyles} />}
     </span>
   );
-}
+};
 
 export default ProductCardPriceSection;
