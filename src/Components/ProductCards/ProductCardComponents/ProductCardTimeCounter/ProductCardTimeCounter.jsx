cd229ccaaa1fb5a8d5b29aded1eb64c4f85c8444
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 16:13:37 +0430

    Cleaned the code

--- src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import styles from "../../style/productCard.module.css"
+import styles from "../../style/productCard.module.css";
 import { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits";
 
-function ProductCardTimeCounter({number , text}) {
+const ProductCardTimeCounter = ({ number, text }) => {
+  const formattedNumber = separateNumberByThousand(number).padStart(2, '0');
   return (
     <div className={`${styles.counter} flex flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30`}>
-      <span className="text-xs font-semibold text-Magenta">{separateNumberByThousand(number).padStart(2, '0')}</span>
+      <span className="text-xs font-semibold text-Magenta">{formattedNumber}</span>
       <span className="text-[0.5rem] text-NeutralN50 transition-all duration-300">{text}</span>
     </div>
   );
-}
+};
 
 export default ProductCardTimeCounter;
