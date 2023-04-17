fbfdc48b1bd1550172c3e602d5da77112db1dc9f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 15:50:16 +0430

    Cleaned and optimized the component

--- src/Components/Header/HeaderShoppingCartModal/HeaderShoppingCartModal.jsx
+++ src/Components/Header/HeaderShoppingCartModal/HeaderShoppingCartModal.jsx
@@ -7,25 +7,50 @@ import ProductCardPriceSection from "../../ProductCards/ProductCardComponents/Pr
 import styles from "./styles.module.css";
 
 function HeaderShoppingCartModal() {
+  const products = [
+    {
+      image: image,
+      title: "باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی",
+      price: 150000,
+      amount: 2,
+    },
+    {
+      image: image,
+      title: "باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی",
+      price: 102000,
+      discountedPrice: 80000,
+      amount: 3,
+      discountCard: true,
+    },
+    {
+      image: image,
+      title: "باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی",
+      price: 250000,
+      amount: 4,
+    },
+  ];
+
+  const totalPrice = products.reduce((acc, product) => acc + (product.discountedPrice || product.price) * product.amount, 0);
+
   return (
     <div className="max-lg:hidden max-h-[90vh] scale-y-0 origin-top opacity-0 duration-300 flex flex-col absolute left-0 top-12 bg-white border border-NeutralN10 rounded-2xl z-10">
       <div className="p-4 flex justify-between items-center w-full">
-        <span className="text-[0.5625rem] text-NeutralN60">۳ محصول</span>
+        <span className="text-xs text-NeutralN60">{`${products.length} محصول`}</span>
         <div className="flex-center gap-3 text-primary6">
           <span className="text-xs font-semibold">مشاهده سبد خرید</span>
           <ArrowIcon />
         </div>
       </div>
       <div className="flex flex-col overflow-y-auto">
-        <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={150000} amount={2} />
-        <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={102000} discountedPrice={80000} amount={3} discountCard />
-        <SideBySideProductCard image={image} title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" price={250000} amount={4} />
+        {products.map((product, index) => (
+          <SideBySideProductCard key={index} {...product} />
+        ))}
       </div>
       <div className="py-5 pr-6 pl-4 flex justify-between flex-wrap">
         <PrimaryButton className="py-4 px-[3.125rem]" text="ثبت سفارش" />
         <div className={`${styles["price-section"]} flex flex-col gap-4`}>
-          <span className="text-[0.5625rem] font-semibold text-NeutralN100">مبلغ قابل پرداخت</span>
-          <ProductCardPriceSection price={502000} />
+          <span className="text-xs font-semibold text-NeutralN100">مبلغ قابل پرداخت</span>
+          <ProductCardPriceSection price={totalPrice} />
         </div>
       </div>
     </div>
