69a1878fdfc5bc9effeffd7b3424e06096a6c2d5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 16:20:15 +0430

    Cleaned and optimized the code

--- src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
+++ src/Components/Utils/Buttons/PrimaryButton/PrimaryButton.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-function PrimaryButton(props) {
-  const spaces = props.addToCartButton ? "py-2 px-2 gap-[0.625rem] text-[0.625rem]" : "gap-3 px-4 py-3 text-sm";
+function PrimaryButton({ addToCartButton, onClick, className, icon, text }) {
+  const spaces = addToCartButton ? "py-2 px-2 gap-[0.625rem] text-[0.625rem]" : "gap-3 px-4 py-3 text-sm";
+
   return (
-    <button onClick={props.onClick} className={`${props.className} ${styles.button} ${spaces} transition-all duration-300 rounded-5px bg-primary6 flex-center text-white`}>
-      {props.icon}
-      {props.text}
+    <button onClick={onClick} className={`${className} ${styles.button} ${spaces} transition-all duration-300 rounded-5px bg-primary6 flex-center text-white`}>
+      {icon}
+      {text}
     </button>
   );
 }
