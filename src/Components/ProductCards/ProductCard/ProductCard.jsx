6478db6445f4c98f54659995709cfbe918a1b29f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 16:08:28 +0430

    Cleaned and optimized the code

--- src/Components/ProductCards/ProductCard/ProductCard.jsx
+++ src/Components/ProductCards/ProductCard/ProductCard.jsx
@@ -12,25 +12,30 @@ import ProductCardDiscountSection from "../ProductCardComponents/ProductCardDisc
 import ProductCardTitleSection from "../ProductCardComponents/ProductCardTitleSection/ProductCardTitleSection";
 import ProductCardTimeCounter from "../ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter";
 
-function ProductCard(props) {
+const ProductCard = ({ discountCard }) => {
+  const discountSection = discountCard && <ProductCardDiscountSection percentage={50} />;
+  const priceSection = (
+    <div className={`flex ${discountCard ? "justify-between" : "justify-end"} items-center w-full mb-3`}>
+      {discountCard && <ProductCardPriceSection price={200000} discountText />}
+      <ProductCardPriceSection price={100000} />
+    </div>
+  );
+
   return (
-    <figure className={`${styles.card} ${props.discountCard && styles.after} pt-3 pb-[0.875rem] relative px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col cursor-pointer transition-all duration-300`}>
+    <figure className={`${styles.card} ${discountCard && styles.after} pt-3 pb-[0.875rem] relative px-3 rounded-lg bg-white border border-NeutralN30 flex flex-col cursor-pointer transition-all duration-300`}>
       <div className="flex-center h-2/4 mb-4 relative">
         <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
-        {props.discountCard && <ProductCardDiscountSection percentage={50} />}
+        {discountSection}
         <ProductCardRatingSection rating={4.5} />
       </div>
       <ProductCardTitleSection title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" />
-      <div className={`flex ${props.discountCard ? "justify-between" : "justify-end"} items-center w-full mb-3`}>
-        {props.discountCard && <ProductCardPriceSection price={200000} discountText />}
-        <ProductCardPriceSection price={100000} />
-      </div>
+      {priceSection}
       <div className="flex-center gap-1 w-full">
         <PrimaryButton addToCartButton text="افزودن به سبد" icon={<CartIcon className="w-4 h-4" stroke="white" />} />
         <SmallButton className="p-2 bg-NeutralN20" icon={<HeartIcon />} />
         <SmallButton className="p-2 bg-NeutralN20" icon={<CompareArrowsIcon />} />
       </div>
-      {props.discountCard && (
+      {discountCard && (
         <div className="flex-center gap-0 bg-NeutralN20 rounded-[4px] py-1 mb-1 mt-3">
           <ProductCardTimeCounter number={6} text="ثانیه" />
           <ProductCardTimeCounter number={24} text="دقیقه" />
@@ -40,6 +45,6 @@ function ProductCard(props) {
       )}
     </figure>
   );
-}
+};
 
 export default ProductCard;
