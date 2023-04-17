79a07c52294384776ece9d961d96b1ef8cce33a4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 16:00:49 +0430

    Make the code clean and optimized

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -6,11 +6,18 @@ import headerNavLinksData from "../../../lib/HeaderNavLinksData/HeaderNavLinksDa
 import { ReactComponent as ArrowIcon } from "../../../assets/images/icons/arrow-icon-toBottom.svg";
 import styles from "./styles.module.css";
 
-function SideBarMenu(props) {
-  const { sideBarOpen, setSideBarOpen } = props;
+function SideBarMenu({ sideBarOpen, setSideBarOpen }) {
+  const handleOutsideClick = () => {
+    if (sideBarOpen) setSideBarOpen(false);
+  };
+
+  const handleInsideClick = (event) => {
+    event.stopPropagation();
+  };
+
   return (
-    <section onClick={() => setSideBarOpen(false)} className={`fixed top-0 right-0 w-screen h-full min-h-screen bg-black ${sideBarOpen ? "bg-opacity-[0.25]" : "bg-opacity-0 pointer-events-none"} lg:hidden duration-300 z-50`}>
-      <aside onClick={(e) => e.stopPropagation()} className={`absolute h-full overflow-y-auto top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white py-16 px-8 flex justify-center h-screen min-w-2/4 duration-300`}>
+    <section onClick={handleOutsideClick} className={`fixed top-0 right-0 w-screen h-full min-h-screen bg-black ${sideBarOpen ? "bg-opacity-[0.25]" : "bg-opacity-0 pointer-events-none"} lg:hidden duration-300 z-50`}>
+      <aside onClick={handleInsideClick} className={`absolute h-full overflow-y-auto top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white py-16 px-8 flex justify-center h-screen min-w-2/4 duration-300`}>
         <Accordion className="flex flex-col items-center gap-2 h-full">
           <AccordionItem>
             <AccordionHeader className={`${styles["accordion-header"]} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 border border-transparent hover:border-primary2 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] duration-300`}>
@@ -18,29 +25,25 @@ function SideBarMenu(props) {
               <h3 className="text-xs text-inherit font-semibold">صفحه اصلی</h3>
             </AccordionHeader>
           </AccordionItem>
-          {headerNavLinksData.map((item, index) => {
-            return (
-              <AccordionItem key={index}>
-                <AccordionHeader className={`${styles["accordion-header"]} ${item.hasSubMenu && styles.AccordionBodyHasMenu} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 border border-transparent hover:border-primary2 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] duration-300`}>
-                  {item.icon}
-                  <h3 className="text-xs text-inherit font-semibold mr-[0.625rem]">{item.text}</h3>
-                  {item.hasSubMenu && <ArrowIcon className="stroke-inherit mr-[0.375rem]" />}
-                </AccordionHeader>
-                {item.hasSubMenu && (
-                  <AccordionBody className="bg-primary1 w-full rounded-b-2xl -mt-2 px-5 flex flex-col items-center gap-2">
-                    <span className="w-full h-[1px] bg-NeutralN20 mb-2"></span>
-                    {item.subMenu.map((item, index) => {
-                      return (
-                        <span key={index} className={`${styles.navCards} bg-NeutralN10 rounded-2xl cursor-pointer w-full py-3 flex-center text-xs text-[#616161]`}>
-                          {item}
-                        </span>
-                      );
-                    })}
-                  </AccordionBody>
-                )}
-              </AccordionItem>
-            );
-          })}
+          {headerNavLinksData.map((item, index) => (
+            <AccordionItem key={index}>
+              <AccordionHeader className={`${styles["accordion-header"]} ${item.hasSubMenu && styles.AccordionBodyHasMenu} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 border border-transparent hover:border-primary2 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] duration-300`}>
+                {item.icon}
+                <h3 className="text-xs text-inherit font-semibold mr-[0.625rem]">{item.text}</h3>
+                {item.hasSubMenu && <ArrowIcon className="stroke-inherit mr-[0.375rem]" />}
+              </AccordionHeader>
+              {item.hasSubMenu && (
+                <AccordionBody className="bg-primary1 w-full rounded-b-2xl -mt-2 px-5 flex flex-col items-center gap-2">
+                  <span className="w-full h-[1px] bg-NeutralN20 mb-2"></span>
+                  {item.subMenu.map((subMenuItem, subMenuIndex) => (
+                    <span key={subMenuIndex} className={`${styles.navCards} bg-NeutralN10 rounded-2xl cursor-pointer w-full py-3 flex-center text-xs text-[#616161]`}>
+                      {subMenuItem}
+                    </span>
+                  ))}
+                </AccordionBody>
+              )}
+            </AccordionItem>
+          ))}
           <AccordionItem>
             <AccordionHeader className={`stroke-white text-white bg-primary6 border border-transparent hover:border-primary2 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] duration-300`}>
               <BatteryIcon className="ml-[0.625rem] stroke-inherit" />
