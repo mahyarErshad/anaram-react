5b911a362e3b82d59380fd930c5090bdad3916e6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-17 15:01:19 +0430

    Cleaned the code

--- src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
+++ src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
-function FooterNavLinks({ data }) {
-  return (
-    <div className="flex flex-col gap-4">
-      <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">{data[0].title}</span>
-      <ul className="flex flex-col justify-start items-start gap-3">
-        {data[0].navLinks.map((item, index) => {
-          return (
-            <li key={index} className="text-sm text-NeutralN200">
-              {item}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-}
+const FooterNavLinks = ({ data }) => (
+  <div className="flex flex-col gap-4">
+    <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">
+      {data[0].title}
+    </span>
+    <ul className="flex flex-col gap-3">
+      {data[0].navLinks.map((item, index) => (
+        <li key={index} className="text-sm text-NeutralN200">
+          {item}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default FooterNavLinks;
