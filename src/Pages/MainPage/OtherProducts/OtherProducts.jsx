bda894cdb6b03fa0101e704a51d9f8c8332d6155
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 11:39:19 +0430

    Cleaned the code

--- src/Pages/MainPage/OtherProducts/OtherProducts.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProducts.jsx
@@ -4,17 +4,18 @@ import charger from "../../../assets/images/data/other-products/charger.png";
 import powerBank from "../../../assets/images/data/other-products/power-bank.png";
 import otherProductsImage from "../../../assets/images/data/other-products/other-products.png";
 
+const data = [flashLight, charger, powerBank, otherProductsImage];
+
 function OtherProducts() {
-  const data = [flashLight, charger, powerBank, otherProductsImage];
   return (
     <section className="container mt-24 gap-8">
       <h2 className="text-xl font-bold text-NeutralN500">
         محصولات دیگر <strong className="text-xl font-bold text-primary6">آنارام</strong>
       </h2>
-      <div className="w-full lg:max-w-[82.5rem] max-lg:max-w-[42.5rem] grid lg:grid-cols-4 max-lg:grid-cols-2 justify-items-center max-lg:gap-y-4">
-        {data.map((data, index) => {
-          return <img src={data} alt="products category" key={index} />;
-        })}
+      <div className="grid grid-cols-2 gap-y-4 justify-items-center max-w-[82.5rem] mx-auto lg:grid-cols-4 max-lg:max-w-[42.5rem] max-lg:grid-cols-2">
+        {data.map((image, index) => (
+          <img key={index} src={image} alt="products category" />
+        ))}
       </div>
     </section>
   );
