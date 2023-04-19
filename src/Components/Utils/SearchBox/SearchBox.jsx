1f4d2b770b4b9499da505cb43ea14bc9e5debc36
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 09:21:17 +0430

    Cleaned the code

--- src/Components/Utils/SearchBox/SearchBox.jsx
+++ src/Components/Utils/SearchBox/SearchBox.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { ReactComponent as SearchIcon } from "../../../assets/images/icons/search-icon.svg";
 import styles from "./styles.module.css";
 
-function SearchBox(props) {
-  const wrapperClass = `${props.className} ${styles.wrapper} bg-NeutralN10 border border-transparent transition-all duration-300 py-[0.625rem] flex px-5 rounded-5px gap-4 hover:border-primary1`;
+function SearchBox({ className }) {
+  const wrapperClass = `${styles.wrapper} ${className} bg-NeutralN10 border border-transparent transition-all duration-300 py-[0.625rem] flex px-5 rounded-5 hover:border-primary1 gap-4`;
 
   return (
     <div className={wrapperClass}>
       <SearchIcon />
-      <input type="search" placeholder="جستجو" className="bg-transparent border-0 min-h-[1.625rem] text-sm focus:outline-none w-full" />
+      <input type="search" placeholder="جستجو" className="bg-transparent border-0 min-h-5 text-sm focus:outline-none w-full" />
     </div>
   );
 }
