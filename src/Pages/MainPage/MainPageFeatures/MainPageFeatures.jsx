1d332563dac0ad4ec607937dfa36b60aed640655
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 11:21:50 +0430

    Cleaned the code

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatures.jsx
@@ -5,14 +5,20 @@ import { ReactComponent as TruckIcon } from "../../../assets/images/main-page-fe
 import { ReactComponent as CreditCardIcon } from "../../../assets/images/main-page-features/credit-card.svg";
 import { ReactComponent as CustomerSupportIcon } from "../../../assets/images/main-page-features/customer-support.svg";
 
+const features = [
+  { icon: <MedalIcon />, text: "ضمانت اصلات کالا" },
+  { icon: <TruckIcon />, text: "ارسال به سراسر کشور" },
+  { icon: <CreditCardIcon />, text: "پرداخت امن" },
+  { icon: <CustomerSupportIcon />, text: "مشاوره رایگان خرید" },
+];
+
 function MainPageFeatures() {
   return (
     <section className="w-full max-w-[80.5rem] mx-auto bg-white rounded-[0.625rem] px-3 py-2 flex-center mt-[5.625rem]">
       <div className="flex-center max-md:gap-2 gap-0 w-full">
-        <MainPageFeatureCards icon={<MedalIcon />} text="ضمانت اصلات کالا" />
-        <MainPageFeatureCards icon={<TruckIcon />} text="ارسال به سراسر کشور" />
-        <MainPageFeatureCards icon={<CreditCardIcon />} text="پرداخت امن" />
-        <MainPageFeatureCards icon={<CustomerSupportIcon />} text="مشاوره رایگان خرید" />
+        {features.map((feature, index) => (
+          <MainPageFeatureCards key={index} icon={feature.icon} text={feature.text} />
+        ))}
       </div>
     </section>
   );
