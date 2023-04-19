ee5db4d02e170842f90819faecdb51f88324da22
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 11:15:43 +0430

    Cleaned the code

--- src/Pages/MainPage/MainPageBlog/MainPageBlog.jsx
+++ src/Pages/MainPage/MainPageBlog/MainPageBlog.jsx
@@ -5,19 +5,19 @@ import { FreeMode } from "swiper";
 
 function MainPageBlog() {
   const arr = [1, 2, 3, 4];
+
   return (
     <section className="container flex-center max-lg:mt-16 mt-[5.625rem] gap-6">
       <h2 className="font-bold text-xl text-NeutralN500">
         مجله <strong className="text-primary6">آنارام</strong>
       </h2>
-      <Swiper slidesPerView={"auto"} spaceBetween={24} modules={[FreeMode]} freeMode={true} className="mySwiper">
-        {arr.map((item, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <BlogCards />
-            </SwiperSlide>
-          );
-        })}
+
+      <Swiper slidesPerView="auto" spaceBetween={24} modules={[FreeMode]} freeMode className="mySwiper">
+        {arr.map((_, index) => (
+          <SwiperSlide key={index}>
+            <BlogCards />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
