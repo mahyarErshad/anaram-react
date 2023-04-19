8dfe023ca5f33572c298ffc913cc256de1a54b0a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 11:29:32 +0430

    Cleaned the code

--- src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
+++ src/Pages/MainPage/MainPageSmallBanners/MainPageSmallBanners.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import banner1 from "../../../assets/images/data/small-banners/banner1.jpg";
 import banner2 from "../../../assets/images/data/small-banners/banner2.jpg";
 
-function MainPageSmallBanners() {
+const MainPageSmallBanners = () => {
   const imageClassName = "rounded-2xl cursor-pointer";
   return (
     <section className="w-full max-w-[82.5rem] px-4 mx-auto max-lg:mt-12 lg:mt-[6.5rem] bg-transparent grid max-md:grid-cols-1 md:grid-cols-2 gap-6 justify-items-center">
-      <img className={imageClassName} src={banner1} alt="Banner for advertising batteries for customers" />
-      <img className={imageClassName} src={banner2} alt="Banner for advertising batteries for customers" />
+      <img className={imageClassName} src={banner1} alt="Banner 1" />
+      <img className={imageClassName} src={banner2} alt="Banner 2" />
     </section>
   );
-}
+};
 
 export default MainPageSmallBanners;
