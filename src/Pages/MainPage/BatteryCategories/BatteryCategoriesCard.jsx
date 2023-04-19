c629a8ba73f80b07e188820b9df20ee7e0b58cd3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 11:00:07 +0430

    Cleaned the code

--- src/Pages/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
+++ src/Pages/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import stage from "../../../assets/images/data/battery-categories/stage.png";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 
-function BatteryCategoriesCard({data}) {
+const BatteryCategoriesCard = ({ data }) => {
+  const { image, text } = data;
   return (
     <figure className={`flex-center flex-col cursor-pointer max-lg:mt-6 ${styles.card}`}>
-      <img className="mb-2 max-h-[5.75rem] object-contain transition-all duration-300" src={data.image} alt={data.text} />
+      <img className="mb-2 max-h-[5.75rem] object-contain transition-all duration-300" src={image} alt={text} />
       <img className="mb-3" src={stage} alt="stage for batteries" />
-      <figcaption className="text-NeutralN300 text-base">{data.text}</figcaption>
+      <figcaption className="text-NeutralN300 text-base">{text}</figcaption>
     </figure>
   );
-}
+};
 
 export default BatteryCategoriesCard;
