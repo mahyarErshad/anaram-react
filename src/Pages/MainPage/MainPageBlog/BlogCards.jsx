da00bc9531c9b1d048cfe23ec744af2f0ca5591c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 11:13:56 +0430

    Cleaned the code

--- src/Pages/MainPage/MainPageBlog/BlogCards.jsx
+++ src/Pages/MainPage/MainPageBlog/BlogCards.jsx
@@ -6,9 +6,9 @@ import image from "../../../assets/images/data/blog-cards/battery.jpg";
 function BlogCards() {
   return (
     <figure className={`${styles.card} transition-all duration-300 rounded-2xl max-lg:p-3 lg:p-4 bg-white border border-primary1 cursor-pointer`}>
-      <img className="h-2/4 rounded-lg duration-300" src={image} alt="" />
+      <img className="h-2/4 rounded-lg duration-300" src={image} alt="Blog Card" />
       <figcaption className="text-right text-sm font-semibold text-NeutralN600 max-lg:mt-3 mt-4">علت شارژ نشدن باتری های موبایل</figcaption>
-      <p className="text-[0.6875rem] text-right w-full leading-6 max-lg:mt-3 mt-4 max-w-[17.5rem] text-NeutralN100">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، </p>
+      <p className="text-[0.6875rem] text-right w-full leading-6 max-lg:mt-3 mt-4 max-w-[17.5rem] text-NeutralN100">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است</p>
       <div className="max-lg:mt-5 mt-8 w-full flex justify-between items-center">
         <span className="text-[0.5625rem] text-NeutralN80">۱۴۰۱/۰۷/۱۹</span>
         <div className="flex-center gap-2">
