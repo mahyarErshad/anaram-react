bfbb6b3d9b1be74edd43f5973994066140dd03db
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-19 11:25:27 +0430

    Cleaned the code

--- src/Pages/MainPage/MainPageSlider/MainPageSlider.jsx
+++ src/Pages/MainPage/MainPageSlider/MainPageSlider.jsx
@@ -1,63 +1,49 @@
 import React from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import SwiperCore, { Autoplay, Pagination, Keyboard, Navigation } from "swiper";
+import "swiper/swiper-bundle.css";
 import "./slider.css";
-
-// import required modules
-import { Autoplay, Pagination, Keyboard, Navigation } from "swiper";
-
-// image imports
 import xbox from "../../../assets/images/Sliders/Xbox.jpg";
 import light1 from "../../../assets/images/Sliders/light1.jpg";
 import light2 from "../../../assets/images/Sliders/light2.jpg";
 import { ReactComponent as PrevIcon } from "../../../assets/images/icons/prev-icon.svg";
 
-export default function MainPageSlider() {
-  const images = [xbox, light1, light2];
+SwiperCore.use([Autoplay, Pagination, Keyboard, Navigation]);
+
+const images = [xbox, light1, light2];
+
+const MainPageSlider = () => {
   return (
     <>
       <Swiper
-        pagination={{
-          clickable: true,
-          el: ".swiper-pagination",
-        }}
-        keyboard={{
-          enabled: true,
-        }}
+        className="mySwiper"
+        modules={[Autoplay, Pagination, Keyboard, Navigation]}
+        pagination={{ clickable: true, el: ".swiper-pagination" }}
+        keyboard={{ enabled: true }}
         navigation={{
           nextEl: ".main-slider-button-next",
           prevEl: ".main-slider-button-prev",
         }}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
         rewind={true}
-        modules={[Pagination, Keyboard, Autoplay, Navigation]}
-        className="mySwiper"
       >
-        {images.map((image, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <img src={image} alt={`Anaram Slider number ${index + 1}`} />
-            </SwiperSlide>
-          );
-        })}
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt={`Anaram Slider number ${index + 1}`} />
+          </SwiperSlide>
+        ))}
         <div className="pagination-navigation-wrapper w-fit flex-center gap-2">
-          <button className="main-slider-button-prev flex-center">
+          <button className="main-slider-button-next flex-center">
             <PrevIcon />
           </button>
           <div className="swiper-pagination flex-center cursor-pointer"></div>
-          <button className="main-slider-button-next flex-center">
+          <button className="main-slider-button-prev flex-center">
             <PrevIcon className="mirror" />
           </button>
         </div>
       </Swiper>
     </>
   );
-}
+};
+
+export default MainPageSlider;
