a8787eb6e63f37cf0f67d8410a31f37541d4025d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 10:10:56 +0430

    Filters component is created

--- src/Pages/ProductsPage/ProductsPage.jsx
+++ src/Pages/ProductsPage/ProductsPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function ProductsPage() {
   return (
-    <div>ProductsPage</div>
-  )
+    <section className="w-full max-lg:px-4 mx-auto max-w-[101.5625rem] flex justify-center gap-6 pt-12 pb-24">
+      <div className="w-fit">
+        <div className="rounded-2xl border border-NeutralN30 py-6 px-4 w-[288px] h-[353px] bg-white"></div>
+      </div>
+      <div className="bg-green-500 h-screen flex-grow"></div>
+    </section>
+  );
 }
 
-export default ProductsPage
+export default ProductsPage;
