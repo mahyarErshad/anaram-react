d04cbbee983dcfdfa0785aada0184fc69283aac6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 11:44:16 +0430

    AccordionHeader is styled

--- src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
+++ src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
@@ -8,27 +8,33 @@ function ProductsPageFilters() {
       <div className="rounded-2xl border border-NeutralN30 py-6 px-4 min-w-[18rem] bg-white flex items-center flex-col gap-6">
         <span className="text-sm text-NeutralN500">فیلترها</span>
         <Accordion className="w-full border-b border-NeutralN30">
-          <AccordionItem>
-            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-              تکنولوژی ساخت
-              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-            </AccordionHeader>
-            <AccordionBody>تست</AccordionBody>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-              تکنولوژی ساخت
-              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-            </AccordionHeader>
-            <AccordionBody>تست</AccordionBody>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-              تکنولوژی ساخت
-              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-            </AccordionHeader>
-            <AccordionBody>تست</AccordionBody>
-          </AccordionItem>
+          <div>
+            <AccordionItem>
+              <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+                تکنولوژی ساخت
+                <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+              </AccordionHeader>
+              <AccordionBody>تست</AccordionBody>
+            </AccordionItem>
+          </div>
+          <div>
+            <AccordionItem>
+              <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+                ولتاژ
+                <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+              </AccordionHeader>
+              <AccordionBody>تست</AccordionBody>
+            </AccordionItem>
+          </div>
+          <div>
+            <AccordionItem>
+              <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+                قیمت
+                <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+              </AccordionHeader>
+              <AccordionBody>تست</AccordionBody>
+            </AccordionItem>
+          </div>
         </Accordion>
       </div>
     </div>
