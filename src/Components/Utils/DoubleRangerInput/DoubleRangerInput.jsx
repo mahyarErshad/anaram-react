c7ee7e22c09934e85c1fc4774b6dce0b3983ab80
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 17:26:19 +0430

    Input type is number and step is 1000

--- src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
+++ src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
-import { separateNumberByThousand } from "../../../lib/function/convertToPersianDigits";
 
 function DoubleRangerInput() {
   const [minimum, setMinimum] = useState(0);
@@ -10,14 +9,14 @@ function DoubleRangerInput() {
       <div className="flex justify-between items-center">
         <span className="text-xs font-semibold text-NeutralN500">از</span>
         <div className="text-xs font-semibold text-NeutralN500 flex-center gap-3">
-          <input onChange={(e) => setMinimum(e.target.value)} dir="ltr" type="text" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" value={separateNumberByThousand(minimum)} />
+          <input onChange={(e) => setMinimum(e.target.value)} dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} value={minimum} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
       <div className="flex justify-between items-center">
         <span className="text-xs font-semibold text-NeutralN500">تا</span>
         <div className="text-xs font-semibold text-NeutralN500 flex-center gap-3">
-          <input onChange={(e) => setMaximum(e.target.value)} dir="ltr" type="text" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" value={separateNumberByThousand(maximum)} />
+          <input onChange={(e) => setMaximum(e.target.value)} dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} value={maximum} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
