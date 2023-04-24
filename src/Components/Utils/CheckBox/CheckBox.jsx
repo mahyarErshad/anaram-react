960c6bec4bcfa896bd53ec3ef813c18d246a9095
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 15:18:00 +0430

    Removed borders

--- src/Components/Utils/CheckBox/CheckBox.jsx
+++ src/Components/Utils/CheckBox/CheckBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { separateNumberByThousand } from "../../../lib/function/convertToPersianDigits";
 
 const CheckBox = ({ label, counter }) => (
-  <div className="flex w-full justify-start items-start gap-2 py-4 border-y border-NeutralN20">
+  <div className="flex w-full justify-start items-start gap-2 py-4">
     <input id={label} className="cursor-pointer mt-[0.125rem]" type="checkbox" />
     <label className="text-sm text-NeutralN500 font-semibold cursor-pointer flex gap-1" htmlFor={label}>
       <span>{label}</span>
