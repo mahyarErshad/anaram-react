4c1c6a5420854ecc03186423779500ec2ad8a9e4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 09:10:06 +0430

    Path products is ProductsPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import PageLayout from "./Pages/PageLayout/PageLayout";
 import MainPageWrapper from "./Pages/MainPage/MainPageWrapper/MainPageWrapper";
+import ProductsPage from "./Pages/ProductsPage/ProductsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<PageLayout />}>
       <Route index element={<MainPageWrapper />} />
-      <Route path="/about" element={<h1>test</h1>} />
+      <Route path="/products" element={<ProductsPage />} />
     </Route>
   )
 );
