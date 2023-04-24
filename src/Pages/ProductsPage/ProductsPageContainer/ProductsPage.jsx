87d41307d98aa5e43e32d4f230038419facefd69
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 10:15:26 +0430

    Copied the codes

--- src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
+++ src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function ProductsPage() {
   return (
     <section className="w-full max-lg:px-4 mx-auto max-w-[101.5625rem] flex justify-center gap-6 pt-12 pb-24">
-      <div className="w-fit">
-        <div className="rounded-2xl border border-NeutralN30 py-6 px-4 w-[288px] h-[353px] bg-white"></div>
-      </div>
+
       <div className="bg-green-500 h-screen flex-grow"></div>
     </section>
   );
--- src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
+++ src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function ProductsPageFilters() {
   return (
-    <div>ProductsPageFilters</div>
-  )
+    <div className="w-fit">
+      <div className="rounded-2xl border border-NeutralN30 py-6 px-4 w-[288px] h-[353px] bg-white"></div>
+    </div>
+  );
 }
 
-export default ProductsPageFilters
+export default ProductsPageFilters;
