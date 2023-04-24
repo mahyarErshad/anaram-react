6d25099a156212571b45e4a93d5e4a51c1d7cb2e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 11:32:27 +0430

    Accordion template is created

--- src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
+++ src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
+import { ReactComponent as ArrowIcon } from "../../../assets/images/icons/arrow-icon-toBottom.svg";
 
 function ProductsPageFilters() {
   return (
     <div className="w-fit">
-      <div className="rounded-2xl border border-NeutralN30 py-6 px-4 w-[288px] h-[353px] bg-white"></div>
+      <div className="rounded-2xl border border-NeutralN30 py-6 px-4 min-w-[18rem] bg-white flex items-center flex-col gap-6">
+        <span className="text-sm text-NeutralN500">فیلترها</span>
+        <Accordion className="w-full border-b border-NeutralN30">
+          <AccordionItem>
+            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+              تکنولوژی ساخت
+              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+            </AccordionHeader>
+            <AccordionBody>تست</AccordionBody>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+              تکنولوژی ساخت
+              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+            </AccordionHeader>
+            <AccordionBody>تست</AccordionBody>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+              تکنولوژی ساخت
+              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+            </AccordionHeader>
+            <AccordionBody>تست</AccordionBody>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 }
