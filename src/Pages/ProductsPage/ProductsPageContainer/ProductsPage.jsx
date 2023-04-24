c0a49bf5cc7923f49e543a29459ae73a87a151ed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 10:15:45 +0430

    Imported ProductsPageFilters

--- src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
+++ src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ProductsPageFilters from "../ProductsPageFilters/ProductsPageFilters";
 
 function ProductsPage() {
   return (
     <section className="w-full max-lg:px-4 mx-auto max-w-[101.5625rem] flex justify-center gap-6 pt-12 pb-24">
-
+      <ProductsPageFilters />
       <div className="bg-green-500 h-screen flex-grow"></div>
     </section>
   );
