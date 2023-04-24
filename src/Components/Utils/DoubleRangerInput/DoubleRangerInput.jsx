3ff4c47ed6f1ce26d3307ee9c99ae76ab25782f5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-24 15:01:14 +0430

    Imported TomanIcon

--- src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
+++ src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
 
 function DoubleRangerInput() {
   return (
-    <div>DoubleRangerInput</div>
-  )
+    <div className="flex flex-col w-full gap-6">
+      <div className="flex justify-between items-center">
+        <span className="text-xs font-semibold text-NeutralN500">از</span>
+      </div>
+    </div>
+  );
 }
 
-export default DoubleRangerInput
+export default DoubleRangerInput;
