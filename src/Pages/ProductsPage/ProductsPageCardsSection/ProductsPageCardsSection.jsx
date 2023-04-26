155068df2a4343ac21a6a7b6f1cf158563e5c7e1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 14:08:27 +0430

    Tabs are created

--- src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
+++ src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { ReactComponent as SettingsIcon } from "../../../assets/images/icons/settings-icon.svg";
-import styles from "./styles.module.css";
+import "./styles.css";
 
 function ProductsPageCardsSection() {
   return (
     <div className="w-full max-md:mt-6 md:mt-12">
-      <Tab>
-        <Tabs className="flex justify-start items-center gap-4">
+      <Tabs>
+        <TabList className="flex justify-start items-center gap-4">
           <SettingsIcon />
-          <span className="text-NeutralN500 text-sm">مرتب سازی:</span>
+          <span className="text-NeutralN500 text-sm font-semibold">مرتب سازی:</span>
           <div className="flex-center gap-6">
-            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>پرفروش‌ترین</TabList>
-            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>ارزان‌ترین</TabList>
-            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>گران‌ترین</TabList>
-            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>محبوب‌ترین</TabList>
+            <Tab className="ProductsPageCardsSection__tabs duration-300 font-semibold text-sm text-NeutralN400 cursor-pointer">پرفروش‌ترین</Tab>
+            <Tab className="ProductsPageCardsSection__tabs duration-300 font-semibold text-sm text-NeutralN400 cursor-pointer">ارزان‌ترین</Tab>
+            <Tab className="ProductsPageCardsSection__tabs duration-300 font-semibold text-sm text-NeutralN400 cursor-pointer">گران‌ترین</Tab>
+            <Tab className="ProductsPageCardsSection__tabs duration-300 font-semibold text-sm text-NeutralN400 cursor-pointer">محبوب‌ترین</Tab>
           </div>
-        </Tabs>
-        <TabPanel></TabPanel>
-      </Tab>
+        </TabList>
+        <TabPanel>
+          <h1>1</h1>
+        </TabPanel>
+        <TabPanel>
+          <h1>2</h1>
+        </TabPanel>
+        <TabPanel>
+          <h1>3</h1>
+        </TabPanel>
+        <TabPanel>
+          <h1>4</h1>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
