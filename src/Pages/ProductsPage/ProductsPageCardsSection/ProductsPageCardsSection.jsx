c9373455d16808a1d97cb5db9da7702b7f7fcd32
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 13:48:34 +0430

    Changed the settings icon

--- src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
+++ src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import {ReactComponent as SettingsIcon} from "../../../assets/images/icons/settings-icon.svg"
+import { ReactComponent as SettingsIcon } from "../../../assets/images/icons/settings-icon.svg";
 
 function ProductsPageCardsSection() {
   return (
     <div className="w-full max-md:mt-6 md:mt-12">
       <Tab>
         <Tabs>
-        <span></span>
+          <SettingsIcon />
+          <span></span>
           <TabList></TabList>
         </Tabs>
         <TabPanel></TabPanel>
