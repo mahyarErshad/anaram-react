d25c2181fede1fdde198dbb201b8ca609404e676
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 13:56:46 +0430

    Created 4 TabLists

--- src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
+++ src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { ReactComponent as SettingsIcon } from "../../../assets/images/icons/settings-icon.svg";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
 function ProductsPageCardsSection() {
   return (
@@ -10,8 +10,11 @@ function ProductsPageCardsSection() {
         <Tabs className="flex justify-start items-center gap-4">
           <SettingsIcon />
           <span className="text-NeutralN500 text-sm">مرتب سازی:</span>
-          <div>
-            <TabList className={`${styles.tabList} text-sm `}>پرفروش‌ ترین</TabList>
+          <div className="flex-center gap-6">
+            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>پرفروش‌ترین</TabList>
+            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>ارزان‌ترین</TabList>
+            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>گران‌ترین</TabList>
+            <TabList className={`${styles.tabList} text-sm text-NeutralN400 cursor-pointer`}>محبوب‌ترین</TabList>
           </div>
         </Tabs>
         <TabPanel></TabPanel>
