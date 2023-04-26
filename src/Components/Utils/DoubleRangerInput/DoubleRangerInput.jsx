f4dd7eb1afe34a035aa7ca4e2f8bdcec1cb321f3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 10:06:20 +0430

    Styled the thumb and the track

--- src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
+++ src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
@@ -4,25 +4,24 @@ import ReactSlider from "react-slider";
 import styles from "./styles.module.css";
 
 function DoubleRangerInput() {
-  const [minimum, setMinimum] = useState(0);
-  const [maximum, setMaximum] = useState(1234567);
+  const [values , setValues] = useState([])
   return (
     <div className="flex flex-col w-full gap-6 mb-6">
       <div className="flex justify-between items-center">
         <span className="text-xs font-semibold text-NeutralN500">از</span>
         <div className="text-xs font-semibold text-NeutralN500 flex-center gap-3">
-          <input onChange={(e) => setMinimum(e.target.value)} dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} value={minimum} />
+          <input dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
       <div className="flex justify-between items-center">
         <span className="text-xs font-semibold text-NeutralN500">تا</span>
         <div className="text-xs font-semibold text-NeutralN500 flex-center gap-3">
-          <input onChange={(e) => setMaximum(e.target.value)} dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} value={maximum} />
+          <input dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
-      <ReactSlider className="horizontal-slider" thumbClassName="example-thumb" trackClassName="example-track" defaultValue={[0, 100]} ariaLabel={["Lower thumb", "Upper thumb"]} ariaValuetext={(state) => `Thumb value ${state.valueNow}`} renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>} pearling minDistance={10} />
+      <ReactSlider className={styles.slider} thumbClassName={styles.thumb} trackClassName={styles.track} defaultValue={[0, 100000]} ariaLabel={["Lower thumb", "Upper thumb"]} ariaValuetext={(state) => `Thumb value ${state.valueNow}`} renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>} pearling minDistance={10} />
     </div>
   );
 }
