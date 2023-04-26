27be4994c453b1bbd7843fecb3f4cb6b0d94d6e2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 10:23:00 +0430

    Range input and number input can change the values

--- src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
+++ src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
@@ -4,24 +4,35 @@ import ReactSlider from "react-slider";
 import styles from "./styles.module.css";
 
 function DoubleRangerInput() {
-  const [values, setValues] = useState([]);
+  const [values, setValues] = useState([0, 100000]);
+
+  const handleChange = (newValues) => {
+    setValues(newValues);
+  };
+
+  const handleInputChange = (event, index) => {
+    const newValues = [...values];
+    newValues[index] = event.target.valueAsNumber;
+    setValues(newValues);
+  };
+
   return (
     <div className="flex flex-col w-full gap-6 mb-6">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between gap-2 items-center">
         <span className="text-xs font-semibold text-NeutralN500">از</span>
-        <div className="text-xs font-semibold text-NeutralN500 flex-center gap-3">
-          <input dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} />
+        <div className="text-xs font-semibold text-NeutralN500 flex-center flex-grow gap-3">
+          <input dir="ltr" type="number" className="text-base text-NeutralN500 flex-grow pt-1 focus-within:outline-none" value={values[0]} min={0} max={values[1]} step={1000} onChange={(event) => handleInputChange(event, 0)} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between gap-2 items-center">
         <span className="text-xs font-semibold text-NeutralN500">تا</span>
-        <div className="text-xs font-semibold text-NeutralN500 flex-center gap-3">
-          <input dir="ltr" type="number" className="text-base text-NeutralN500 pt-1 focus-within:outline-none" min={0} step={1000} />
+        <div className="text-xs font-semibold text-NeutralN500 flex-center flex-grow gap-3">
+          <input dir="ltr" type="number" className="text-base text-NeutralN500 flex-grow pt-1 focus-within:outline-none" value={values[1]} min={values[0]} max={100000} step={1000} onChange={(event) => handleInputChange(event, 1)} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
-      <ReactSlider className={styles.slider} thumbClassName={styles.thumb} trackClassName={styles.track} defaultValue={[0, 100000]} ariaLabel={["Lower thumb", "Upper thumb"]} pearling minDistance={10} />
+      <ReactSlider className={styles.slider} thumbClassName={styles.thumb} trackClassName={styles.track} value={values} onChange={handleChange} min={0} max={100000} ariaLabel={["Lower thumb", "Upper thumb"]} pearling minDistance={1000} />
     </div>
   );
 }
