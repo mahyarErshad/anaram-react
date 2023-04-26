7b6215d1284b2fb825070bcc9d83aeca4a22c236
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 12:27:21 +0430

    ProductsPageBatteryCategory is dynamic

--- src/Pages/ProductsPage/ProductsPageBatteryCategory/ProductsPageBatteryCategory.jsx
+++ src/Pages/ProductsPage/ProductsPageBatteryCategory/ProductsPageBatteryCategory.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import stage from "../../../assets/images/data/battery-categories/stage.png";
 
-const ProductsPageBatteryCategory = ({ data }) => {
-  const { image, text } = data;
+const ProductsPageBatteryCategory = (props) => {
+  const { image, text, active } = props;
+  const activeStyles = active ? "bg-primary1 border-primary2" : "bg-NeutralN20 border-white";
   return (
-    <figure className="group flex-center flex-col cursor-pointer max-lg:mt-6 bg-NeutralN20 w-[12rem] py-4 rounded-2xl">
+    <figure className={`group flex-center flex-col border cursor-pointer max-lg:mt-6 w-[12rem] py-4 rounded-2xl ${activeStyles}`}>
       <img className="mb-2 group-hover:-translate-y-1 max-h-[5.75rem] object-contain transition-all duration-300" src={image} alt={text} />
       <img className="mb-3" src={stage} alt="stage for batteries" />
       <figcaption className="text-NeutralN300 text-base">{text}</figcaption>
