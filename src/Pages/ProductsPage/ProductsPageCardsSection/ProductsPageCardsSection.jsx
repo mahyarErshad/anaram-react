f48611d0caea7bbee9f7cce5607b579616408d73
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 13:46:42 +0430

    Imported SettingsIcon

--- src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
+++ src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import {ReactComponent as SettingsIcon} from "../../../assets/images/icons/settings-icon.svg"
 
 function ProductsPageCardsSection() {
   return (
     <div className="w-full max-md:mt-6 md:mt-12">
       <Tab>
         <Tabs>
+        <span></span>
           <TabList></TabList>
         </Tabs>
         <TabPanel></TabPanel>
