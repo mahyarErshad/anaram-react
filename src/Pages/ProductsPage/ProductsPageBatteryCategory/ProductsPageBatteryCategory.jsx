eed8179e9fb9e47fdf62b4faf41ab741523ce2d1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-26 11:50:35 +0430

    Copied the codes

--- src/Pages/ProductsPage/ProductsPageBatteryCategory/ProductsPageBatteryCategory.jsx
+++ src/Pages/ProductsPage/ProductsPageBatteryCategory/ProductsPageBatteryCategory.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import stage from "../../../assets/images/data/battery-categories/stage.png";
+import styles from "./style.module.css";
 
-function ProductsPageBatteryCategory() {
+const BatteryCategoriesCard = ({ data }) => {
+  const { image, text } = data;
   return (
-    <div>ProductsPageBatteryCategory</div>
-  )
-}
+    <figure className={`flex-center flex-col cursor-pointer max-lg:mt-6 ${styles.card}`}>
+      <img className="mb-2 max-h-[5.75rem] object-contain transition-all duration-300" src={image} alt={text} />
+      <img className="mb-3" src={stage} alt="stage for batteries" />
+      <figcaption className="text-NeutralN300 text-base">{text}</figcaption>
+    </figure>
+  );
+};
 
-export default ProductsPageBatteryCategory
+export default BatteryCategoriesCard;
