d6666b1ec794cec62f495dd588b0125e705ec95f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 11:03:00 +0430

    Fixed a bug

--- src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
+++ src/Components/Utils/DoubleRangerInput/DoubleRangerInput.jsx
@@ -26,15 +26,15 @@ function DoubleRangerInput() {
     <div className="flex flex-col w-full gap-6 mb-6">
       <div className="flex justify-between gap-2 items-center">
         <span className="text-xs font-semibold text-NeutralN500">از</span>
-        <div className="text-xs font-semibold text-NeutralN500 flex-center flex-grow gap-3">
-          <input onBlur={handleMinBlur} dir="ltr" type="number" className="text-base text-NeutralN500 flex-grow pt-1 focus-within:outline-none" value={values[0]} min={0} max={values[1]} step={1000} onChange={(event) => handleInputChange(event, 0)} />
+        <div className="text-xs font-semibold text-NeutralN500 flex-center flex-grow gap-2">
+          <input onBlur={handleMinBlur} dir="ltr" type="number" className="text-base text-NeutralN500 w-full pt-1 focus-within:outline-none" value={values[0]} min={0} max={values[1]} step={1000} onChange={(event) => handleInputChange(event, 0)} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
       <div className="flex justify-between gap-2 items-center">
         <span className="text-xs font-semibold text-NeutralN500">تا</span>
-        <div className="text-xs font-semibold text-NeutralN500 flex-center flex-grow gap-3">
-          <input dir="ltr" type="number" className="text-base text-NeutralN500 flex-grow pt-1 focus-within:outline-none" value={values[1]} min={values[0]} max={100000} step={1000} onChange={(event) => handleInputChange(event, 1)} />
+        <div className="text-xs font-semibold text-NeutralN500 flex-center flex-grow gap-2">
+          <input dir="ltr" type="number" className="text-base text-NeutralN500 w-full pt-1 focus-within:outline-none" value={values[1]} min={values[0]} max={100000} step={1000} onChange={(event) => handleInputChange(event, 1)} />
           <TomanIcon className="w-4 h-3" />
         </div>
       </div>
