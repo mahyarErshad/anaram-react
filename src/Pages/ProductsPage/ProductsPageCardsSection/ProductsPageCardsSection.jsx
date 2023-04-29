08b1f33bdc11633f59069851040aa0401a77b66e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 09:50:17 +0430

    Using one paginate at the bottom of the page

--- src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
+++ src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCardsSection.jsx
@@ -14,6 +14,7 @@ function ProductsPageCardsSection() {
   const handlePageChange = ({ selected }) => {
     setActivePage(selected);
   };
+  
 
   return (
     <div className="w-full max-md:mt-6 md:mt-12">
@@ -31,21 +32,19 @@ function ProductsPageCardsSection() {
           {array.slice(activePage * cardsPerPage, (activePage + 1) * cardsPerPage).map((_, index) => {
             return <ProductsPageCards key={index} />;
           })}
-          <ReactPaginate pageCount={pageCount} onPageChange={handlePageChange} containerClassName={"flex justify-center mt-4"} pageClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} activeClassName={"bg-blue-500 text-white"} previousClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} nextClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} disabledClassName={"opacity-50 cursor-not-allowed"} />
         </TabPanel>
         <TabPanel className="w-full flex justify-start items-center flex-wrap gap-4 max-w-[82.75rem]">
           {array.slice(activePage * cardsPerPage, (activePage + 1) * cardsPerPage).map((_, index) => {
             return <ProductsPageCards discountCard key={index} />;
           })}
-          <ReactPaginate pageCount={pageCount} onPageChange={handlePageChange} containerClassName={"flex justify-center mt-4"} pageClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} activeClassName={"bg-blue-500 text-white"} previousClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} nextClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} disabledClassName={"opacity-50 cursor-not-allowed"} />
         </TabPanel>
         <TabPanel className="w-full flex justify-start items-center flex-wrap gap-4 max-w-[82.75rem]">
           {array.slice(activePage * cardsPerPage, (activePage + 1) * cardsPerPage).map((_, index) => {
             return <ProductsPageCards outOfStock key={index} />;
           })}
-          <ReactPaginate pageCount={pageCount} onPageChange={handlePageChange} containerClassName={"flex justify-center mt-4"} pageClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} activeClassName={"bg-blue-500 text-white"} previousClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} nextClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} disabledClassName={"opacity-50 cursor-not-allowed"} />
         </TabPanel>
       </Tabs>
+      <ReactPaginate pageCount={pageCount} onPageChange={handlePageChange} containerClassName={"flex justify-center mt-4"} pageClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} activeClassName={"bg-blue-500 text-white"} previousClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} nextClassName={"inline-block mx-2 px-3 py-2 bg-gray-300 text-gray-600 rounded-lg"} disabledClassName={"opacity-50 cursor-not-allowed"} />
     </div>
   );
 }
