365bca136914e400f002c160d18b1d2b2532e77b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 15:08:12 +0430

    ProductCardRatingSection positioned using props

--- src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCards/ProductsPageCards.jsx
+++ src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCards/ProductsPageCards.jsx
@@ -21,7 +21,7 @@ function ProductsPageCards({ discountCard, outOfStock }) {
   );
   return (
     <figure className={`${styles.card} ${discountCard && styles.after} ${outOfStock && "grayscale"} pt-3 pb-[0.875rem] max-sm:w-full sm:max-w-[15.75rem] relative px-3 rounded-lg bg-white border border-NeutralN30 grid max-sm:grid-cols-2 grid-cols-1 cursor-pointer transition-all duration-300`}>
-      <ProductCardRatingSection rating={4.5} />
+      <ProductCardRatingSection position="top-3 left-2" rating={4.5} />
       <div className="flex-center h-full mb-4">
         <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
         {discountSection}
