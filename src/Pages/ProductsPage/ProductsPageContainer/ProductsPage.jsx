46894a1c3eeb40b3e1d7ac364574e7e27d9b5882
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 13:43:26 +0430

    Working on max-width 1023.5px

--- src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
+++ src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ProductsPageFilters from "../ProductsPageFilters/ProductsPageFilters";
 import ProductsPageBatteryCategory from "../ProductsPageBatteryCategory/ProductsPageBatteryCategory";
 import ProductsPageCardsSection from "../ProductsPageCardsSection/ProductsPageCardsSection";
-import styles from "./layout.module.css"
+import styles from "./layout.module.css";
 
 function ProductsPage() {
   return (
-    <section className="w-full max-w-[103.5625rem] px-4 pt-12 pb-24 mx-auto test-grid">
+    <section className={`w-full max-w-[103.5625rem] px-4 pt-12 pb-24 mx-auto ${styles.container}`}>
       <ProductsPageFilters />
       <div className="w-full pt-6 overflow-x-hidden">
         <ProductsPageBatteryCategory />
