35fb226b54d4ccf965ce99afb4a63d20f1d845d5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 14:19:52 +0430

    Added border radius

--- src/Components/Utils/Buttons/ProductPageFilterButton/ProductPageFilterButton.jsx
+++ src/Components/Utils/Buttons/ProductPageFilterButton/ProductPageFilterButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ProductPageFilterButton({ className, icon, text }) {
   return (
-    <button className={`${className} p-2 flex justify-start items-center gap-3 bg-white border border-NeutralN20`}>
+    <button className={`${className} p-2 flex justify-start items-center gap-3 bg-white border border-NeutralN20 rounded-lg`}>
       {icon}
       <span className="text-xs font-semibold text-NeutralN500">{text}</span>
     </button>
