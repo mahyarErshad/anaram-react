4c8c53409bbe77aba7ede182ca141637494811bb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 13:59:34 +0430

    Space between slides is responsive

--- src/Pages/ProductsPage/ProductsPageBatteryCategory/ProductsPageBatteryCategory.jsx
+++ src/Pages/ProductsPage/ProductsPageBatteryCategory/ProductsPageBatteryCategory.jsx
@@ -7,9 +7,9 @@ import ProductsPageBatteryCategoryCard from "./ProductsPageBatteryCategoryCard";
 function ProductsPageBatteryCategory() {
   return (
     <div className="w-full px-2">
-      <Swiper slidesPerView="auto" spaceBetween={28} modules={[FreeMode]} freeMode className="mySwiper">
+      <Swiper slidesPerView="auto" modules={[FreeMode]} freeMode className="mySwiper">
         {BatteryCategoriesData.map((item, index) => (
-          <SwiperSlide style={{backgroundColor: "transparent"}} key={index}>
+          <SwiperSlide className="lg:mr-7 md:mr-4 sm:mr-2" style={{ backgroundColor: "transparent" }} key={index}>
             <ProductsPageBatteryCategoryCard active={item.active} image={item.image} text={item.text} />
           </SwiperSlide>
         ))}
