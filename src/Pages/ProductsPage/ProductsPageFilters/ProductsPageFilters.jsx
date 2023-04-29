cb6f0723aaacb11d8444a22826fddcada75764f0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 13:48:20 +0430

    ProductsPageFilters is sticky

--- src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
+++ src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
@@ -6,52 +6,52 @@ import DoubleRangerInput from "../../../Components/Utils/DoubleRangerInput/Doubl
 
 function ProductsPageFilters() {
   return (
-      <div className="rounded-2xl border border-NeutralN30 h-fit py-6 px-4 w-[18rem] bg-white flex items-center flex-col gap-6 max-lg:hidden">
-        <span className="text-sm text-NeutralN500">فیلترها</span>
-        <Accordion className="w-full border-b border-NeutralN30">
-          <div>
-            <AccordionItem>
-              <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-                تکنولوژی ساخت
-                <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-              </AccordionHeader>
-              <AccordionBody>
-                <CheckBox label="آلکاین" counter={24} />
-                <CheckBox label="روی کربن" counter={14} />
-              </AccordionBody>
-            </AccordionItem>
-          </div>
-          <div>
-            <AccordionItem>
-              <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-                ولتاژ
-                <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-              </AccordionHeader>
-              <AccordionBody>
-                <CheckBox label="۲.۱ ولت" counter={18} />
-                <CheckBox label="۳.۱ ولت" counter={5} />
-              </AccordionBody>
-            </AccordionItem>
-          </div>
-          <div>
-            <AccordionItem>
-              <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-                قیمت
-                <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-              </AccordionHeader>
-              <AccordionBody>
-                <DoubleRangerInput />
-              </AccordionBody>
-            </AccordionItem>
-          </div>
-        </Accordion>
-        <div className="flex w-full justify-start items-start gap-2">
-          <input id="exist-only" className="cursor-pointer" type="checkbox" />
-          <label className="text-xs text-primary8 font-semibold cursor-pointer" htmlFor="exist-only">
-            فقط کالا های موجود را نمایش بده
-          </label>
+    <div className="sticky top-0 w-[18rem] h-fit py-6 px-4 bg-white rounded-2xl border border-NeutralN30 hidden lg:block">
+      <span className="text-sm text-NeutralN500">فیلترها</span>
+      <Accordion className="w-full border-b border-NeutralN30">
+        <div>
+          <AccordionItem>
+            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+              تکنولوژی ساخت
+              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+            </AccordionHeader>
+            <AccordionBody>
+              <CheckBox label="آلکاین" counter={24} />
+              <CheckBox label="روی کربن" counter={14} />
+            </AccordionBody>
+          </AccordionItem>
         </div>
+        <div>
+          <AccordionItem>
+            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+              ولتاژ
+              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+            </AccordionHeader>
+            <AccordionBody>
+              <CheckBox label="۲.۱ ولت" counter={18} />
+              <CheckBox label="۳.۱ ولت" counter={5} />
+            </AccordionBody>
+          </AccordionItem>
+        </div>
+        <div>
+          <AccordionItem>
+            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
+              قیمت
+              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
+            </AccordionHeader>
+            <AccordionBody>
+              <DoubleRangerInput />
+            </AccordionBody>
+          </AccordionItem>
+        </div>
+      </Accordion>
+      <div className="flex w-full justify-start items-start gap-2">
+        <input id="exist-only" className="cursor-pointer" type="checkbox" />
+        <label className="text-xs text-primary8 font-semibold cursor-pointer" htmlFor="exist-only">
+          فقط کالا های موجود را نمایش بده
+        </label>
       </div>
+    </div>
   );
 }
 
