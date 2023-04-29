d1c0587790efdb61be56ddcb731aaad6c07507ee
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-29 14:53:53 +0430

    Component is responsive on md

--- src/Components/Utils/Buttons/ProductPageFilterButton/ProductPageFilterButton.jsx
+++ src/Components/Utils/Buttons/ProductPageFilterButton/ProductPageFilterButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function ProductPageFilterButton({ className, icon, text }) {
   return (
-    <button className={`${className} p-2 flex justify-start items-center gap-3 bg-white border border-NeutralN20 rounded-lg lg:hidden`}>
+    <button className={`${className} p-2 flex justify-start items-center gap-3 bg-white border border-NeutralN20 rounded-lg lg:hidden max-sm:flex-grow`}>
       {icon}
-      <span className="text-xs font-semibold text-NeutralN500 ml-[4.3125rem]">{text}</span>
+      <span className="text-xs font-semibold text-NeutralN500 sm:ml-[4.3125rem] whitespace-nowrap">{text}</span>
     </button>
   );
 }
