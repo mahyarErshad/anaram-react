578394be0d41e42e680ab6bc11ea1a2afdb1aef4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-30 10:48:31 +0430

    Flex center class uses @apply

--- src/Components/ProductPageModal/ProductPageModal.jsx
+++ src/Components/ProductPageModal/ProductPageModal.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function ProductPageModal() {
-  return (
-    <section className='fixed top-0 right-0 left-0 bottom-0 p-4 bg-white border border-NeutralN20 rounded-md'>
-    
-    </section>
-  )
+  return <section className="fixed top-0 right-0 left-0 bottom-0 p-4 bg-white border border-NeutralN20 rounded-md"></section>;
 }
 
-export default ProductPageModal
+export default ProductPageModal;
