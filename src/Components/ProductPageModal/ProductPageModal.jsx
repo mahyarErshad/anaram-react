7c163f165dde60c88e581bacb7b86d8c5d228942
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-30 10:40:53 +0430

    Section is styled

--- src/Components/ProductPageModal/ProductPageModal.jsx
+++ src/Components/ProductPageModal/ProductPageModal.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function ProductPageModal() {
   return (
-    <div>ProductPageModal</div>
+    <section className='fixed top-0 right-0 left-0 bottom-0 p-4 bg-white border border-NeutralN20 rounded-md'>
+    
+    </section>
   )
 }
 
