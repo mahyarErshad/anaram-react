b12098ba87b588a4cc10d137d1fb09ba92ae0d7d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-30 12:07:56 +0430

    Open and close states are defined

--- src/Components/ProductPageModal/ProductPageModal.jsx
+++ src/Components/ProductPageModal/ProductPageModal.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { ReactComponent as CloseIcon } from "../../assets/images/icons/close-icon.svg";
 
-function ProductPageModal({ icon, title, element }) {
+function ProductPageModal({ icon, title, element, open, setOpen }) {
+  const openState = open ? "top-0" : "top-[100%]";
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <section className="fixed w-screen h-screen top-0 right-0 left-0 bottom-0 p-7 bg-white lg:hidden z-50">
+    <section className={`fixed ${openState} w-screen h-screen right-0 left-0 p-7 bg-white lg:hidden z-50 duration-300`}>
       <div className="w-full h-full p-4 border border-NeutralN20 rounded-5px flex flex-col">
         <div className="flex justify-between items-center mb-10">
           <div className="flex-center gap-3">
             {icon}
             <span className="max-md:text-xs md:text-base font-semibold text-NeutralN500">{title}</span>
           </div>
-          <button className="p-2">
+          <button onClick={handleClose} className="p-2">
             <CloseIcon className="md:w-10 md:h-10" />
           </button>
         </div>
