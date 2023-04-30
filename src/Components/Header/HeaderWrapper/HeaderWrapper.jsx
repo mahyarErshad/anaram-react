03ca34e2569c074c451a3b1d9eaa37e68c157bb5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-30 15:38:51 +0430

    Removed codes that leading to bugs

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import HeaderActionSection from "../HeaderActionSection/HeaderActionSection";
 import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
 import SideBarMenu from "../SideBarMenu/SideBarMenu";
-import "./HeaderWrapper.css";
 
-function HeaderWrapper({ isFixed }) {
+function HeaderWrapper() {
   const [sideBarOpen, setSideBarOpen] = useState(false);
-
   return (
-    <header className={`pt-4 z-50 px-4 2xl:px-16 flex flex-col max-md:gap-0 md:gap-6 bg-white ${isFixed ? "fixed top-0 w-full header-fixed" : ""}`}>
+    <header className="pt-4 px-4 2xl:px-16 flex flex-col max-md:gap-0 md:gap-6 bg-white">
       <HeaderActionSection setSideBarOpen={setSideBarOpen} />
       <HeaderNavBars />
       <SideBarMenu sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
--- src/Pages/PageLayout/PageLayout.jsx
+++ src/Pages/PageLayout/PageLayout.jsx
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import HeaderWrapper from "../../Components/Header/HeaderWrapper/HeaderWrapper";
 import FooterContainer from "../../Components/Footer/FooterContainer/FooterContainer";
 import { Outlet } from "react-router-dom";
 
 function PageLayout() {
-  const [isFixed, setIsFixed] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.pageYOffset > 600) {
-        setIsFixed(true);
-      } else {
-        setIsFixed(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <>
-      <HeaderWrapper isFixed={isFixed} />
+      <HeaderWrapper />
       <main>
         <Outlet />
       </main>
