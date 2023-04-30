5ba6630e544522d7a80a44ca267dde4f979f6c71
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-30 10:51:37 +0430

    Container class is so much shorter using @apply method

--- src/Components/ProductPageModal/ProductPageModal.jsx
+++ src/Components/ProductPageModal/ProductPageModal.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function ProductPageModal() {
-  return <section className="fixed top-0 right-0 left-0 bottom-0 p-4 bg-white border border-NeutralN20 rounded-md"></section>;
+  return <section className="fixed top-0 right-0 left-0 bottom-0 p-4 bg-white border border-NeutralN20 rounded-5px">
+    
+  </section>;
 }
 
 export default ProductPageModal;
