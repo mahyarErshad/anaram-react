ba7602b2af5bef2d8702d06662587215fa06e740
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-30 14:59:42 +0430

    Moved sticky one level up

--- src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
+++ src/Components/Header/HeaderWrapper/HeaderWrapper.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import HeaderActionSection from "../HeaderActionSection/HeaderActionSection";
 import HeaderNavBars from "../HeaderNavBars/HeaderNavBars";
 import SideBarMenu from "../SideBarMenu/SideBarMenu";
+import { useEffect } from "react";
 
-function HeaderWrapper() {
-  const [sideBarOpen, setSideBarOpen] = useState(false)
+function HeaderWrapper({stickyHeader}) {
+  const [sideBarOpen, setSideBarOpen] = useState(false);
+
   return (
-    <header className="pt-4 px-4 2xl:px-16 flex-col max-md:gap-0 md:gap-6 bg-white">
+    <header className={`pt-4 px-4 2xl:px-16 flex flex-col max-md:gap-0 md:gap-6 bg-white duration-300 z-50`}>
       <HeaderActionSection setSideBarOpen={setSideBarOpen} />
       <HeaderNavBars />
       <SideBarMenu sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
--- src/Pages/PageLayout/PageLayout.jsx
+++ src/Pages/PageLayout/PageLayout.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeaderWrapper from "../../Components/Header/HeaderWrapper/HeaderWrapper";
 import FooterContainer from "../../Components/Footer/FooterContainer/FooterContainer";
 import { Outlet } from "react-router-dom";
 
 function PageLayout() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }; // eslint-disable-next-line
+  }, []);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 600) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+  console.log(sticky);
   return (
     <>
       <HeaderWrapper />
