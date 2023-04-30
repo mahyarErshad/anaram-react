6d17f6670c7e7e879bc35374e522a770f2863a26
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-30 14:22:13 +0430

    ModalOrders is done

--- src/Components/ProductPageModal/ModalOrders.jsx
+++ src/Components/ProductPageModal/ModalOrders.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import { useState } from "react";
 
 function ModalOrders() {
-  return (
-    <section className='w-full'>
-        <ul className='w-full flex-col'>
+  const [selectedItem, setSelectedItem] = useState(0);
 
-        </ul>
+  const items = ["جدیدترین", "ارزان ترین", "گران ترین", "پربازدید ترین", "پرفروش ترین"];
+
+  const handleClick = (index) => {
+    setSelectedItem(index);
+  };
+
+  return (
+    <section className="w-full">
+      <ul className="w-full flex-col gap-2">
+        {items.map((item, index) => (
+          <li key={index} className={`${index === selectedItem ? "text-primary6 bg-primary1" : "text-NeutralN90 bg-transparent"} rounded-sm px-3 py-4 text-right w-full text-xs font-semibold duration-300`} onClick={() => handleClick(index)}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </section>
-  )
+  );
 }
 
-export default ModalOrders
+export default ModalOrders;
