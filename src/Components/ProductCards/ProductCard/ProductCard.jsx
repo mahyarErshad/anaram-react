c5cb6577602e4ee84ffcff2d681c741ee90674ed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 11:18:39 +0430

    Added Link to Product card

--- src/Components/ProductCards/ProductCard/ProductCard.jsx
+++ src/Components/ProductCards/ProductCard/ProductCard.jsx
@@ -11,6 +11,7 @@ import ProductCardRatingSection from "../ProductCardComponents/ProductCardRating
 import ProductCardDiscountSection from "../ProductCardComponents/ProductCardDiscountSection/ProductCardDiscountSection";
 import ProductCardTitleSection from "../ProductCardComponents/ProductCardTitleSection/ProductCardTitleSection";
 import ProductCardTimeCounter from "../ProductCardComponents/ProductCardTimeCounter/ProductCardTimeCounter";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ discountCard }) => {
   const discountSection = discountCard && <ProductCardDiscountSection percentage={50} />;
@@ -22,28 +23,30 @@ const ProductCard = ({ discountCard }) => {
   );
 
   return (
-    <figure className={`${styles.card} ${discountCard && styles.after} pt-3 pb-[0.875rem] relative px-3 rounded-lg bg-white border border-NeutralN30 flex-col cursor-pointer transition-all duration-300`}>
-      <div className="flex-center h-2/4 mb-4 relative">
-        <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
-        {discountSection}
-        <ProductCardRatingSection rating={4.5} />
-      </div>
-      <ProductCardTitleSection title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" />
-      {priceSection}
-      <div className="flex-center gap-1 w-full">
-        <PrimaryButton addToCartButton text="افزودن به سبد" icon={<CartIcon className="w-4 h-4" stroke="white" />} />
-        <SmallButton className="p-2 bg-NeutralN20" icon={<HeartIcon />} />
-        <SmallButton className="p-2 bg-NeutralN20" icon={<CompareArrowsIcon />} />
-      </div>
-      {discountCard && (
-        <div className="flex-center gap-0 bg-NeutralN20 rounded-[4px] py-1 mb-1 mt-3">
-          <ProductCardTimeCounter number={6} text="ثانیه" />
-          <ProductCardTimeCounter number={24} text="دقیقه" />
-          <ProductCardTimeCounter number={12} text="ساعت" />
-          <ProductCardTimeCounter number={2} text="روز" />
+    <Link to="/product/id-1234">
+      <figure className={`${styles.card} ${discountCard && styles.after} pt-3 pb-[0.875rem] relative px-3 rounded-lg bg-white border border-NeutralN30 flex-col cursor-pointer transition-all duration-300`}>
+        <div className="flex-center h-2/4 mb-4 relative">
+          <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
+          {discountSection}
+          <ProductCardRatingSection rating={4.5} />
         </div>
-      )}
-    </figure>
+        <ProductCardTitleSection title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" />
+        {priceSection}
+        <div className="flex-center gap-1 w-full">
+          <PrimaryButton addToCartButton text="افزودن به سبد" icon={<CartIcon className="w-4 h-4" stroke="white" />} />
+          <SmallButton className="p-2 bg-NeutralN20" icon={<HeartIcon />} />
+          <SmallButton className="p-2 bg-NeutralN20" icon={<CompareArrowsIcon />} />
+        </div>
+        {discountCard && (
+          <div className="flex-center gap-0 bg-NeutralN20 rounded-[4px] py-1 mb-1 mt-3">
+            <ProductCardTimeCounter number={6} text="ثانیه" />
+            <ProductCardTimeCounter number={24} text="دقیقه" />
+            <ProductCardTimeCounter number={12} text="ساعت" />
+            <ProductCardTimeCounter number={2} text="روز" />
+          </div>
+        )}
+      </figure>
+    </Link>
   );
 };
 
