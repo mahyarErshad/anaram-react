01a1942514a4c43856c90dff04f54a68c8d23de4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 14:39:41 +0430

    Using ModalFilter component for having a cleaner code

--- src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
+++ src/Pages/ProductsPage/ProductsPageFilters/ProductsPageFilters.jsx
@@ -3,48 +3,13 @@ import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-
 import { ReactComponent as ArrowIcon } from "../../../assets/images/icons/arrow-icon-toBottom.svg";
 import CheckBox from "../../../Components/Utils/CheckBox/CheckBox";
 import DoubleRangerInput from "../../../Components/Utils/DoubleRangerInput/DoubleRangerInput";
+import ModalFilter from "../../../Components/ProductPageModal/ModalFilters";
 
 function ProductsPageFilters() {
   return (
     <div className="sticky top-6 w-[18rem] h-fit py-6 px-4 bg-white rounded-2xl border border-NeutralN30 hidden lg:block space-y-6">
       <div className="text-sm text-NeutralN500 text-center font-semibold">فیلترها</div>
-      <Accordion className="w-full border-b border-NeutralN30">
-        <div>
-          <AccordionItem>
-            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-              تکنولوژی ساخت
-              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-            </AccordionHeader>
-            <AccordionBody>
-              <CheckBox label="آلکاین" counter={24} />
-              <CheckBox label="روی کربن" counter={14} />
-            </AccordionBody>
-          </AccordionItem>
-        </div>
-        <div>
-          <AccordionItem>
-            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-              ولتاژ
-              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-            </AccordionHeader>
-            <AccordionBody>
-              <CheckBox label="۲.۱ ولت" counter={18} />
-              <CheckBox label="۳.۱ ولت" counter={5} />
-            </AccordionBody>
-          </AccordionItem>
-        </div>
-        <div>
-          <AccordionItem>
-            <AccordionHeader className="w-full flex justify-between items-center py-6 border-t border-NeutralN30">
-              قیمت
-              <ArrowIcon className="stroke-NeutralN100 w-3 h-3" />
-            </AccordionHeader>
-            <AccordionBody>
-              <DoubleRangerInput />
-            </AccordionBody>
-          </AccordionItem>
-        </div>
-      </Accordion>
+      <ModalFilter />
       <div className="flex w-full justify-start items-start gap-2">
         <input id="exist-only" className="cursor-pointer" type="checkbox" />
         <label className="text-xs text-primary8 font-semibold cursor-pointer" htmlFor="exist-only">
--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function SingleProductDetailSection() {
   return (
-    <section className='large-container px-6 pt-6 pb-8 grid'>
-
+    <section className='large-container px-6 pt-6 pb-8 flex-col gap-4'>
+    <div>breadCrumb</div>
+    <div className='grid grid-cols-3'>
+    
+    </div>
     </section>
   )
 }
