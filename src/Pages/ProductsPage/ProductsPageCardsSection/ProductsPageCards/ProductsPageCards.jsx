07a2d798b17d3f4e7754faaf4df07be6b38f445e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 11:28:00 +0430

    Passed goToTop to the Link

--- src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCards/ProductsPageCards.jsx
+++ src/Pages/ProductsPage/ProductsPageCardsSection/ProductsPageCards/ProductsPageCards.jsx
@@ -11,6 +11,7 @@ import { ReactComponent as HeartIcon } from "../../../../assets/images/icons/hea
 import { ReactComponent as CompareArrowsIcon } from "../../../../assets/images/icons/compare-arrows.svg";
 import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
+import goToTop from "../../../../lib/function/goToTop";
 
 function ProductsPageCards({ discountCard, outOfStock }) {
   const discountSection = discountCard && <ProductCardDiscountSection percentage={50} />;
@@ -21,7 +22,7 @@ function ProductsPageCards({ discountCard, outOfStock }) {
     </div>
   );
   return (
-    <Link to="/product/id-1234">
+    <Link onClick={goToTop} to="/product/id-1234">
       <figure className={`${styles.card} ${discountCard && styles.after} ${outOfStock && "grayscale"} pt-3 pb-[0.875rem] max-sm:w-full sm:max-w-[15.75rem] relative px-3 rounded-lg bg-white border border-NeutralN30 grid sm:grid-cols-1 cursor-pointer transition-all duration-300`}>
         <ProductCardRatingSection position="top-3 left-2" rating={4.5} />
         <div className="flex-center h-full mb-4 max-sm:col-span-1 max-sm:row-start-1 max-sm:row-end-5">
