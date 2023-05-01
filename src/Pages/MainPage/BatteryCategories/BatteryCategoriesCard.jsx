5d98d49fae394ed26dc73c4d9b6a424c30b7cb44
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 11:19:54 +0430

    Added Link

--- src/Pages/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
+++ src/Pages/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import stage from "../../../assets/images/data/battery-categories/stage.png";
 import styles from "./style.module.css";
+import { Link } from "react-router-dom";
 
 const BatteryCategoriesCard = ({ data }) => {
   const { image, text } = data;
   return (
-    <figure className={`flex-center flex-col cursor-pointer max-lg:mt-6 ${styles.card}`}>
-      <img className="mb-2 max-h-[5.75rem] object-contain transition-all duration-300" src={image} alt={text} />
-      <img className="mb-3" src={stage} alt="stage for batteries" />
-      <figcaption className="text-NeutralN300 text-base">{text}</figcaption>
-    </figure>
+    <Link to="/products">
+      <figure className={`flex-center flex-col cursor-pointer max-lg:mt-6 ${styles.card}`}>
+        <img className="mb-2 max-h-[5.75rem] object-contain transition-all duration-300" src={image} alt={text} />
+        <img className="mb-3" src={stage} alt="stage for batteries" />
+        <figcaption className="text-NeutralN300 text-base">{text}</figcaption>
+      </figure>
+    </Link>
   );
 };
 
