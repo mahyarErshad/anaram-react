cdd367e8d74689f5bc0500f3d8f6a954319a1eb3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 11:12:36 +0430

    Root /product/:id leads to SingleProductContainer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import PageLayout from "./Pages/PageLayout/PageLayout";
 import MainPageWrapper from "./Pages/MainPage/MainPageWrapper/MainPageWrapper";
 import ProductsPage from "./Pages/ProductsPage/ProductsPageContainer/ProductsPage";
+import SingleProductContainer from "./Pages/SingleProduct/SingleProductContainer/SingleProductContainer";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<PageLayout />}>
       <Route index element={<MainPageWrapper />} />
       <Route path="/products" element={<ProductsPage />} />
+      <Route path="/product/:id" element={<SingleProductContainer />} />
     </Route>
   )
 );
