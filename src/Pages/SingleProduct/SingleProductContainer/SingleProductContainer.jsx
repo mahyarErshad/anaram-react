709fc2037893155fb735259e7134f2b8f6ea2960
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 13:57:59 +0430

    Added padding on y axis

--- src/Pages/SingleProduct/SingleProductContainer/SingleProductContainer.jsx
+++ src/Pages/SingleProduct/SingleProductContainer/SingleProductContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SingleProductContainer() {
   return (
-    <section className='flex-col gap-6 w-full px-4'>
+    <section className='flex-col gap-6 w-full px-4 pt-10 pb-20'>
         
     </section>
   )
