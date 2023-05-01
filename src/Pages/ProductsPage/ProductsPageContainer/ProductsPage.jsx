1cb756ea867a737946fd794fa4e651b0dbfa7305
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 10:02:13 +0430

    Using BatteryChargingComponent in ProductsPage

--- src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
+++ src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
@@ -3,18 +3,18 @@ import ProductsPageFilters from "../ProductsPageFilters/ProductsPageFilters";
 import ProductsPageBatteryCategory from "../ProductsPageBatteryCategory/ProductsPageBatteryCategory";
 import ProductsPageCardsSection from "../ProductsPageCardsSection/ProductsPageCardsSection";
 import styles from "./layout.module.css";
+import BatteryChargingComponent from "../../../Components/BatteryChargingComponent/BatteryChargingComponent";
 
 function ProductsPage() {
   return (
-    <>
-      <section className={`w-full max-w-[103.5625rem] px-4 lg:pt-12 pb-24 mx-auto ${styles.container}`}>
-        <ProductsPageFilters />
-        <div className="w-full pt-6 overflow-x-hidden">
-          <ProductsPageBatteryCategory />
-          <ProductsPageCardsSection />
-        </div>
-      </section>
-    </>
+    <section className={`w-full max-w-[103.5625rem] px-4 lg:pt-12 pb-24 mx-auto ${styles.container}`}>
+      <ProductsPageFilters />
+      <div className="w-full pt-6 overflow-x-hidden">
+        <ProductsPageBatteryCategory />
+        <ProductsPageCardsSection />
+      </div>
+      <BatteryChargingComponent />
+    </section>
   );
 }
 
