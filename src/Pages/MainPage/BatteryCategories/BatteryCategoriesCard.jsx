4d090e424800df1c2173dff7561e050972b627a1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 11:29:00 +0430

    Passed goToTop to the Link

--- src/Pages/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
+++ src/Pages/MainPage/BatteryCategories/BatteryCategoriesCard.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import stage from "../../../assets/images/data/battery-categories/stage.png";
 import styles from "./style.module.css";
 import { Link } from "react-router-dom";
+import goToTop from "../../../lib/function/goToTop";
 
 const BatteryCategoriesCard = ({ data }) => {
   const { image, text } = data;
   return (
-    <Link to="/products">
+    <Link onClick={goToTop} to="/products">
       <figure className={`flex-center flex-col cursor-pointer max-lg:mt-6 ${styles.card}`}>
         <img className="mb-2 max-h-[5.75rem] object-contain transition-all duration-300" src={image} alt={text} />
         <img className="mb-3" src={stage} alt="stage for batteries" />
