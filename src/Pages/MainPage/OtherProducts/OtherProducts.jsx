e03c93fc2480e7c257c67e47003a33e0d7dea310
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 11:25:42 +0430

    Passed To the Link

--- src/Pages/MainPage/OtherProducts/OtherProducts.jsx
+++ src/Pages/MainPage/OtherProducts/OtherProducts.jsx
@@ -4,6 +4,7 @@ import charger from "../../../assets/images/data/other-products/charger.png";
 import powerBank from "../../../assets/images/data/other-products/power-bank.png";
 import otherProductsImage from "../../../assets/images/data/other-products/other-products.png";
 import { Link } from "react-router-dom";
+import goToTop from "../../../lib/function/goToTop";
 
 const data = [flashLight, charger, powerBank, otherProductsImage];
 
@@ -15,7 +16,7 @@ function OtherProducts() {
       </h2>
       <div className="grid grid-cols-2 gap-y-4 justify-items-center max-w-[82.5rem] mx-auto lg:grid-cols-4 max-lg:max-w-[42.5rem] max-lg:grid-cols-2">
         {data.map((image, index) => (
-          <Link key={index} to="/products">
+          <Link onClick={goToTop} key={index} to="/products">
             <img src={image} alt="products category" />
           </Link>
         ))}
