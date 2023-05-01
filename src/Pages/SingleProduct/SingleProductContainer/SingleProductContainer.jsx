aba0c355c6e80b1b0d659dd038ab31cb30f59aab
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 14:01:13 +0430

    Imported SingleProductDetailSection

--- src/Pages/SingleProduct/SingleProductContainer/SingleProductContainer.jsx
+++ src/Pages/SingleProduct/SingleProductContainer/SingleProductContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SingleProductDetailSection from '../SingleProductDetailSection/SingleProductDetailSection'
 
 function SingleProductContainer() {
   return (
     <section className='flex-col gap-6 w-full px-4 pt-10 pb-20'>
-        
+        <SingleProductDetailSection />
     </section>
   )
 }
