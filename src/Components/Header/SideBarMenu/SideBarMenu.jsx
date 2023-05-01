d31e4ea2a280232c379b9ac958c4714f5731dd82
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 11:45:29 +0430

    ded Link to menus

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as BatteryIcon } from "../../../assets/images/data/sideb
 import headerNavLinksData from "../../../lib/HeaderNavLinksData/HeaderNavLinksData";
 import { ReactComponent as ArrowIcon } from "../../../assets/images/icons/arrow-icon-toBottom.svg";
 import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
+import goToTop from "../../../lib/function/goToTop";
 
 function SideBarMenu({ sideBarOpen, setSideBarOpen }) {
   const handleOutsideClick = () => {
@@ -14,16 +16,22 @@ function SideBarMenu({ sideBarOpen, setSideBarOpen }) {
   const handleInsideClick = (event) => {
     event.stopPropagation();
   };
+  const handleMenuClick = () => {
+    goToTop();
+    setSideBarOpen(false);
+  };
 
   return (
     <section onClick={handleOutsideClick} className={`fixed top-0 right-0 w-screen h-full min-h-screen bg-black ${sideBarOpen ? "bg-opacity-[0.25]" : "bg-opacity-0 pointer-events-none"} lg:hidden duration-300 z-50`}>
       <aside onClick={handleInsideClick} className={`absolute h-full overflow-y-auto top-0 ${sideBarOpen ? "right-0" : "-right-[100%]"} bg-white py-16 px-8 flex justify-center h-screen min-w-2/4 duration-300`}>
         <Accordion className="flex-col items-center gap-2 h-full">
           <AccordionItem>
-            <AccordionHeader className={`${styles["accordion-header"]} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 border border-transparent hover:border-primary2 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] duration-300`}>
-              <HomeIcon className="ml-[0.625rem] stroke-inherit" />
-              <h3 className="text-xs text-inherit font-semibold">صفحه اصلی</h3>
-            </AccordionHeader>
+            <Link onClick={handleMenuClick} to="/">
+              <AccordionHeader className={`${styles["accordion-header"]} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 border border-transparent hover:border-primary2 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] duration-300`}>
+                <HomeIcon className="ml-[0.625rem] stroke-inherit" />
+                <h3 className="text-xs text-inherit font-semibold">صفحه اصلی</h3>
+              </AccordionHeader>
+            </Link>
           </AccordionItem>
           {headerNavLinksData.map((item, index) => (
             <AccordionItem key={index}>
@@ -36,9 +44,11 @@ function SideBarMenu({ sideBarOpen, setSideBarOpen }) {
                 <AccordionBody className="bg-primary1 w-full rounded-b-2xl -mt-2 px-5 flex-col items-center gap-2">
                   <span className="w-full h-[1px] bg-NeutralN20 mb-2"></span>
                   {item.subMenu.map((subMenuItem, subMenuIndex) => (
-                    <span key={subMenuIndex} className={`${styles.navCards} bg-NeutralN10 rounded-2xl cursor-pointer w-full py-3 flex-center text-xs text-[#616161]`}>
-                      {subMenuItem}
-                    </span>
+                    <Link className="w-full" onClick={handleMenuClick} to="/products">
+                      <span key={subMenuIndex} className={`${styles.navCards} bg-NeutralN10 rounded-2xl cursor-pointer w-full py-3 flex-center text-xs text-[#616161]`}>
+                        {subMenuItem}
+                      </span>
+                    </Link>
                   ))}
                 </AccordionBody>
               )}
