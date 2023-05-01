0968b161d3604e2f444fd0861f9702c1b49791fd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-01 10:00:58 +0430

    Moved BatteryCharging Component to Components Folder

--- src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
+++ src/Pages/ProductsPage/ProductsPageContainer/ProductsPage.jsx
@@ -6,13 +6,15 @@ import styles from "./layout.module.css";
 
 function ProductsPage() {
   return (
-    <section className={`w-full max-w-[103.5625rem] px-4 lg:pt-12 pb-24 mx-auto ${styles.container}`}>
-      <ProductsPageFilters />
-      <div className="w-full pt-6 overflow-x-hidden">
-        <ProductsPageBatteryCategory />
-        <ProductsPageCardsSection />
-      </div>
-    </section>
+    <>
+      <section className={`w-full max-w-[103.5625rem] px-4 lg:pt-12 pb-24 mx-auto ${styles.container}`}>
+        <ProductsPageFilters />
+        <div className="w-full pt-6 overflow-x-hidden">
+          <ProductsPageBatteryCategory />
+          <ProductsPageCardsSection />
+        </div>
+      </section>
+    </>
   );
 }
 
