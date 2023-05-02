eb30eeb623313f957cb2465897e412dccdd15c37
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-02 10:20:00 +0430

    Imported BreadCrumbs

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
+import BreadCrumbs from "../../../Components/BreadCrumbs/BreadCrumbs";
 
 function SingleProductDetailSection() {
   return (
-    <section className='large-container px-6 pt-6 pb-8 flex-col gap-4'>
-    <div>breadCrumb</div>
-    <div className='grid grid-cols-3'>
-    
-    </div>
+    <section className="large-container px-6 pt-6 pb-8 flex-col gap-4">
+      <BreadCrumbs />
+      <div className="grid grid-cols-3"></div>
     </section>
-  )
+  );
 }
 
-export default SingleProductDetailSection
+export default SingleProductDetailSection;
