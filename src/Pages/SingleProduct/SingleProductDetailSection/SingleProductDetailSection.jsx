c90524a7365e5e2dc655bc5cb9668bc0d8ed04f9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-02 10:44:57 +0430

    Removed the component

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import BreadCrumbs from "../../../Components/BreadCrumbs/BreadCrumbs";
 
 function SingleProductDetailSection() {
   return (
     <section className="large-container px-6 pt-6 pb-8 flex-col gap-4">
-      <BreadCrumbs />
+      BreadCrumbs
       <div className="grid grid-cols-3"></div>
     </section>
   );
