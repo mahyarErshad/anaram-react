fe814e4fa46ef3cbddc3638c96df6f90b02c2a20
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-03 09:58:06 +0430

    Slider for image is added

--- src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
+++ src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
@@ -5,13 +5,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
-import "swiper/css/navigation";
 import "swiper/css/thumbs";
 
 import "./styles.css";
 
 // import required modules
-import { FreeMode, Navigation, Thumbs } from "swiper";
+import { FreeMode, Thumbs } from "swiper";
 import SmallButton from "../../../Components/Utils/Buttons/SmallButton/SmallButton";
 import { ReactComponent as HeartIcon } from "../../../assets/images/icons/heart-icon.svg";
 import { ReactComponent as CompareIcon } from "../../../assets/images/icons/compare-arrows.svg";
@@ -21,21 +20,19 @@ function SingleProductImageSection() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
     <section className="flex justify-start items-center gap-3">
-      <div className="flex-center flex-col gap-4">
+      <div className="flex-col gap-4 h-full">
         <SmallButton className="p-3 bg-NeutralN10 group" icon={<HeartIcon className="group-hover:scale-110 duration-300" />} />
         <SmallButton className="p-3 bg-NeutralN10 group" icon={<CompareIcon className="group-hover:scale-110 duration-300" />} />
         <SmallButton className="p-3 bg-NeutralN10 group" icon={<PresentationChart className="group-hover:scale-110 duration-300" />} />
       </div>
-      <div>
+      <div className="w-full">
         <Swiper
           style={{
-            "--swiper-navigation-color": "#fff",
             "--swiper-pagination-color": "#fff",
           }}
           spaceBetween={10}
-          navigation={true}
-          thumbs={{ swiper: thumbsSwiper }}
-          modules={[FreeMode, Navigation, Thumbs]}
+          thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+          modules={[FreeMode, Thumbs]}
           className="swiper-single-product2"
         >
           <SwiperSlide>
@@ -69,7 +66,7 @@ function SingleProductImageSection() {
             <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
           </SwiperSlide>
         </Swiper>
-        <Swiper onSwiper={setThumbsSwiper} spaceBetween={10} slidesPerView={4} freeMode={true} watchSlidesProgress={true} modules={[FreeMode, Navigation, Thumbs]} className="swiper-single-product">
+        <Swiper onSwiper={setThumbsSwiper} spaceBetween={10} slidesPerView={4} freeMode={true} watchSlidesProgress={true} modules={[FreeMode, Thumbs]} className="swiper-single-product">
           <SwiperSlide>
             <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
           </SwiperSlide>
