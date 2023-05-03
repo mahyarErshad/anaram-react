1104a0908d5f4af790bcc72cc1fc199f77223d55
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-03 09:21:15 +0430

    Added CompareIcon

--- src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
+++ src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import SmallButton from "../../../Components/Utils/Buttons/SmallButton/SmallButton"
-import {ReactComponent as HeartIcon} from "../../../assets/images/icons/heart-icon.svg"
+import SmallButton from "../../../Components/Utils/Buttons/SmallButton/SmallButton";
+import { ReactComponent as HeartIcon } from "../../../assets/images/icons/heart-icon.svg";
+import { ReactComponent as CompareIcon } from "../../../assets/images/icons/compare-arrows.svg";
 
 function SingleProductImageSection() {
   return (
     <section className="flex justify-start items-center gap-3">
       <div className="flex-center flex-col gap-4">
         <SmallButton className="p-3 bg-NeutralN10" icon={<HeartIcon />} />
+        <SmallButton className="p-3 bg-NeutralN10" icon={<CompareIcon />} />
       </div>
     </section>
   );
