6472f39d87e50ab4fbbe51179d0744c68aa3a214
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-03 09:17:48 +0430

    Imported SmallButton

--- src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
+++ src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import SmallButton from "../../../Components/Utils/Buttons/SmallButton/SmallButton"
 
 function SingleProductImageSection() {
   return (
-    <section>SingleProductImageSection</section>
-  )
+    <section className="flex justify-start items-center gap-3">
+      <div className="flex-center flex-col gap-4">
+        <SmallButton className="p-3 bg-NeutralN10" icon={<UserIconBlack />} />
+      </div>
+    </section>
+  );
 }
 
-export default SingleProductImageSection
+export default SingleProductImageSection;
