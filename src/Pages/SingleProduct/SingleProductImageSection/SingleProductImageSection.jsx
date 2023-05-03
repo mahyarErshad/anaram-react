5469713d49c1118677b7a614d17b1aa5bc24dc71
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-03 09:23:06 +0430

    Added PresentationChart

--- src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
+++ src/Pages/SingleProduct/SingleProductImageSection/SingleProductImageSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SmallButton from "../../../Components/Utils/Buttons/SmallButton/SmallButton";
 import { ReactComponent as HeartIcon } from "../../../assets/images/icons/heart-icon.svg";
 import { ReactComponent as CompareIcon } from "../../../assets/images/icons/compare-arrows.svg";
-import { ReactComponent as CompareIcon } from "../../../assets/images/icons/presentation-chart.svg";
+import { ReactComponent as PresentationChart } from "../../../assets/images/icons/presentation-chart.svg";
 
 function SingleProductImageSection() {
   return (
@@ -10,7 +10,7 @@ function SingleProductImageSection() {
       <div className="flex-center flex-col gap-4">
         <SmallButton className="p-3 bg-NeutralN10" icon={<HeartIcon />} />
         <SmallButton className="p-3 bg-NeutralN10" icon={<CompareIcon />} />
-        <SmallButton className="p-3 bg-NeutralN10" icon={<CompareIcon />} />
+        <SmallButton className="p-3 bg-NeutralN10" icon={<PresentationChart />} />
       </div>
     </section>
   );
