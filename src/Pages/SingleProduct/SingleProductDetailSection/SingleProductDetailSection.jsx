49e37f58b54e56da7edd2fe0927bc148a2a62ac4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-03 09:19:38 +0430

    Imported SingleProductImageSection

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import BreadCrumbs from "../../../Components/BreadCrumb/BreadCrumb";
+import SingleProductImageSection from "../SingleProductImageSection/SingleProductImageSection";
 
 function SingleProductDetailSection() {
   return (
     <section className="large-container px-6 pt-6 pb-8 flex-col gap-4">
       <BreadCrumbs />
       <div className="grid grid-cols-3">
-        
+        <SingleProductImageSection />
       </div>
     </section>
   );
