c0f0e0ca970202f1a26dbabbba07fd985aec72cf
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-04 10:25:50 +0430

    Target is blank

--- src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
+++ src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FooterAddress = ({ icon, text, href }) => (
-  <a href={href} className="flex items-center gap-2 not-italic">
+  <a href={href} target="_blank" rel="noreferrer" className="flex items-center gap-2 not-italic">
     {icon}
     <p className="text-sm text-NeutralN90">{text}</p>
   </a>
