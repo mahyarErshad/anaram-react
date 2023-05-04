1c919f352764618ccb1d4d92d6504fcbb64fc259
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-04 10:33:42 +0430

    Added hover effects

--- src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
+++ src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const FooterAddress = ({ icon, text, href, target }) => (
-  <a href={href} target={target} rel="noreferrer" className="flex items-center gap-2 not-italic">
+  <a href={href} target={target} rel="noreferrer" className="w-fit flex items-center gap-2 not-italic group">
     {icon}
-    <p className="text-sm text-NeutralN90">{text}</p>
+    <p className="text-sm text-NeutralN90 group-hover:text-primary6 duration-300">{text}</p>
   </a>
 );
 
