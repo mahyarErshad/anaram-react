b803c8c7dab8987a0d4126928c4edd57ce33bb2d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-04 10:27:20 +0430

    Catching target from props

--- src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
+++ src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const FooterAddress = ({ icon, text, href }) => (
-  <a href={href} target="_blank" rel="noreferrer" className="flex items-center gap-2 not-italic">
+const FooterAddress = ({ icon, text, href, target }) => (
+  <a href={href} target={target} rel="noreferrer" className="flex items-center gap-2 not-italic">
     {icon}
     <p className="text-sm text-NeutralN90">{text}</p>
   </a>
