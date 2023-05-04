f4e323916e0e37555d36e296f4ad6dd804b94d57
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-04 10:23:17 +0430

    Href is the new prop

--- src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
+++ src/Components/Footer/FooterComponents/FooterAddress/FooterAddress.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const FooterAddress = ({ icon, text }) => (
-  <div className="flex items-center gap-2 not-italic">
+const FooterAddress = ({ icon, text, href }) => (
+  <a href={href} className="flex items-center gap-2 not-italic">
     {icon}
     <p className="text-sm text-NeutralN90">{text}</p>
-  </div>
+  </a>
 );
 
 export default FooterAddress;
