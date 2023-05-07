136404a942c36d128c8c3b203d76215c2c07f9bb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 12:01:35 +0430

    First div is created

--- src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionAction.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionAction.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import {ShieldTick as ReactComponent } from "../../../assets/images/icons/shield-tick.svg"
+import React from "react";
+import { ReactComponent as ShieldTick } from "../../../assets/images/icons/shield-tick.svg";
 
 function DetailSectionAction() {
   return (
-    <section className='justify-self-center bg-NeutralN10 border border-NeutralN20 rounded-2xl pt-10 pb-6 px-6'>
-        dhdgdgh
+    <section className="justify-self-end bg-NeutralN10 border border-NeutralN20 rounded-2xl pt-10 pb-6 px-6 flex-col gap-7 min-w-[24.375rem]">
+      <div className="flex justify-start items-center gap-2 pb-6 border-b border-NeutralN30">
+        <ShieldTick />
+        <span className="text-sm text-NeutralN100">گارانتی و تضمین اصالت کالا</span>
+      </div>
     </section>
-  )
+  );
 }
 
-export default DetailSectionAction
+export default DetailSectionAction;
