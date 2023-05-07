4304e90815b09e2f397be89fdf092ded51f6084e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 13:58:09 +0430

    Component is done creating and styling

--- src/Pages/SingleProduct/SingleProductDetailSection/DeliveryOptionCard.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/DeliveryOptionCard.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import {ReactComponent as DoubleCheck} from "../../../assets/images/icons/double-check-icon.svg"
+import React from "react";
+import { ReactComponent as DoubleCheck } from "../../../assets/images/icons/double-check-icon.svg";
 
-function DeliveryOptionCard({text}) {
+function DeliveryOptionCard({ text }) {
   return (
-    <div className=''>
-
+    <div className="bg-NeutralN20 rounded-2xl flex-center gap-1 py-2 pr-1 pl-3">
+      <DoubleCheck />
+      <span className="text-xs text-NeutralN80 font-semibold">{text}</span>
     </div>
-  )
+  );
 }
 
-export default DeliveryOptionCard
+export default DeliveryOptionCard;
