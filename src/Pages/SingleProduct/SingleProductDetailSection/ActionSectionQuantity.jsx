16f568bb6a4b95b04384e1ed9164d47879565083
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 14:15:08 +0430

    Toman icon is conditional

--- src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { ReactComponent as Toman } from "../../../assets/images/icons/toman-icon-black.svg";
 
-function ActionSectionQuantity({ title, quantity }) {
+function ActionSectionQuantity({ title, quantity, toman }) {
   return (
     <div className="flex justify-between items-center pb-6 border-b border-NeutralN30">
-      <span></span>
+      <span className="text-NeutralN500 font-semibold text-base">{title}</span>
+      <div className="flex-center gap-2">
+        <span className="text-NeutralN500 font-semibold text-base">{quantity}</span>
+        {toman && <Toman />}
+      </div>
     </div>
   );
 }
