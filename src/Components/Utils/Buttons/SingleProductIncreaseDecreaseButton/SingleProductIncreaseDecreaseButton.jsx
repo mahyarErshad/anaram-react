0b3c8837959d1f8ed0324dbd254b496455468f7a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 11:00:20 +0430

    Added the text

--- src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
+++ src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function SingleProductIncreaseDecreaseButton() {
   return (
-    <div>SingleProductIncreaseDecreaseButton</div>
-  )
+    <div className="py-3 px-6 bg-NeutralN10 flex-center gap-9 w-fit rounded-lg">
+      <span className="text-sm text-NeutralN100">تعداد</span>
+
+    </div>
+  );
 }
 
-export default SingleProductIncreaseDecreaseButton
+export default SingleProductIncreaseDecreaseButton;
