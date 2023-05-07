018a293a8baebc8868f0f6c112a79afc69e3ca8e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 15:21:16 +0430

    Passed className to MainPageFeatures

--- src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
@@ -6,7 +6,7 @@ function SingleProductPage() {
   return (
     <section className="flex-col gap-6 w-full px-4 pt-10 pb-20">
       <SingleProductDetailSection />
-      <MainPageFeatures />
+      <MainPageFeatures className="max-w-[102rem]" />
     </section>
   );
 }
