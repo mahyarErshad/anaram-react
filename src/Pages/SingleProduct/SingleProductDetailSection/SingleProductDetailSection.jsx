f9c7d988be83e6dca5b739caf0e6db64db0bffa3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 14:34:30 +0430

    Created a state for calculation

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
@@ -3,8 +3,10 @@ import BreadCrumbs from "../../../Components/BreadCrumb/BreadCrumb";
 import DetailSectionImage from "./DetailSectionImage";
 import DetailSectionTextSection from "./DetailSectionTextSection";
 import DetailSectionAction from "./DetailSectionAction";
+import { useState } from "react";
 
 function SingleProductDetailSection() {
+  const [state , setState] = useState(1)
   return (
     <section className="large-container px-6 pt-6 pb-8 flex-col gap-4">
       <BreadCrumbs />
