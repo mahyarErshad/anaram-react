d02211935fefe06bd262088386abf14534d8713a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 14:31:35 +0430

    Cleaned and optimized the code

--- src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { ReactComponent as Toman } from "../../../assets/images/icons/toman-icon-black.svg";
+import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
 import { separateNumberByThousand } from "../../../lib/function/convertToPersianDigits";
 
-function ActionSectionQuantity({ title, quantity, toman, counter }) {
+const ActionSectionQuantity = ({ title, quantity, toman, counter }) => {
   return (
     <div className="flex justify-between items-center pb-6 border-b border-NeutralN30">
       <span className="text-NeutralN500 font-semibold text-base">{title}</span>
-      <div className="flex-center gap-2">
+      <div className="flex items-center gap-2">
         <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(quantity)}</span>
-        {toman && <Toman className="w-5 h-4" />}
+        {toman && <TomanIcon className="w-5 h-4" />}
         {counter && <span className="text-sm text-NeutralN500 font-semibold">عدد</span>}
       </div>
     </div>
   );
-}
+};
 
 export default ActionSectionQuantity;
