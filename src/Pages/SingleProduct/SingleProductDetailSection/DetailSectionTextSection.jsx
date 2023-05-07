f7a28f63d42e3389f9387e8d4bdc19bc71a7cba1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 10:07:43 +0430

    Added h1

--- src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionTextSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionTextSection.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function DetailSectionTextSection() {
   return (
-    <section>
-        
+    <section className="w-full px-5 flex-col gap-10 lg:mr-16">
+      <h1 className="text-lg text-NeutralN500 font-semibold">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h1>
     </section>
-  )
+  );
 }
 
-export default DetailSectionTextSection
+export default DetailSectionTextSection;
