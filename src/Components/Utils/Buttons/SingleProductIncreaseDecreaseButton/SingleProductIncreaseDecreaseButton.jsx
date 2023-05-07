68a3d0f4dcd1cd3a18567ead45d897364f167241
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 11:19:37 +0430

    Imported PlusIcon

--- src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
+++ src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
@@ -6,7 +6,9 @@ function SingleProductIncreaseDecreaseButton() {
     <div className="py-3 px-6 bg-NeutralN10 flex-center gap-9 w-fit rounded-lg">
       <span className="text-sm text-NeutralN100 font-semibold">تعداد</span>
       <div className="flex-center">
-        <button className="h-full flex-center p-2">+</button>
+        <button className="h-full flex-center p-2">
+          <PlusIcon />
+        </button>
       </div>
     </div>
   );
