17250a16165bf6f1160f6393830447c97bf32fac
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 15:18:33 +0430

    Imported MainPageFeatures

--- src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SingleProductDetailSection from "../SingleProductDetailSection/SingleProductDetailSection";
+import MainPageFeatures from "../../MainPage/MainPageFeatures/MainPageFeatures";
 
 function SingleProductPage() {
   return (
     <section className="flex-col gap-6 w-full px-4 pt-10 pb-20">
       <SingleProductDetailSection />
+      <MainPageFeatures />
     </section>
   );
 }
