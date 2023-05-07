e6f5b990bae0a05f381e091c37aba2ba1cedff48
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 14:25:47 +0430

    Separated number by 1000

--- src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
@@ -7,7 +7,7 @@ function ActionSectionQuantity({ title, quantity, toman }) {
     <div className="flex justify-between items-center pb-6 border-b border-NeutralN30">
       <span className="text-NeutralN500 font-semibold text-base">{title}</span>
       <div className="flex-center gap-2">
-        <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand}</span>
+        <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(quantity)}</span>
         {toman && <Toman className="w-5 h-4" />}
       </div>
     </div>
