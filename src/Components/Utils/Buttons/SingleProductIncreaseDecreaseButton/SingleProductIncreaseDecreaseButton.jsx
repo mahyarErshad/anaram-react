2c6262b15e4d87d3d7187d81ff7226365b073ba8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 11:28:33 +0430

    Functionality is added

--- src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
+++ src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as PlusIcon } from "../../../../assets/images/icons/increase-singleProduct-icon.svg";
 import { ReactComponent as MinusIcon } from "../../../../assets/images/icons/decrease-singleProduct-icon.svg";
 
 function SingleProductIncreaseDecreaseButton() {
+  const [state, setState] = useState(0);
   return (
     <div className="py-3 px-6 bg-NeutralN10 flex-center gap-9 w-fit rounded-lg">
       <span className="text-sm text-NeutralN100 font-semibold">تعداد</span>
       <div className="flex-center">
-        <button className="h-full flex-center p-2 bg-NeutralN20 rounded-[0.25rem]">
+        <button onClick={() => setState((prev) => prev + 1)} className="h-full flex-center p-2 bg-NeutralN20 rounded-[0.25rem]">
           <PlusIcon />
         </button>
-        <button className="h-full flex-center p-2 bg-NeutralN20 rounded-[0.25rem]">
+        <span className="text-base font-bold text-NeutralN500 px-4 min-w-[3rem] flex-center">{state}</span>
+        <button onClick={() => setState((prev) => prev - 1)} className="h-full flex-center p-2 bg-NeutralN20 rounded-[0.25rem]">
           <MinusIcon />
         </button>
       </div>
