ac96bb636acbe8d4da9da597e92e4edf95488df0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 10:23:57 +0430

    List items are done

--- src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionTextSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionTextSection.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
 function DetailSectionTextSection() {
+  const data = [
+    {
+      Q: "نوع تکنولوژی باتری :",
+      A: "آلکالاین",
+    },
+    {
+      Q: "قابلیت های باتری :",
+      A: "ماندگاری طولانی",
+    },
+    {
+      Q: "ولتاژ باتری باتری :",
+      A: "۹ ولت",
+    },
+    {
+      Q: "نوع باتری :",
+      A: "کتابی",
+    },
+    {
+      Q: "تعداد باتری‌های موجود در پک :",
+      A: "۱ عدد",
+    },
+  ];
   return (
     <section className="w-full px-5 flex-col gap-10 lg:mr-16">
       <h1 className="text-lg text-NeutralN500 font-semibold">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h1>
+      <div className="flex-col gap-4">
+        <h3 className="text-NeutralN500 font-semibold">ویژگی ها</h3>
+        <ul className="flex-col gap-4">
+          {data.map((item, index) => {
+            return (
+              <li key={index} className="text-sm text-NeutralN100 flex justify-start items-center gap-2">
+                <span className="w-1 h-1 bg-primary6 rounded-full"></span>
+                <span className="font-semibold">{item.Q}</span>
+                <strong className="text-NeutralN500 text-sm font-semibold">{item.A}</strong>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 }
