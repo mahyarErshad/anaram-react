9afad01ab3ad9693264a4c5c95459ee525c63897
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-07 11:17:46 +0430

    Added a new icon

--- src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
+++ src/Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function SingleProductIncreaseDecreaseButton() {
   return (
     <div className="py-3 px-6 bg-NeutralN10 flex-center gap-9 w-fit rounded-lg">
-      <span className="text-sm text-NeutralN100">تعداد</span>
-
+      <span className="text-sm text-NeutralN100 font-semibold">تعداد</span>
+      <div className="flex-center">
+        <button className="h-full flex-center p-2">+</button>
+      </div>
     </div>
   );
 }
