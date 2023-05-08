f29679a0cc01c35a5d9b63f54bce043068e24d0e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-08 10:04:45 +0430

    Arrow icon has animation on opening and closing

--- src/Components/Header/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Header/SideBarMenu/SideBarMenu.jsx
@@ -38,7 +38,7 @@ function SideBarMenu({ sideBarOpen, setSideBarOpen }) {
               <AccordionHeader className={`${styles["accordion-header"]} ${item.hasSubMenu && styles.AccordionBodyHasMenu} stroke-NeutralN90 text-NeutralN700 bg-NeutralN10 border border-transparent hover:border-primary2 rounded-2xl p-5 flex justify-start items-center min-w-[13.5rem] duration-300`}>
                 {item.icon}
                 <h3 className="text-xs text-inherit font-semibold mr-[0.625rem]">{item.text}</h3>
-                {item.hasSubMenu && <ArrowIcon className="stroke-inherit mr-[0.375rem]" />}
+                {item.hasSubMenu && <ArrowIcon className="stroke-inherit mr-[0.375rem] duration-300 arrow-icon" />}
               </AccordionHeader>
               {item.hasSubMenu && (
                 <AccordionBody className="bg-primary1 w-full rounded-b-2xl -mt-2 px-5 flex-col items-center gap-2">
