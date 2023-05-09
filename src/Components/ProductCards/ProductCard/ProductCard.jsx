e51e97cdf30a3df84961f6c394058bd1a5ce62af
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-09 10:47:35 +0430

    Fixed a bug of clicking card's buttons not working

--- src/Components/ProductCards/ProductCard/ProductCard.jsx
+++ src/Components/ProductCards/ProductCard/ProductCard.jsx
@@ -25,15 +25,17 @@ const ProductCard = ({ discountCard }) => {
   );
 
   return (
-    <Link onClick={goToTop} to="products/product/id-1234">
+    <>
       <figure className={`${styles.card} ${discountCard && styles.after} pt-3 pb-[0.875rem] relative px-3 rounded-lg bg-white border border-NeutralN30 flex-col cursor-pointer transition-all duration-300`}>
-        <div className="flex-center h-2/4 mb-4 relative">
-          <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
-          {discountSection}
-          <ProductCardRatingSection rating={4.5} />
-        </div>
-        <ProductCardTitleSection title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" />
-        {priceSection}
+        <Link onClick={goToTop} to="products/product/id-1234">
+          <div className="flex-center h-2/4 mb-4 relative">
+            <img className="mt-5 transition-all duration-300" src={batteryImage} alt="battery" />
+            {discountSection}
+            <ProductCardRatingSection rating={4.5} />
+          </div>
+          <ProductCardTitleSection title="باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی" />
+          {priceSection}
+        </Link>
         <div className="flex-center gap-1 w-full">
           <PrimaryButton addToCartButton text="افزودن به سبد" icon={<CartIcon className="w-4 h-4" stroke="white" />} />
           <SmallButton onClick={() => setRedHeart(!redHeart)} className="p-3 bg-NeutralN10" icon={<HeartIcon className={`stroke-NeutralN300 ${redHeart && "animate-pulse-heart fill-red-500 stroke-red-500"}`} />} />
@@ -48,7 +50,7 @@ const ProductCard = ({ discountCard }) => {
           </div>
         )}
       </figure>
-    </Link>
+    </>
   );
 };
 
