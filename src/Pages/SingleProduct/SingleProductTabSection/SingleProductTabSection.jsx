316e886e2ffb3325a9db31ea31f78070afadecc0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-10 09:41:09 +0430

    Selected tabs have ::after

--- src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
 function SingleProductTabSection() {
   return (
     <section className="large-container p-10 flex-col">
       <Tabs>
-        <TabList className="flex gap-10 pb-4 border-b border-NeutralN30 text-sm text-NeutralN500 font-medium">
-          <Tab className={`${styles.tab} relative`}>adada</Tab>
-          <Tab className={`${styles.tab} relative`}>sfsfsdgfs</Tab>
+        <TabList className="flex gap-10 border-b border-NeutralN30 text-sm text-NeutralN500 font-medium">
+          <Tab className={`${styles.tab} relative pb-4`}>adada</Tab>
+          <Tab className={`${styles.tab} relative pb-4`}>sfsfsdgfs</Tab>
         </TabList>
         <TabPanel>sfsdfsfgdsfg</TabPanel>
         <TabPanel>a324423</TabPanel>
