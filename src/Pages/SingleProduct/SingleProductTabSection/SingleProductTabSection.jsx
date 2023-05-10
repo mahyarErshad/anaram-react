9e893bc16a8a85d1da973852acaa1f02a7a8edb7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-10 09:29:46 +0430

    Added classes of TabList

--- src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 function SingleProductTabSection() {
-  return <section className="large-container p-10 flex-col">
-    <Tabs>
-        <TabList>
-            <Tab>adada</Tab>
-            <Tab>sfsfsdgfs</Tab>
+  return (
+    <section className="large-container p-10 flex-col">
+      <Tabs>
+        <TabList className="flex gap-10 pb-4 border-b border-NeutralN30 text-sm text-NeutralN500 font-medium">
+          <Tab>adada</Tab>
+          <Tab>sfsfsdgfs</Tab>
         </TabList>
-        <TabPanel>
-            sfsdfsfgdsfg
-        </TabPanel>
-        <TabPanel>
-            a324423
-        </TabPanel>
-    </Tabs>
-  </section>;
+        <TabPanel>sfsdfsfgdsfg</TabPanel>
+        <TabPanel>a324423</TabPanel>
+      </Tabs>
+    </section>
+  );
 }
 
 export default SingleProductTabSection;
