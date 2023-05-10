49baff8da8db65ce5fa34dcfed4b9c0854273686
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-10 09:21:12 +0430

    Giving necessary classnames

--- src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function SingleProductTabSection() {
-  return (
-    <div>SingleProductTabSection</div>
-  )
+  return <section className="large-container px-6 pt-6 pb-8 flex-col gap-4">
+    
+  </section>;
 }
 
-export default SingleProductTabSection
+export default SingleProductTabSection;
