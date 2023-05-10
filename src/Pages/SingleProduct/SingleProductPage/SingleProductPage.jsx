8ce60055eae3df9f509b8fa5fb3b09fdc2a535b7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-10 09:21:30 +0430

    Imported SingleProductTabSection

--- src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SingleProductDetailSection from "../SingleProductDetailSection/SingleProductDetailSection";
 import MainPageFeatures from "../../MainPage/MainPageFeatures/MainPageFeatures";
+import SingleProductTabSection from "../SingleProductTabSection/SingleProductTabSection";
 
 function SingleProductPage() {
   return (
     <section className="flex-col gap-6 w-full px-4 pt-10 pb-20">
       <SingleProductDetailSection />
       <MainPageFeatures className="max-w-[102rem]" />
+      <SingleProductTabSection />
     </section>
   );
 }
