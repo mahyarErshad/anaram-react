747f1f7f8c9ddfa86e2dd0cf8d1431811030483c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-10 09:41:57 +0430

    Changed the title of Tabs

--- src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
@@ -7,8 +7,8 @@ function SingleProductTabSection() {
     <section className="large-container p-10 flex-col">
       <Tabs>
         <TabList className="flex gap-10 border-b border-NeutralN30 text-sm text-NeutralN500 font-medium">
-          <Tab className={`${styles.tab} relative pb-4`}>adada</Tab>
-          <Tab className={`${styles.tab} relative pb-4`}>sfsfsdgfs</Tab>
+          <Tab className={`${styles.tab} relative pb-4`}>مشخصات</Tab>
+          <Tab className={`${styles.tab} relative pb-4`}>نظرات</Tab>
         </TabList>
         <TabPanel>sfsdfsfgdsfg</TabPanel>
         <TabPanel>a324423</TabPanel>
