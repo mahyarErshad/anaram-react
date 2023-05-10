f1c8783229c15f8c11a72e81cb0df26c63de4807
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-10 09:25:56 +0430

    Imported Tab, TabList, TabPanel, Tabs

--- src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 function SingleProductTabSection() {
-  return <section className="large-container px-6 pt-6 pb-8 flex-col gap-4">
-    
+  return <section className="large-container p-10 flex-col">
+    <Tabs>
+        <TabList>
+            <Tab>adada</Tab>
+            <Tab>sfsfsdgfs</Tab>
+        </TabList>
+        <TabPanel>
+            sfsdfsfgdsfg
+        </TabPanel>
+        <TabPanel>
+            a324423
+        </TabPanel>
+    </Tabs>
   </section>;
 }
 
