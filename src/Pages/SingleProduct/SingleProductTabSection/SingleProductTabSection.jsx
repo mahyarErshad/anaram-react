a008f0683b00ee46ad394f58000cb2a3f5bd956c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-11 08:21:58 +0330

    Removed Title from Rating

--- src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/SingleProductTabSection.jsx
@@ -56,7 +56,7 @@ function SingleProductTabSection() {
               <textarea id="text-area" className="mt-2 p-3 border border-NeutralN30 rounded-lg w-full resize-none text-xs focus:border-primary2 duration-300 outline-none h-[9.0625rem] mb-8" placeholder="متن نظر ..."></textarea>
               <span className="text-NeutralN500 font-semibold text-xs">امتیاز شما به این محصول</span>
               <div className="mt-2 mb-8 w-full border border-NeutralN30 rounded-lg py-[0.875rem] flex-center">
-                <Rating onClick={(rating) => setRating(rating)} initialValue={0} ratingValue={rating} size={40} transition fillColorArray={["#e85d04", "#f48c06", "#faa307", "#ffba08", "#ffd60a"]} />
+                <Rating onClick={(rating) => setRating(rating)} allowTitleTag={false} initialValue={0} ratingValue={rating} size={40} transition fillColorArray={["#e85d04", "#f48c06", "#faa307", "#ffba08", "#ffd60a"]} />
               </div>
               <PrimaryButton text="ثبت دیدگاه" className="w-full" />
             </form>
