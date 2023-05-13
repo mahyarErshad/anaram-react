a1b1e11881ec02d2944e21a3983a18d3b4c6f2c0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 10:48:03 +0330

    Catching props

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function LikeDislikeButton() {
+function LikeDislikeButton({counter, icon, onClick}) {
   return (
     <button className="flex-center py-2 pl-2 pr-4 gap-3 bg-NeutralN10 rounded-5px">
       <span className="text-sm text-NeutralN300 font-semibold">{data.dislikes}</span>
