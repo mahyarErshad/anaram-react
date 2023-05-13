487a39930325136ee862afdbf6efa5d1adf6efa4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 10:11:49 +0330

    Passed Data to the ClientComment

--- src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductComments/SingleProductComments.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductComments/SingleProductComments.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import SingleProductForm from "../SingleProductForm/SingleProductForm";
 import ClientComment from "../ClientComment/ClientComment";
+import SingleProductCommentsData from "../../../../../lib/SingleProductCommentsData/SingleProductCommentsData";
 
 function SingleProductComments() {
-
   return (
     <>
       <div className="bg-white border border-NeutralN20 rounded-lg p-6 min-w-[21.875rem]">
         <SingleProductForm />
       </div>
       <div className="h-full w-full flex-col gap-8 justify-center items-start self-start">
-        <ClientComment />
+        {SingleProductCommentsData.map((data, index) => {
+          return <ClientComment key={index} data={data} />;
+        })}
       </div>
     </>
   );
