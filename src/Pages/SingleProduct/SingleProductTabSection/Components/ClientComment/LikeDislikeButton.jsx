4cf7ee5ddeb6df3036abf144e64284ca451eba8b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 10:48:43 +0330

    Putting props into the component to make it dynamic

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function LikeDislikeButton({counter, icon, onClick}) {
+function LikeDislikeButton({ counter, icon, onClick }) {
   return (
-    <button className="flex-center py-2 pl-2 pr-4 gap-3 bg-NeutralN10 rounded-5px">
-      <span className="text-sm text-NeutralN300 font-semibold">{data.dislikes}</span>
-      <DislikeIcon />
+    <button onClick={onClick} className="flex-center py-2 pl-2 pr-4 gap-3 bg-NeutralN10 rounded-5px">
+      <span className="text-sm text-NeutralN300 font-semibold">{counter}</span>
+      {icon}
     </button>
   );
 }
