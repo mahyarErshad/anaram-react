97f671f8527695a5bf9c83ae410b99a1bbe1c4a9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 09:58:15 +0330

    Imported ClientComment

--- src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductComments/SingleProductComments.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductComments/SingleProductComments.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import SingleProductForm from "../SingleProductForm/SingleProductForm";
+import ClientComment from "../ClientComment/ClientComment";
 
 function SingleProductComments() {
   return (
-    <div className="bg-white border border-NeutralN20 rounded-lg p-6 min-w-[21.875rem]">
-      <SingleProductForm />
-      
-    </div>
+    <>
+      <div className="bg-white border border-NeutralN20 rounded-lg p-6 min-w-[21.875rem]">
+        <SingleProductForm />
+      </div>
+      <ClientComment />
+    </>
   );
 }
 
