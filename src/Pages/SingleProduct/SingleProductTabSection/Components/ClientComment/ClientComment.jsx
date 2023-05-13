449b050e5641474313746dab49e949203d226f88
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 10:53:47 +0330

    Added the p tag

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
@@ -21,6 +21,7 @@ function ClientComment({ data }) {
           <LikeDislikeButton icon={<LikeIcon />} counter={data.likes} />
         </div>
       </div>
+      <p className="text-sm text-NeutralN500 font-semibold mr-[3.25rem]">{data.comment}</p>
     </div>
   );
 }
