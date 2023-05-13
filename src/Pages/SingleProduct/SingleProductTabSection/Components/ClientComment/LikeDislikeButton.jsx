540554e68763f77d29725cdb09c4d2b8e36cc8ed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 11:37:32 +0330

    className can be passed via props

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function LikeDislikeButton({ counter, icon, onClick }) {
+function LikeDislikeButton({ counter, icon, onClick, className }) {
   return (
-    <button onClick={onClick} className="flex-center py-2 pl-2 pr-4 gap-3 bg-NeutralN10 rounded-5px">
+    <button onClick={onClick} className={`${className} flex-center py-2 pl-2 pr-4 gap-3 bg-NeutralN10 rounded-5px`}>
       <span className="text-sm text-NeutralN300 font-semibold">{counter}</span>
       {icon}
     </button>
