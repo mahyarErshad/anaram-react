babfd1606e51f98773a48097e886faaecf01bda5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 11:44:00 +0330

    Like button gets bg color conditionally

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
@@ -25,8 +25,8 @@ function ClientComment({ data }) {
           <Rating initialValue={data.rating} size={14} readonly />
         </div>
         <div className="flex-center gap-2">
-          <LikeDislikeButton onClick={handleDislike} icon={<DislikeIcon className={`${likeState === "disliked" ? "stroke-Magenta fill-Magenta" : "stroke-NeutralN300"}`} />} counter={likeState === "disliked" ? data.dislikes + 1 : data.dislikes} />
-          <LikeDislikeButton onClick={handleLike} icon={<LikeIcon className={`${likeState === "liked" ? "stroke-primary6 fill-primary6" : "stroke-NeutralN300"}`} />} counter={likeState === "liked" ? data.likes + 1 : data.likes} /> 
+          <LikeDislikeButton className={`${likeState === "disliked" ? "stroke-Magenta fill-Magenta" : "bg-NeutralN10"}`} onClick={handleDislike} icon={<DislikeIcon className={`${likeState === "disliked" ? "stroke-Magenta fill-Magenta" : "stroke-NeutralN300"}`} />} counter={likeState === "disliked" ? data.dislikes + 1 : data.dislikes} />
+          <LikeDislikeButton className={`${likeState === "liked" ? "bg-primary1" : "bg-NeutralN10"}`} onClick={handleLike} icon={<LikeIcon className={`${likeState === "liked" ? "stroke-primary6 fill-primary6" : "stroke-NeutralN300"}`} />} counter={likeState === "liked" ? data.likes + 1 : data.likes} />
         </div>
       </div>
       <p className="text-xs text-NeutralN500 font-semibold mr-[3.25rem]">{data.comment}</p>
--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function LikeDislikeButton({ counter, icon, onClick, className }) {
   return (
-    <button onClick={onClick} className={`${className} flex-center py-2 pl-2 pr-4 gap-3 bg-NeutralN10 rounded-5px`}>
+    <button onClick={onClick} className={`${className} flex-center py-2 pl-2 pr-4 gap-3 rounded-5px min-w-[5.3125rem]`}>
       <span className="text-sm text-NeutralN300 font-semibold">{counter}</span>
       {icon}
     </button>
