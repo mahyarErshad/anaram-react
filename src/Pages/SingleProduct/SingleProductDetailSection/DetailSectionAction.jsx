fa3655fc33f0177cb5ea2b8d9aa72f909f7066aa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 08:24:57 +0330

    Set a state for add to cart

--- src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionAction.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/DetailSectionAction.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as ShieldTick } from "../../../assets/images/icons/shield-tick.svg";
 import { ReactComponent as ShopAdd } from "../../../assets/images/icons/shop-add.svg";
 import DeliveryOptionCard from "./DeliveryOptionCard";
@@ -6,27 +6,30 @@ import ActionSectionQuantity from "./ActionSectionQuantity";
 import PrimaryButton from "../../../Components/Utils/Buttons/PrimaryButton/PrimaryButton";
 import { ReactComponent as CartIcon } from "../../../assets/images/icons/bag-icon.svg";
 
-const DetailSectionAction = ({ state }) => (
-  <section className="justify-self-end bg-NeutralN10 border border-NeutralN10 rounded-2xl pt-10 pb-6 px-6 flex-col gap-7 min-w-[24.375rem]">
-    <div className="flex justify-start items-center gap-2 pb-6 border-b border-NeutralN30">
-      <ShieldTick />
-      <span className="text-sm text-NeutralN100">گارانتی و تضمین اصالت کالا</span>
-    </div>
-    <div className="flex-col justify-center items-start gap-5 pb-6 border-b border-NeutralN30">
-      <div className="flex justify-start items-center gap-2">
-        <ShopAdd />
-        <span className="text-sm text-Green7 font-semibold">موجود در انبار داناپیوست</span>
+const DetailSectionAction = ({ state }) => {
+  const [addToCart, setAddToCart] = useState(false);
+  return (
+    <section className="justify-self-end bg-NeutralN10 border border-NeutralN10 rounded-2xl pt-10 pb-6 px-6 flex-col gap-7 min-w-[24.375rem]">
+      <div className="flex justify-start items-center gap-2 pb-6 border-b border-NeutralN30">
+        <ShieldTick />
+        <span className="text-sm text-NeutralN100">گارانتی و تضمین اصالت کالا</span>
       </div>
-      <div className="flex justify-start items-center gap-6">
-        <DeliveryOptionCard text="تحویل حضوری" />
-        <DeliveryOptionCard text="ارسال رایگان" />
+      <div className="flex-col justify-center items-start gap-5 pb-6 border-b border-NeutralN30">
+        <div className="flex justify-start items-center gap-2">
+          <ShopAdd />
+          <span className="text-sm text-Green7 font-semibold">موجود در انبار داناپیوست</span>
+        </div>
+        <div className="flex justify-start items-center gap-6">
+          <DeliveryOptionCard text="تحویل حضوری" />
+          <DeliveryOptionCard text="ارسال رایگان" />
+        </div>
       </div>
-    </div>
-    <ActionSectionQuantity title="قیمت محصول :" quantity={124000} toman />
-    <ActionSectionQuantity title="تعداد سفارش :" quantity={state} counter />
-    <ActionSectionQuantity title="قیمت نهایی محصولات :" quantity={state * 124000} toman />
-    <PrimaryButton icon={<CartIcon className="stroke-white" />} text="افزودن به سبد" />
-  </section>
-);
+      <ActionSectionQuantity title="قیمت محصول :" quantity={124000} toman />
+      <ActionSectionQuantity title="تعداد سفارش :" quantity={state} counter />
+      <ActionSectionQuantity title="قیمت نهایی محصولات :" quantity={state * 124000} toman />
+      <PrimaryButton icon={<CartIcon className="stroke-white" />} text="افزودن به سبد" />
+    </section>
+  );
+};
 
 export default DetailSectionAction;
