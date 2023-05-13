e4ab3a30d9349e05bbbf22605a304d34bb2dd18b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 10:32:03 +0330

    Name, date and rating is added

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Rating } from "react-simple-star-rating";
 
 function ClientComment({ data }) {
   return (
-    <div className="pt-8 border-b border-NeutralN30 flex-col gap-4">
+    <div className="pb-8 border-b border-NeutralN30 flex-col gap-4 w-full">
       <div className="flex justify-between items-center">
         <div className="flex gap-3">
           <img className="w-10 h-10 rounded-full" src={data.avatar} alt="avatar" />
+          <div className="flex-col justify-center gap-1">
+            <span className="text-NeutralN500 text-xs font-semibold">{data.name}</span>
+            <span className="text-[0.5625rem] text-NeutralN70">{data.date}</span>
+          </div>
+          <Rating initialValue={data.rating} size={14} onClick={function noRefCheck() {}} readonly />
         </div>
       </div>
     </div>
