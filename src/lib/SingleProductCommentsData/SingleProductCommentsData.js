f038ff8850657bebf211be09220562467b053682
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-13 11:56:29 +0330

    Added fake comments

--- src/lib/SingleProductCommentsData/SingleProductCommentsData.js
+++ src/lib/SingleProductCommentsData/SingleProductCommentsData.js
@@ -1,4 +1,7 @@
 import avatar1 from "../../assets/images/data/single-product-comments/1.svg";
+import avatar2 from "../../assets/images/data/single-product-comments/2.svg";
+import avatar3 from "../../assets/images/data/single-product-comments/3.svg";
+import avatar4 from "../../assets/images/data/single-product-comments/4.svg";
 
 const SingleProductCommentsData = [
   {
@@ -9,6 +12,37 @@ const SingleProductCommentsData = [
     dislikes: 2,
     likes: 24,
     comment: "خوب و کارآمد مثل بقیه باتری‌های وارتا، من این مدل رو برای مولتی متر تهیه کردم. علیرغم اینکه تاریخ تولید و انقضا نداشت امیدوارم دوام خوبی داشته باشه.",
+  },
+  {
+    avatar: avatar2,
+    name: "نازنین صادقی",
+    date: "۱۴۰۲/۰۵/۱۲",
+    rating: 4,
+    dislikes: 0,
+    likes: 100,
+    comment: "خیلی چیز خوبیه واقعا خیلی قویه و عمرش خیلی طولانیه من الانم استفاده میکنم بازم خیلی خوبه",
+  },
+  {
+    avatar: avatar3,
+    name: "برکه احمدی",
+    date: "۱۴۰۲/۰۵/۱۲",
+    rating: 4,
+    dislikes: 5,
+    likes: 42,
+    comment: "تازه گرفتیم مرور زمان کیفیت رو نشون میده",
+  },
+  {
+    avatar: avatar4,
+    name: "ساحل سماواتی",
+    date: "۱۴۰۲/۰۵/۱۲",
+    rating: 1,
+    dislikes: 28,
+    comment: "خییییییلی بی کیفیت بود پشیمون شدم از سفارشم",
+    likes: 8,
+    AdminReply: {
+      date: "۱۴۰۲/۰۵/۱۲",
+      comment: "سلام دوست عزیز ، امیدوارم در سفارش های بعدیتون تجربه بهتری داشته باشید.",
+    },
   },
 ];
 
