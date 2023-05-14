c6e399ec57c7ab4475676dcc95c2645d275698b4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-14 10:14:09 +0330

    Styled the text

--- src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductOfferTimeCounter/SingleProductOfferTimeCounter.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductOfferTimeCounter/SingleProductOfferTimeCounter.jsx
@@ -6,7 +6,7 @@ const SingleProductOfferTimeCounter = ({ number, text }) => {
   return (
     <div className={`flex-col gap-2 items-center`}>
       <span className="text-sm font-semibold text-Magenta bg-NeutralN20 py-2 px-3 rounded-5px h-10 w-10 flex-center">{formattedNumber}</span>
-      <span className="text-[0.5rem] text-NeutralN50 transition-all duration-300">{text}</span>
+      <span className="text-[0.5625rem] text-NeutralN100 font-semibold transition-all duration-300">{text}</span>
     </div>
   );
 };
