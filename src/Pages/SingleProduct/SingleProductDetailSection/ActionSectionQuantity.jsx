097c458426c099e3873e56d5bb1f9423f68ca9d9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-14 13:19:05 +0330

    Changed the photos

--- src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
 import { separateNumberByThousand } from "../../../lib/function/convertToPersianDigits";
 
-const ActionSectionQuantity = ({ title, quantity, toman, counter, hasDiscount }) => {
+const ActionSectionQuantity = ({ title, quantity, toman, counter }) => {
   return (
     <div className="flex justify-between items-center pb-6 border-b border-NeutralN30 last-of-type:border-none">
       <span className="text-NeutralN500 font-medium text-base">{title}</span>
       <div className="flex items-center gap-2">
-        <div className="flex-col justify-center items-end">
-        
-          <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(quantity)}</span>
-        </div>
+        <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(quantity)}</span>
         {toman && <TomanIcon className="w-5 h-4" />}
         {counter && <span className="text-sm text-NeutralN500 font-semibold">عدد</span>}
       </div>
