f17e7429b1529f67de605fc6ff93500c584f5cc7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-14 13:04:11 +0330

    Using global css instead of module css

--- src/Components/ProductCards/ProductCardComponents/ProductCardDiscountSection/ProductCardDiscountSection.jsx
+++ src/Components/ProductCards/ProductCardComponents/ProductCardDiscountSection/ProductCardDiscountSection.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import styles from "../../style/productCard.module.css";
 import { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits";
 
-const ProductCardDiscountSection = ({ percentage }) => <span className={`${styles["discount-percentage"]} text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal`}>{separateNumberByThousand(percentage)}٪</span>;
+const ProductCardDiscountSection = ({ percentage }) => <span className="discount-percentage-icon text-white absolute -top-1 -right-1 h-[2.1875rem] w-[2.1875rem] flex-center text-[0.8125rem] font-normal">{separateNumberByThousand(percentage)}٪</span>;
 
 export default ProductCardDiscountSection;
