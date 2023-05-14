2318502b766550d9387a831dd78ff55e9c9e4c57
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-14 13:34:47 +0330

    Discounted text is added

--- src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
+import { ReactComponent as GrayToman } from "../../../assets/images/icons/toman-icon-gray.svg";
 import { separateNumberByThousand } from "../../../lib/function/convertToPersianDigits";
 
-const ActionSectionQuantity = ({ title, quantity, toman, counter }) => {
+const ActionSectionQuantity = ({ title, quantity, toman, counter, hasDiscount, discountedPrice }) => {
   return (
     <div className="flex justify-between items-center pb-6 border-b border-NeutralN30 last-of-type:border-none">
       <span className="text-NeutralN500 font-medium text-base">{title}</span>
-      <div className="flex items-center gap-2">
-        <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(quantity)}</span>
-        {toman && <TomanIcon className="w-5 h-4" />}
-        {counter && <span className="text-sm text-NeutralN500 font-semibold">عدد</span>}
+      <div className="flex-col">
+        {hasDiscount && (
+          <div className="flex justify-end items-center gap-2">
+            <span className="relative lineThrough text-sm">{discountedPrice}</span>
+            <GrayToman className="w-4 h-3" />
+          </div>
+        )}
+        <div className="flex items-center gap-2">
+          <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(quantity)}</span>
+          {toman && <TomanIcon className="w-5 h-4" />}
+          {counter && <span className="text-sm text-NeutralN500 font-semibold">عدد</span>}
+        </div>
       </div>
     </div>
   );
