cae7bfd63ee15fa5c377868817c00a07783b735a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-14 09:53:50 +0330

    Copied the codes

--- src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductOfferTimeCounter/SingleProductOfferTimeCounter.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductOfferTimeCounter/SingleProductOfferTimeCounter.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import styles from "../../style/productCard.module.css";
+import { separateNumberByThousand } from "../../../../lib/function/convertToPersianDigits";
 
-function SingleProductOfferTimeCounter() {
+const SingleProductOfferTimeCounter = ({ number, text }) => {
+  const formattedNumber = separateNumberByThousand(number).padStart(2, '0');
   return (
-    <div>SingleProductOfferTimeCounter</div>
-  )
-}
+    <div className={`${styles.counter} flex-col justify-between items-center h-full w-1/4 border-r border-NeutralN30`}>
+      <span className="text-xs font-semibold text-Magenta">{formattedNumber}</span>
+      <span className="text-[0.5rem] text-NeutralN50 transition-all duration-300">{text}</span>
+    </div>
+  );
+};
 
-export default SingleProductOfferTimeCounter
+export default ProductCardTimeCounter;
