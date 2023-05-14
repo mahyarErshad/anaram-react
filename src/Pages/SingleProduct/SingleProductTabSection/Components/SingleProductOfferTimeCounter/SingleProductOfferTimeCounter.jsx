275253c408edc5cce0bc9df99ff04ee17eb6cf2f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-14 09:53:10 +0330

    Created the component

---
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/SingleProductOfferTimeCounter/SingleProductOfferTimeCounter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingleProductOfferTimeCounter() {
+  return (
+    <div>SingleProductOfferTimeCounter</div>
+  )
+}
+
+export default SingleProductOfferTimeCounter
