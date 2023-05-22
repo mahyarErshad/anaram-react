7228e9ad4faff7d40f25db02f2994ba63f62bbaa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-22 12:48:57 +0330

    Removed BatteryChargingComponent

--- src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import SingleProductDetailSection from "../SingleProductDetailSection/SingleProductDetailSection";
 import MainPageFeatures from "../../MainPage/MainPageFeatures/MainPageFeatures";
 import SingleProductTabSection from "../SingleProductTabSection/SingleProductTabSection";
-import BatteryChargingComponent from "../../../Components/BatteryChargingComponent/BatteryChargingComponent";
 
 function SingleProductPage() {
   return (
     <section className="flex-col gap-6 w-full px-4 pt-10 pb-20">
-      <BatteryChargingComponent />
       <SingleProductDetailSection />
       <MainPageFeatures className="max-w-[102rem]" />
       <SingleProductTabSection />
