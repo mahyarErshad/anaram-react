7cdd0e9b6162b091a852628cf2999cb99aa96bd8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 11:00:17 +0330

    Added PrimaryButton

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import PrimaryButton from "../../../Components/Utils/Buttons/PrimaryButton/PrimaryButton";
+import { ReactComponent as CartIcon } from "../../../assets/images/icons/bag-icon.svg";
 
 function SingleProductSmallScreenAction() {
   return (
-    <div>SingleProductSmallScreenAction</div>
-  )
+    <section className="w-full bg-white rounded-lg px-3 pt-3 pb-4 flex-col">
+      <div className="flex justify-between items-center px-6">
+        <PrimaryButton icon={<CartIcon className="stroke-white" />} text="افزودن به سبد" />
+      </div>
+    </section>
+  );
 }
 
-export default SingleProductSmallScreenAction
+export default SingleProductSmallScreenAction;
