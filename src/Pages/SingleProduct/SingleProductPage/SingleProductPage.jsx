156619fc8883540ae7dd70df2f368caa3fb60ac9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 10:50:14 +0330

    Imported SingleProductSmallScreenAction

--- src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/SingleProduct/SingleProductPage/SingleProductPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import SingleProductDetailSection from "../SingleProductDetailSection/SingleProductDetailSection";
 import MainPageFeatures from "../../MainPage/MainPageFeatures/MainPageFeatures";
 import SingleProductTabSection from "../SingleProductTabSection/SingleProductTabSection";
+import SingleProductSmallScreenAction from "../SingleProductDetailSection/SingleProductSmallScreenAction";
 
 function SingleProductPage() {
   return (
     <section className="flex-col gap-6 w-full px-4 pt-10 pb-20">
       <SingleProductDetailSection />
+      <SingleProductSmallScreenAction />
       <MainPageFeatures className="max-w-[102rem]" />
       <SingleProductTabSection />
     </section>
