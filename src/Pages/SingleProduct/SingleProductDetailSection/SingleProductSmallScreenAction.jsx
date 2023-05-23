cb76aae8bc846e7325a32e55240ee04df48b812b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 10:46:29 +0330

    Created the component

---
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingleProductSmallScreenAction() {
+  return (
+    <div>SingleProductSmallScreenAction</div>
+  )
+}
+
+export default SingleProductSmallScreenAction
