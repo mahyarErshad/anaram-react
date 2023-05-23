1d54cae8e9111200eecc19d4fa1d84a621620e88
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 11:39:57 +0330

    Added the price section

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import PrimaryButton from "../../../Components/Utils/Buttons/PrimaryButton/PrimaryButton";
 import { ReactComponent as CartIcon } from "../../../assets/images/icons/bag-icon.svg";
+import { separateNumberByThousand } from "../../../lib/function/convertToPersianDigits";
+import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
 
 function SingleProductSmallScreenAction() {
   return (
     <section className="w-full bg-white rounded-lg px-3 pt-3 pb-4 flex-col">
       <div className="flex justify-between items-center px-6">
         <PrimaryButton icon={<CartIcon className="stroke-white" />} text="افزودن به سبد" />
+        <div className="flex items-center gap-2">
+          <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(124000)}</span>
+          <TomanIcon className="w-5 h-4" />
+        </div>
       </div>
     </section>
   );
