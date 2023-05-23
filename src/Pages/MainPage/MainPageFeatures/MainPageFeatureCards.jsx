4b93a09f62e45ee85e0893e396cee6f0ec1ea35e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 12:22:52 +0330

    Classname can be passed as props

--- src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
+++ src/Pages/MainPage/MainPageFeatures/MainPageFeatureCards.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function MainPageFeatureCards({ icon, text }) {
+function MainPageFeatureCards({ icon, text, className }) {
   return (
-    <div className="flex max-md:flex-col justify-start items-center max-md:gap-2 md:gap-6 flex-grow">
+    <div className={`flex max-md:flex-col justify-start items-center max-md:gap-2 md:gap-6 flex-grow ${className}`}>
       <div className="flex-center bg-NeutralN10 rounded-[0.3125rem] max-md:p-3 md:p-4 max-md:w-12 max-md:h-12 md:w-[4.5rem] md:h-[4.5rem]">{icon}</div>
       <span className="max-md:text-[0.5rem] md:text-sm text-center lg:font-semibold text-NeutralN500">{text}</span>
     </div>
