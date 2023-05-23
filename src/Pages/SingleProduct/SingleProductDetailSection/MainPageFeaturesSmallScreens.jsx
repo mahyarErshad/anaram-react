471661b41219728826254ea36c393259b52ead09
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 12:14:02 +0330

    Created the component

---
+++ src/Pages/SingleProduct/SingleProductDetailSection/MainPageFeaturesSmallScreens.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainPageFeaturesSmallScreens() {
+  return (
+    <div>MainPageFeaturesSmallScreens</div>
+  )
+}
+
+export default MainPageFeaturesSmallScreens
