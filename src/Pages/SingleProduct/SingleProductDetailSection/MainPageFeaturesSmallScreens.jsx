2f4af3e9b1b324f8327b518ff23f05345b880b4f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 12:20:30 +0330

    Copied the codes

--- src/Pages/SingleProduct/SingleProductDetailSection/MainPageFeaturesSmallScreens.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/MainPageFeaturesSmallScreens.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { ReactComponent as MedalIcon } from "../../../assets/images/main-page-features/medal.svg";
+import { ReactComponent as TruckIcon } from "../../../assets/images/main-page-features/truck.svg";
+import { ReactComponent as CreditCardIcon } from "../../../assets/images/main-page-features/credit-card.svg";
+import { ReactComponent as CustomerSupportIcon } from "../../../assets/images/main-page-features/customer-support.svg";
+import MainPageFeatureCards from "../../MainPage/MainPageFeatures/MainPageFeatureCards";
 
 function MainPageFeaturesSmallScreens() {
+  const features = [
+    { icon: <MedalIcon />, text: "ضمانت اصلات کالا" },
+    { icon: <TruckIcon />, text: "ارسال به سراسر کشور" },
+    { icon: <CreditCardIcon />, text: "پرداخت امن" },
+    { icon: <CustomerSupportIcon />, text: "مشاوره رایگان خرید" },
+  ];
   return (
-    <section className='w-full flex-col gap-3'>
-    
+    <section className="w-full flex-col gap-3 lg:hidden">
+      <div className="flex-center max-md:gap-2 gap-0 w-full">
+        {features.map((feature, index) => (
+          <MainPageFeatureCards key={index} icon={feature.icon} text={feature.text} />
+        ))}
+      </div>
     </section>
-  )
+  );
 }
 
-export default MainPageFeaturesSmallScreens
+export default MainPageFeaturesSmallScreens;
