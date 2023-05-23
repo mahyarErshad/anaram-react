e72fc98ecd069986e09d0983b4ff35e0c06bb6b2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 11:42:24 +0330

    Added delivery options

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
@@ -3,6 +3,8 @@ import PrimaryButton from "../../../Components/Utils/Buttons/PrimaryButton/Prima
 import { ReactComponent as CartIcon } from "../../../assets/images/icons/bag-icon.svg";
 import { separateNumberByThousand } from "../../../lib/function/convertToPersianDigits";
 import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
+import DeliveryOptionCard from "./DeliveryOptionCard";
+import { ReactComponent as ShopAdd } from "../../../assets/images/icons/shop-add.svg";
 
 function SingleProductSmallScreenAction() {
   return (
@@ -12,6 +14,16 @@ function SingleProductSmallScreenAction() {
         <div className="flex items-center gap-2">
           <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(124000)}</span>
           <TomanIcon className="w-5 h-4" />
+        </div>
+      </div>
+      <div className="flex-col justify-center items-start gap-5 pb-6 px-6 mt-5">
+        <div className="flex justify-start items-center gap-2">
+          <ShopAdd />
+          <span className="text-sm text-Green7 font-semibold">موجود در انبار داناپیوست</span>
+        </div>
+        <div className="flex justify-start items-center gap-6">
+          <DeliveryOptionCard text="تحویل حضوری" />
+          <DeliveryOptionCard text="ارسال رایگان" />
         </div>
       </div>
     </section>
