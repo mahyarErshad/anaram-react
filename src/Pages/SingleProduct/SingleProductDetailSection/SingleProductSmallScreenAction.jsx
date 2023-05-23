e72a148e96cd94c3ebc01e9169cf834f34c8226a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-23 12:42:17 +0330

    Added SingleProductIncreaseDecreaseButton

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductSmallScreenAction.jsx
@@ -5,12 +5,14 @@ import { separateNumberByThousand } from "../../../lib/function/convertToPersian
 import { ReactComponent as TomanIcon } from "../../../assets/images/icons/toman-icon-black.svg";
 import DeliveryOptionCard from "./DeliveryOptionCard";
 import { ReactComponent as ShopAdd } from "../../../assets/images/icons/shop-add.svg";
+import SingleProductIncreaseDecreaseButton from "../../../Components/Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton";
 
 function SingleProductSmallScreenAction() {
   return (
     <section className="w-full bg-white rounded-lg px-3 pt-5 pb-4 flex-col">
       <div className="flex justify-between items-center px-6">
         <PrimaryButton icon={<CartIcon className="stroke-white" />} text="افزودن به سبد" />
+        <SingleProductIncreaseDecreaseButton state={state} setState={setState} />
         <div className="flex items-center gap-2">
           <span className="text-NeutralN500 font-semibold text-lg">{separateNumberByThousand(124000)}</span>
           <TomanIcon className="w-5 h-4" />
