c24d66aac6f2a0ace0a6fc1ee6a85784d3453497
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-24 09:58:34 +0330

    Defined like-dislike-buttons className

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
@@ -30,9 +30,9 @@ function ClientComment({ data }) {
           <LikeDislikeButton className={`${likeState === "liked" ? "bg-primary1" : "bg-NeutralN10"}`} onClick={handleLike} icon={<LikeIcon className={`${likeState === "liked" ? "stroke-primary6 fill-primary6" : "stroke-NeutralN300"}`} />} counter={likeState === "liked" ? data.likes + 1 : data.likes} />
         </div>
       </div>
-      <p className="text-xs text-NeutralN500 font-semibold mr-[3.25rem]">{data.comment}</p>
+      <p className="text-xs text-NeutralN500 font-semibold mr-[3.25rem] user-comment">{data.comment}</p>
       {data.AdminReply && (
-        <div className="flex-col gap-2 mt-6 mr-[3.25rem]">
+        <div className="flex-col gap-2 mt-6 mr-[3.25rem]">     
           <div className="flex gap-3">
             <img className="w-10 h-10 rounded-full" src={adminAvatar} alt="Anaram admin avatar" />
             <div className="flex-col justify-center gap-1">
--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/LikeDislikeButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function LikeDislikeButton({ counter, icon, onClick, className }) {
   return (
-    <button onClick={onClick} className={`${className} flex-center py-2 pl-2 pr-4 gap-3 rounded-5px min-w-[5.3125rem]`}>
+    <button onClick={onClick} className={`${className} flex-center py-2 pl-2 pr-4 gap-3 rounded-5px like-dislike-buttons min-w-[5.3125rem]`}>
       <span className="text-sm text-NeutralN300 font-semibold">{counter}</span>
       {icon}
     </button>
