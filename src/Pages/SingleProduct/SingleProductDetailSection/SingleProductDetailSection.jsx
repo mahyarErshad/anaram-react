22129bc90158e9f25a644b21f1eab5662fa0ff88
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-24 10:37:23 +0330

    Added SingleProductOfferSection to the start of the page

--- src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
+++ src/Pages/SingleProduct/SingleProductDetailSection/SingleProductDetailSection.jsx
@@ -3,11 +3,13 @@ import BreadCrumbs from "../../../Components/BreadCrumb/BreadCrumb";
 import DetailSectionImage from "./DetailSectionImage";
 import DetailSectionTextSection from "./DetailSectionTextSection";
 import DetailSectionAction from "./DetailSectionAction";
+import SingleProductOfferSection from "./SingleProductOfferSection";
 
-function SingleProductDetailSection({state , setState}) {
+function SingleProductDetailSection({ state, setState }) {
   return (
     <section className="large-container px-6 pt-6 pb-8 flex-col gap-4">
       <BreadCrumbs />
+      <SingleProductOfferSection className="lg:hidden bg-NeutralN10 p-4" />
       <div className="flex gap-4 max-lg:flex-col">
         <DetailSectionImage />
         <DetailSectionTextSection state={state} setState={setState} />
