4e2ebad1ee65136b35ea20b734714bd65307d854
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-25 09:25:13 +0330

    Added whitespace-nowrap to comment name

--- src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
+++ src/Pages/SingleProduct/SingleProductTabSection/Components/ClientComment/ClientComment.jsx
@@ -20,7 +20,7 @@ function ClientComment({ data }) {
         <div className="flex gap-3">
           <img className="w-10 h-10 rounded-full" src={data.avatar} alt="avatar" />
           <div className="flex-col justify-center gap-1">
-            <span className="text-NeutralN500 text-xs font-semibold">{data.name}</span>
+            <span className="text-NeutralN500 text-xs font-semibold whitespace-nowrap">{data.name}</span>
             <span className="text-[0.5625rem] text-NeutralN70">{data.date}</span>
           </div>
           <Rating initialValue={data.rating} size={14} readonly />
