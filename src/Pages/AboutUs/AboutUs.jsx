578201339ac3c35341b994316e1b2e60d947c22d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 10:34:57 +0330

    H1 is styled

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function AboutUs() {
   return (
-    <section className="container bg-white pb-9 px-28 pt-14">
-      <h1>فروشگاه اینترنتی آنارام</h1>
+    <section className="container bg-white pb-9 px-28 pt-14 mt-10 gap-10 rounded-2xl">
+      <h1 className="font-semibold text-xl">
+        فروشگاه اینترنتی <strong className="text-primary6">آنارام</strong>
+      </h1>
     </section>
   );
 }
