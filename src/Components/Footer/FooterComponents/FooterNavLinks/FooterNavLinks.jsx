09fc9ba55b9b3d36383156e50caa9c9f6e39a2ae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 09:31:29 +0330

    Imported and using Link

--- src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
+++ src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FooterNavLinks = ({ data }) => (
   <div className="flex-col gap-4">
     <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">
       {data[0].title}
     </span>
-    <ul className="flex-col gap-3">
+    <div className="flex-col gap-3">
       {data[0].navLinks.map((item, index) => (
-        <li key={index} className="text-sm text-NeutralN200">
+        <Link key={index} className="text-sm text-NeutralN200">
           {item}
-        </li>
+        </Link>
       ))}
-    </ul>
+    </div>
   </div>
 );
 
