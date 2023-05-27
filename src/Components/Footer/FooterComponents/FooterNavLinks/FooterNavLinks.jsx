3daa3532dd086e71a782393eaa994c3a42015c70
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 09:42:30 +0330

    Clicking on Link triggers goToTop()

--- src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
+++ src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import goToTop from "../../../../lib/function/goToTop";
 
 const FooterNavLinks = ({ data }) => (
   <div className="flex-col gap-4">
     <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">{data[0].title}</span>
     <div className="flex-col gap-3">
       {data[0].navLinks.map((item, index) => (
-        <Link to={item.href || "/"} key={index} className="text-sm text-NeutralN200">
+        <Link onClick={() => goToTop()} to={item.href || "/"} key={index} className="text-sm text-NeutralN200">
           {item.text}
         </Link>
       ))}
