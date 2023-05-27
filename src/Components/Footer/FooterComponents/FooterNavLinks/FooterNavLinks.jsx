23c83173e96975cc1531e00894072fcbf66e7f41
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 09:33:37 +0330

    If item has no href, "/" will be replaced

--- src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
+++ src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
@@ -6,7 +6,7 @@ const FooterNavLinks = ({ data }) => (
     <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">{data[0].title}</span>
     <div className="flex-col gap-3">
       {data[0].navLinks.map((item, index) => (
-        <Link to={item.href} key={index} className="text-sm text-NeutralN200">
+        <Link to={item.href || "/"} key={index} className="text-sm text-NeutralN200">
           {item}
         </Link>
       ))}
