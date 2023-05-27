66cabe337a8010eb53f496fa007df75e18627d6f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 10:27:08 +0330

    Added paddings

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AboutUs() {
   return (
-    <section className="container bg-white">
+    <section className="container bg-white pb-9 px-28 pt-14">
       <div>about us</div>
     </section>
   );
