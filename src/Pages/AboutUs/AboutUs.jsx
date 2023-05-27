7804be77a6cbf9e4fa0074a1d1f03e524372d486
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 10:31:01 +0330

    H1 added

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function AboutUs() {
   return (
     <section className="container bg-white pb-9 px-28 pt-14">
-      <div>about us</div>
+      <h1>فروشگاه اینترنتی آنارام</h1>
     </section>
   );
 }
