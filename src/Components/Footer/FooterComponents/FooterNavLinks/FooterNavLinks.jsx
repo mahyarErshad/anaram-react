4ba889a1b7af7611b10f14687c427777c7e0337c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 09:32:21 +0330

    Defined href

--- src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
+++ src/Components/Footer/FooterComponents/FooterNavLinks/FooterNavLinks.jsx
@@ -3,12 +3,10 @@ import { Link } from "react-router-dom";
 
 const FooterNavLinks = ({ data }) => (
   <div className="flex-col gap-4">
-    <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">
-      {data[0].title}
-    </span>
+    <span className="text-base text-primary6 pb-3 border-b-2 border-NeutralN40 font-semibold whitespace-nowrap">{data[0].title}</span>
     <div className="flex-col gap-3">
       {data[0].navLinks.map((item, index) => (
-        <Link key={index} className="text-sm text-NeutralN200">
+        <Link to={item.href} key={index} className="text-sm text-NeutralN200">
           {item}
         </Link>
       ))}
