805576b0188d6907986adad90974702450222ee1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 09:27:27 +0330

    Catching href from props

--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -5,9 +5,9 @@ import styles from "./styles.module.css";
 import headerDropDownMockData from "../../../lib/HeaderDropDownMock/HeaderDropDownMockData";
 import { Link } from "react-router-dom";
 
-function NavLinks({ textColor = "text-NeutralN300", text, hasSubMenu }) {
+function NavLinks({ textColor = "text-NeutralN300", text, hasSubMenu, href }) {
   return (
-    <Link className={`gap-2 text-sm ${textColor} flex-center cursor-pointer px-4 pb-7 font-semibold ${styles.navlink}`}>
+    <Link to={href || "/"} className={`gap-2 text-sm ${textColor} flex-center cursor-pointer px-4 pb-7 font-semibold ${styles.navlink}`}>
       {text}
       {hasSubMenu && <Icon className="stroke-NeutralN70 duration-300" />}
       {hasSubMenu && <HeaderMenuOpenOnHover data={headerDropDownMockData} />}
