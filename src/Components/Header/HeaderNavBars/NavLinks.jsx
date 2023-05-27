0d33cfc0c8449f64bdc851793ba97eb512f3a8bb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 09:25:29 +0330

    Changed the li to Link

--- src/Components/Header/HeaderNavBars/NavLinks.jsx
+++ src/Components/Header/HeaderNavBars/NavLinks.jsx
@@ -3,15 +3,16 @@ import { ReactComponent as Icon } from "../../../assets/images/icons/arrow-icon-
 import HeaderMenuOpenOnHover from "../HeaderMenuOpenOnHover/HeaderMenuOpenOnHover";
 import styles from "./styles.module.css";
 import headerDropDownMockData from "../../../lib/HeaderDropDownMock/HeaderDropDownMockData";
+import { Link } from "react-router-dom";
 
 function NavLinks({ textColor = "text-NeutralN300", text, hasSubMenu }) {
   return (
-    <li className={`gap-2 text-sm ${textColor} flex-center cursor-pointer px-4 pb-7 font-semibold ${styles.navlink}`}>
+    <Link className={`gap-2 text-sm ${textColor} flex-center cursor-pointer px-4 pb-7 font-semibold ${styles.navlink}`}>
       {text}
       {hasSubMenu && <Icon className="stroke-NeutralN70 duration-300" />}
       {hasSubMenu && <HeaderMenuOpenOnHover data={headerDropDownMockData} />}
       <span className="w-[1px] h-[2.0625rem] bg-NeutralN30 translate-x-[-1rem] overflow-visible"></span>
-    </li>
+    </Link>
   );
 }
 
