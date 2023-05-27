7210d8cb0c94f4cb8c17da779ed00c1177a7c990
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-05-27 15:00:06 +0330

    Created a separated class for about-page-gradient

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -34,8 +34,8 @@ function AboutUs() {
               <p className="text-sm text-NeutralN80">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده</p>
             </div>
           </div>
-          <div className="w-2/4">
-            
+          <div className="w-2/4 pr-6">
+            <span className="text-lg font-semibold text-white py-[0.875rem] w-[11.75rem] rounded-2xl">مرداد ۱۳۹۵</span>
           </div>
         </div>
       </section>
