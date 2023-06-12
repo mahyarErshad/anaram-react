53b5dd278b9ff4f0e641b32b545db1dba0574ab4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-12 10:28:58 +0330

    Last card will have no after mobile

--- src/Pages/AboutUs/AboutUsTimeLineCard.jsx
+++ src/Pages/AboutUs/AboutUsTimeLineCard.jsx
@@ -5,7 +5,7 @@ function AboutUsTimeLineCard({ title, city, description, date, leftPattern }) {
   const alignClass = leftPattern ? "md:pl-6 flex justify-end items-start" : "md:pr-6";
 
   return (
-    <div className="w-full flex max-md:flex-col max-md:items-center justify-center relative about-us-timeLine-card">
+    <div className="w-full flex max-md:flex-col max-md:items-center justify-center relative about-us-timeLine-card group">
       <span className="about-page-circle about-page-gradient absolute right-[50%] max-md:hidden"></span>
       <div className={`about-us-timeLine-card__border w-2/4 flex-col gap-4 max-md:gap-6 border-NeutralN30 ${borderClass} md:pl-6 md:pb-12 max-md:border-none max-md:w-full`}>
         <div className="flex justify-start max-md:justify-center items-center gap-2 w-full md:max-w-[25.75rem] max-md:mt-3">
@@ -19,7 +19,7 @@ function AboutUsTimeLineCard({ title, city, description, date, leftPattern }) {
       <div className={`w-2/4 max-md:-order-1 max-md:flex max-md:justify-center ${alignClass}`}>
         <span className="text-lg text-white py-[0.875rem] inline-block flex-center w-[11.75rem] rounded-2xl about-page-gradient md:-translate-y-[35%]">{date}</span>
       </div>
-      <div className="h-20 w-full bg-yellow-500 order-10 md:hidden">
+      <div className="h-20 w-full order-10 md:hidden bg-red-500 group-last-of-type:hidden">
 
       </div>
     </div>
