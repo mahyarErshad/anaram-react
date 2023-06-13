8f86c218d742a23d9697346b594d384edbadc7ed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-13 09:54:24 +0330

    Cleaned the code

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ContactDetails from './ContactDetails'
+import React from "react";
+import ContactDetails from "./ContactDetails";
 
 function ContactUs() {
   return (
-    <section className='w-full flex-center gap-6 px-4 pt-20 pb-72'>
-    <ContactDetails />
+    <section className="w-full flex-center gap-6 px-4 pt-20 pb-72">
+      <ContactDetails />
     </section>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
