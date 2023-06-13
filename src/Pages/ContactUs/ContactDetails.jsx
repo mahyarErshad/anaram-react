de4ffe733b20dbe0a23a3e72d627378d440e4893
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-13 09:41:07 +0330

    Added support phone svg

--- src/Pages/ContactUs/ContactDetails.jsx
+++ src/Pages/ContactUs/ContactDetails.jsx
@@ -10,9 +10,13 @@ function ContactDetails() {
       </div>
       <p className="text-sm text-Gray4 mb-12">۷ روز هفته، ۲۴ ساعت روز، پاسخگوی تماس های شما هستیم.</p>
       <div className="flex-col">
-      <div className="flex justify-between items-center">
-        <span className="text-sm">پشتیبانی</span>
-      </div>
+        <div className="flex justify-between items-center">
+          <span className="text-sm text-Gray2">پشتیبانی</span>
+          <span className="text-sm text-Gray4">مهیار ارشاد</span>
+          <div className="flex-center gap-1">
+          
+          </div>
+        </div>
       </div>
     </section>
   );
