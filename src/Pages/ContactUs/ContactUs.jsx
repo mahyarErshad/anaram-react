15f2555537575931f517204b255feb7c30098b04
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-14 08:50:13 +0330

    Imported ContactForm

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactDetails from "./ContactDetails";
+import ContactForm from "./ContactForm";
 
 function ContactUs() {
   return (
     <section className="w-full flex-center gap-6 px-4 pt-20 pb-72">
       <ContactDetails />
+      <ContactForm />
     </section>
   );
 }
