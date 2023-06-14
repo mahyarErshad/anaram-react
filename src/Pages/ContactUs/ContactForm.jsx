500232b9992a13b59bfafcac05be4ca26de74113
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-14 08:42:38 +0330

    Imported MessageIcon

--- src/Pages/ContactUs/ContactForm.jsx
+++ src/Pages/ContactUs/ContactForm.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { ReactComponent as MessageIcon } from "../../assets/images/icons/contact-form-message-icon.svg";
 
 function ContactForm() {
   return (
     <section className="bg-white p-6 rounded-2xl w-full max-w-[40.4375rem] flex-col lg:max-h-[43.25rem]">
-        
+      <div className="flex justify-start items-center gap-3 mb-5">
+        <MessageIcon />
+        <span className="font-semibold text-xl text-primary6">پشتیبانی</span>
+      </div>
     </section>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
