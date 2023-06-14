9e5d62a305d90268e11a3fa7c7dbaca91b11b7bf
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-14 09:14:39 +0330

    Created the component

--- src/Components/Utils/TextArea/TextArea.jsx
+++ src/Components/Utils/TextArea/TextArea.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function TextArea() {
+function TextArea({ label, className, placeholder }) {
   return (
-    <div>TextArea</div>
-  )
+    <div className="flex-col gap-2 w-full">
+      <label htmlFor={label} className="text-NeutralN500 text-xs font-semibold cursor-pointer">
+        {label}
+      </label>
+      <textarea id={label} placeholder={placeholder || label} className={`p-4 text-sm placeholder:text-NeutralN70 bg-white rounded-lg border border-NeutralN30 focus:border-primary2 outline-none duration-300 ${className}`} />
+    </div>
+  );
 }
 
-export default TextArea
+export default TextArea;
