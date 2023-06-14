2c74a274e9868f5a987e350754b7d13d75944302
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-14 09:05:10 +0330

    Input created

--- src/Pages/ContactUs/ContactForm.jsx
+++ src/Pages/ContactUs/ContactForm.jsx
@@ -4,10 +4,16 @@ import { ReactComponent as MessageIcon } from "../../assets/images/icons/contact
 function ContactForm() {
   return (
     <section className="bg-white p-6 rounded-2xl w-full max-w-[40.4375rem] flex-col lg:max-h-[43.25rem]">
-      <div className="flex justify-start items-center gap-3 mb-5">
+      <div className="flex justify-start items-center gap-3 mb-6">
         <MessageIcon />
         <span className="font-semibold text-xl text-primary6">پشتیبانی</span>
       </div>
+      <p className="text-Gray4 text-sm">با ما در تماس باشید.</p>
+      <form onSubmit={(e) => e.preventDefault} className="flex-col gap-6">
+        <div className="flex-center gap-4">
+
+        </div>
+      </form>
     </section>
   );
 }
