ac5b871be5d71fdb8c21a2d619bffd5300d47633
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-14 13:43:27 +0330

    Page is Fully Responsive

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -4,7 +4,7 @@ import ContactForm from "./ContactForm";
 
 function ContactUs() {
   return (
-    <section className="w-full flex-center max-lg:flex-col max-md:gap-3 gap-6 max-md:px-3 px-4 pt-20 md:pb-24 lg:pb-72">
+    <section className="w-full flex-center max-lg:flex-col max-md:gap-3 gap-6 max-md:px-3 px-4 lg:pt-20 md:pt-12 max-md:pt-3 max-md:pb-16 md:pb-24 lg:pb-72">
       <ContactDetails />
       <ContactForm />
     </section>
