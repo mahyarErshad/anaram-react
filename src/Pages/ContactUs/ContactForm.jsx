0dc5287868d84df7db9b62af63d0b5dd42a08f6e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-14 08:40:37 +0330

    Created the component

---
+++ src/Pages/ContactUs/ContactForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactForm() {
+  return (
+    <section className="bg-white p-6 rounded-2xl w-full max-w-[40.4375rem] flex-col lg:max-h-[43.25rem]">
+        
+    </section>
+  )
+}
+
+export default ContactForm
