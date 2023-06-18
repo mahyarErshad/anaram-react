ea21a61ec3d65648c18371640a97224a8ba80b69
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-18 09:25:17 +0330

    Imported FirstCartPageTitleRow

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import FirstCartPageTitleRow from "../../Components/CartPage/FirstCartPageTitleRow";
 
 function CartPage() {
   return (
     <section className="container bg-white rounded-2xl flex-row p-6 mt-16 mb-72">
       <div className="flex-col w-full">
-
+        <FirstCartPageTitleRow />
       </div>
       <div></div>
     </section>
