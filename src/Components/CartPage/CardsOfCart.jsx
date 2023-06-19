4994c734930dfd74943bef7304f9252b6cdb5db1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:22:12 +0330

    Added a border for cards

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import batteryImage from "../../assets/images/data/SingleProductSwiperImage/battery.png";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
 function CardsOfCart() {
   return (
-    <figure className={`relative grid grid-rows-4 gap-6 ${styles.card}`}>
+    <figure className={`relative grid grid-rows-4 gap-6 pt-6 border-t border-NeutralN30 ${styles.card}`}>
       <img src={batteryImage} alt="Varta Battery" className="row-span-3 max-h-[12.5rem]" />
     </figure>
   );
