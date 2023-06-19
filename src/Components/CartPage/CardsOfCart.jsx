2c133ebb0f9b08f6fc9838f3a9b2b98694fa8232
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:27:12 +0330

    Added price

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import batteryImage from "../../assets/images/data/SingleProductSwiperImage/battery.png";
 import styles from "./styles.module.css";
+import ActionSectionQuantity from "../../Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity";
 
 function CardsOfCart() {
   return (
     <figure className={`relative grid grid-rows-4 gap-6 pt-6 border-t border-NeutralN30 ${styles.card}`}>
       <img src={batteryImage} alt="Varta Battery" className="row-span-3 max-h-[12.5rem]" />
       <h2 className="text-NeutralN500 text-lg font-semibold">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h2>
+      <ActionSectionQuantity title="قیمت محصول :" quantity={124000} toman />
     </figure>
   );
 }
