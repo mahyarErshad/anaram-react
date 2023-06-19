3b815f5ab58078fa500bfb4e389802284dd44d7d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:21:14 +0330

    Image is placed

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import batteryImage from "../../assets/images/data/product-card/battery.png";
+import batteryImage from "../../assets/images/data/SingleProductSwiperImage/battery.png";
 import styles from "./styles.module.css"
 
 function CardsOfCart() {
   return (
-    <figure className="relative grid grid-cols-2 gap-6 grid-rows-4">
-      <img src={batteryImage} alt="Varta Battery" className="row-span-3" />
+    <figure className={`relative grid grid-rows-4 gap-6 ${styles.card}`}>
+      <img src={batteryImage} alt="Varta Battery" className="row-span-3 max-h-[12.5rem]" />
     </figure>
   );
 }
