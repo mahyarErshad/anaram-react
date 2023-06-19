9715283abad8d714a88c4e7c2a68231dde1d9d93
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:15:53 +0330

    Added image tag

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import batteryImage from "../../assets/images/data/product-card/battery.png"
+import React from "react";
+import batteryImage from "../../assets/images/data/product-card/battery.png";
 
 function CardsOfCart() {
   return (
-    <figure className='relative grid grid-cols-2 gap-6 grid-rows-4'>
-
+    <figure className="relative grid grid-cols-2 gap-6 grid-rows-4">
+      <img src={batteryImage} alt="Varta Battery" className="row-span-3" />
     </figure>
-  )
+  );
 }
 
-export default CardsOfCart
+export default CardsOfCart;
