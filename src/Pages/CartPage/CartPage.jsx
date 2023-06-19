68b381ec0ffee4225d444d54fc0b07050ea2b7ef
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:16:09 +0330

    Imported CardsOfCart

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FirstCartPageTitleRow from "../../Components/CartPage/FirstCartPageTitleRow";
+import CardsOfCart from "../../Components/CartPage/CardsOfCart";
 
 function CartPage() {
   return (
     <section className="container bg-white rounded-2xl flex-row p-6 mt-16 mb-72">
       <div className="flex-col w-full">
         <FirstCartPageTitleRow />
+        <CardsOfCart />
       </div>
       <div></div>
     </section>
