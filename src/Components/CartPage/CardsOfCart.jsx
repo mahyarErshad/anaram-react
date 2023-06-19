73d204dc4dc9ffcaf8a2d2f99cca3799a6554471
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:14:43 +0330

    Imported battery image

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import batteryImage from "../../assets/images/data/product-card/battery.png"
 
 function CardsOfCart() {
   return (
     <figure className='relative grid grid-cols-2 gap-6 grid-rows-4'>
-    
+
     </figure>
   )
 }
