076e28dc7563982b0091999f15f8c8ea1b480039
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 10:06:58 +0330

    Component accepts counter as prop

--- src/Components/CartPage/FirstCartPageTitleRow.jsx
+++ src/Components/CartPage/FirstCartPageTitleRow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactComponent as BagIcon } from "../../assets/images/icons/bag-icon.svg";
 import { ReactComponent as Dots } from "../../assets/images/icons/3dots.svg";
 
-function FirstCartPageTitleRow() {
+function FirstCartPageTitleRow({ counter }) {
   return (
     <div className="flex justify-between items-center mb-3">
       <div className="flex items-center">
@@ -10,7 +10,7 @@ function FirstCartPageTitleRow() {
           <BagIcon stroke="#575757" />
         </span>
         <span className="text-base text-NeutralN500 mr-2">سبد خرید</span>
-        <span className="mr-1 font-semibold text-xs flex-center bg-primary6 text-white rounded-full w-6 h-6">3</span>
+        <span className="mr-1 font-semibold text-xs flex-center bg-primary6 text-white rounded-full w-6 h-6">{counter}</span>
       </div>
       <button className="bg-NeutralN10 rounded-5px flex-center">
         <Dots />
