99a1c7026aa9b6691754a887d7b27bfd6899cf94
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:40:54 +0330

    Setting an state for the price

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -3,14 +3,16 @@ import batteryImage from "../../assets/images/data/SingleProductSwiperImage/batt
 import styles from "./styles.module.css";
 import ActionSectionQuantity from "../../Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity";
 import SingleProductIncreaseDecreaseButton from "../Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton";
+import { useState } from "react";
 
 function CardsOfCart() {
+  const [state, setState] = useState(1);
   return (
     <figure className={`relative grid gap-6 pt-6 border-t border-NeutralN30 ${styles.card}`}>
       <img src={batteryImage} alt="Varta Battery" className="row-span-3 max-h-[12.5rem]" />
       <h2 className="text-NeutralN500 text-lg font-semibold">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h2>
-      <ActionSectionQuantity className="max-w-fit gap-6 border-none pb-0" title="قیمت محصول :" quantity={124000} toman />
-      <SingleProductIncreaseDecreaseButton />
+      <ActionSectionQuantity className="max-w-fit gap-6 border-none pb-0" title="قیمت محصول :" quantity={124000 * state} toman />
+      <SingleProductIncreaseDecreaseButton state={state} setState={setState} />
     </figure>
   );
 }
