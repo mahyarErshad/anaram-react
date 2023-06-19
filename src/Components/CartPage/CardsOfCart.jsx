8faaab59b439e1b85de334dbc074844cf2d5fd2b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:23:49 +0330

    Added h2

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -6,6 +6,7 @@ function CardsOfCart() {
   return (
     <figure className={`relative grid grid-rows-4 gap-6 pt-6 border-t border-NeutralN30 ${styles.card}`}>
       <img src={batteryImage} alt="Varta Battery" className="row-span-3 max-h-[12.5rem]" />
+      <h2 className="text-NeutralN500 text-lg font-semibold">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h2>
     </figure>
   );
 }
