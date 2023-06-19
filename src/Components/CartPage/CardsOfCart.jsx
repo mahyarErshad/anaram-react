fc6a6bc0491716f906768d85c09a2efbfef04f87
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 09:38:19 +0330

    Changed the style of rows

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import batteryImage from "../../assets/images/data/SingleProductSwiperImage/battery.png";
 import styles from "./styles.module.css";
 import ActionSectionQuantity from "../../Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity";
+import SingleProductIncreaseDecreaseButton from "../Utils/Buttons/SingleProductIncreaseDecreaseButton/SingleProductIncreaseDecreaseButton";
 
 function CardsOfCart() {
   return (
-    <figure className={`relative grid grid-rows-4 gap-6 pt-6 border-t border-NeutralN30 ${styles.card}`}>
+    <figure className={`relative grid gap-6 pt-6 border-t border-NeutralN30 ${styles.card}`}>
       <img src={batteryImage} alt="Varta Battery" className="row-span-3 max-h-[12.5rem]" />
       <h2 className="text-NeutralN500 text-lg font-semibold">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h2>
-      <ActionSectionQuantity className="max-w-fit gap-6" title="قیمت محصول :" quantity={124000} toman />
+      <ActionSectionQuantity className="max-w-fit gap-6 border-none pb-0" title="قیمت محصول :" quantity={124000} toman />
+      <SingleProductIncreaseDecreaseButton />
     </figure>
   );
 }
