8ad28556892272d346a7f5fff63f9e0243ab412b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-19 10:05:54 +0330

    Cards are deleted by Clicking on remove button

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import FirstCartPageTitleRow from "../../Components/CartPage/FirstCartPageTitleRow";
 import CardsOfCart from "../../Components/CartPage/CardsOfCart";
 
 function CartPage() {
+  const [data, setData] = useState([1, 2, 3]);
+
+  function handleClick(index) {
+    setData(data.filter((_, i) => i !== index));
+  }
+
   return (
     <section className="container bg-white rounded-2xl flex-row p-6 mt-16 mb-72">
       <div className="flex-col w-full">
         <FirstCartPageTitleRow />
-        <CardsOfCart />
+        {data.map((_, index) => (
+          <CardsOfCart onClick={() => handleClick(index)} key={index} />
+        ))}
       </div>
       <div></div>
     </section>
