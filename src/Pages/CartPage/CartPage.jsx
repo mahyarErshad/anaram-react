775aa0c0343b546365f8ba55bc3f6d34f0e6cb1e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-21 08:56:19 +0330

    Added a ActionSectionQuantity

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from "react";
 import FirstCartPageTitleRow from "../../Components/CartPage/FirstCartPageTitleRow";
 import CardsOfCart from "../../Components/CartPage/CardsOfCart";
+import ActionSectionQuantity from "../SingleProduct/SingleProductDetailSection/ActionSectionQuantity";
 
 function CartPage() {
   const [data, setData] = useState([1, 2, 3]);
@@ -17,7 +18,9 @@ function CartPage() {
           <CardsOfCart onClick={() => handleClick(index)} key={index} />
         ))}
       </div>
-      <div className="pt-10 pb-6 px-6 flex-col bg-NeutralN10 border border-NeutralN20 rounded-2xl"></div>
+      <div className="pt-10 pb-6 px-6 flex-col bg-NeutralN10 border border-NeutralN20 rounded-2xl min-w-[24.375rem]">
+        <ActionSectionQuantity className="pb-8 border-b border-NeutralN30" title="تعداد محصولات :" quantity="3" counter />
+      </div>
     </section>
   );
 }
