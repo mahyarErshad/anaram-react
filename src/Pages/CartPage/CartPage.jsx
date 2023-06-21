0e2768cfc1282e190cf84fdf1e305e65a0cc7911
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-21 09:06:48 +0330

    Added the button

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -21,7 +21,8 @@ function CartPage() {
       <div className="pt-10 pb-6 px-6 flex-col bg-NeutralN10 border border-NeutralN20 rounded-2xl min-w-[24.375rem]">
         <ActionSectionQuantity className="pb-8 border-b border-NeutralN30" title="تعداد محصولات :" quantity="3" counter />
         <ActionSectionQuantity className="border-b border-NeutralN30 pt-8 pb-4" title="قیمت نهایی محصولات :" quantity={620000} toman />
-        <p className="text-NeutralN100 text-center text-xs pb-6 border-b border-NeutralN30">مبلغ نهایی سفارش شامل مالیات بر ارزش افزوده است.</p>
+        <p className="text-NeutralN100 text-center text-xs pb-6 mb-6 border-b border-NeutralN30">مبلغ نهایی سفارش شامل مالیات بر ارزش افزوده است.</p>
+        <button className="bg-transparent p-0 mb-8 self-start text-primary6 text-xs hover:text-primary8 duration-300">کد تخفیف دارم</button>
       </div>
     </section>
   );
