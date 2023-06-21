0728873de1af6735776ce7f7caacc3922f110dd6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-21 09:27:48 +0330

    Redirect to product page is done

--- src/Components/CartPage/CardsOfCart.jsx
+++ src/Components/CartPage/CardsOfCart.jsx
@@ -8,12 +8,15 @@ import { Link } from "react-router-dom";
 
 function CardsOfCart({ onClick }) {
   const [state, setState] = useState(1);
+  const href = "/products/product/id-1234";
   return (
     <figure className={`relative mb-8 grid gap-6 pt-6 border-t border-NeutralN30 first-of-type:border-t-0 ${styles.card}`}>
-      <Link className="row-span-3" to="product/id-1234">
+      <Link className="row-span-3" to={href}>
         <img src={batteryImage} alt="Varta Battery" className="max-h-[12.5rem]" />
       </Link>
-      <h2 className="text-NeutralN500 text-lg font-semibold">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h2>
+      <Link to={href}>
+        <h2 className="text-NeutralN500 text-lg font-semibold hover:text-primary6 duration-300">باتری نیم قلمی آلکالاین وارتا Longlife Power بسته 6 عددی</h2>
+      </Link>
       <ActionSectionQuantity className="max-w-fit gap-6 border-none pb-0" title="قیمت محصول :" quantity={124000} toman />
       <SingleProductIncreaseDecreaseButton state={state} setState={setState} />
       <ActionSectionQuantity className="max-w-fit gap-6 border-none pb-0 col-start-2" title="قیمت کل :" quantity={124000 * state} toman />
