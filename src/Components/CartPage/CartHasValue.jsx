d5404eae540a0b26d49af9f89436f09d200b74a1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 08:46:17 +0330

    Copied the codes

--- src/Components/CartPage/CartHasValue.jsx
+++ src/Components/CartPage/CartHasValue.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import FirstCartPageTitleRow from "./FirstCartPageTitleRow";
+import CardsOfCart from "./CardsOfCart";
+import ActionSectionQuantity from "../../Pages/SingleProduct/SingleProductDetailSection/ActionSectionQuantity";
+import PrimaryButton from "../Utils/Buttons/PrimaryButton/PrimaryButton";
+import { useState } from "react";
+import { useCallback } from "react";
 
 function CartHasValue() {
+  const [data, setData] = useState([1, 2, 3]);
+
+  const handleClick = useCallback((index) => {
+    setData((prevData) => prevData.filter((_, i) => i !== index));
+  }, []);
+
   return (
-    <div>CartHasValue</div>
-  )
+    <>
+      <div className="flex-col flex-grow">
+        <FirstCartPageTitleRow counter={data.length} />
+        {data.map((_, index) => (
+          <CardsOfCart onClick={() => handleClick(index)} key={index} />
+        ))}
+      </div>
+      <div className="pt-10 pb-6 px-6 flex-col bg-NeutralN10 border border-NeutralN20 rounded-2xl min-w-[24.375rem]">
+        <ActionSectionQuantity className="pb-8 border-b border-NeutralN30" title="تعداد محصولات :" quantity="3" counter />
+        <ActionSectionQuantity className="border-b border-NeutralN30 pt-8 pb-4" title="قیمت نهایی محصولات :" quantity={620000} toman />
+        <p className="text-NeutralN100 text-center text-xs pb-6 mb-6 border-b border-NeutralN30">مبلغ نهایی سفارش شامل مالیات بر ارزش افزوده است.</p>
+        <button className="bg-transparent p-0 mb-8 self-start text-primary6 text-xs hover:text-primary8 duration-300">کد تخفیف دارم</button>
+        <PrimaryButton text="ادامه" />
+      </div>
+    </>
+  );
 }
 
-export default CartHasValue
+export default CartHasValue;
--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,31 +1,10 @@
-import React, { useCallback, useState } from "react";
-import FirstCartPageTitleRow from "../../Components/CartPage/FirstCartPageTitleRow";
-import CardsOfCart from "../../Components/CartPage/CardsOfCart";
-import ActionSectionQuantity from "../SingleProduct/SingleProductDetailSection/ActionSectionQuantity";
-import PrimaryButton from "../../Components/Utils/Buttons/PrimaryButton/PrimaryButton";
+import React from "react";
 
 function CartPage() {
-  const [data, setData] = useState([1, 2, 3]);
-
-  const handleClick = useCallback((index) => {
-    setData((prevData) => prevData.filter((_, i) => i !== index));
-  }, []);
 
   return (
     <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
-      <div className="flex-col flex-grow">
-        <FirstCartPageTitleRow counter={data.length} />
-        {data.map((_, index) => (
-          <CardsOfCart onClick={() => handleClick(index)} key={index} />
-        ))}
-      </div>
-      <div className="pt-10 pb-6 px-6 flex-col bg-NeutralN10 border border-NeutralN20 rounded-2xl min-w-[24.375rem]">
-        <ActionSectionQuantity className="pb-8 border-b border-NeutralN30" title="تعداد محصولات :" quantity="3" counter />
-        <ActionSectionQuantity className="border-b border-NeutralN30 pt-8 pb-4" title="قیمت نهایی محصولات :" quantity={620000} toman />
-        <p className="text-NeutralN100 text-center text-xs pb-6 mb-6 border-b border-NeutralN30">مبلغ نهایی سفارش شامل مالیات بر ارزش افزوده است.</p>
-        <button className="bg-transparent p-0 mb-8 self-start text-primary6 text-xs hover:text-primary8 duration-300">کد تخفیف دارم</button>
-        <PrimaryButton text="ادامه" />
-      </div>
+
     </section>
   );
 }
