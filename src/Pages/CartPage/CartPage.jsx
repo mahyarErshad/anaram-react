c94b857e644471976b232ecd7b23a5ba99b082cb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:28:30 +0330

    Passed state and children to the Modal

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from "react";
 import CartHasValue from "../../Components/CartPage/CartHasValue";
 import CartIsEmpty from "../../Components/CartPage/CartIsEmpty";
 import Modal from "../../Components/Modal/Modal";
+import CartModal from "../../Components/CartPage/CartModal";
 
 function CartPage() {
   const [data, setData] = useState([1, 2, 3]);
@@ -13,7 +14,7 @@ function CartPage() {
   return (
     <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
       {data.length > 0 ? <CartHasValue data={data} handleClick={handleClick} /> : <CartIsEmpty data={data} />}
-      <Modal />
+      <Modal isOpen={modalOpen} children={<CartModal />} />
     </section>
   );
 }
