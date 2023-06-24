ff539a09b19d538bfcc711f87ffa0d73a34867a9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:01:52 +0330

    Added cart is empty image

--- src/Components/CartPage/CartIsEmpty.jsx
+++ src/Components/CartPage/CartIsEmpty.jsx
@@ -5,6 +5,9 @@ function CartIsEmpty({ data }) {
   return (
     <div className="flex-grow">
       <FirstCartPageTitleRow counter={data.length} />
+      <div className="flex-col gap-2 rounded-2xl bg-NeutralN10 pt-16 pb-4 mt-14">
+
+      </div>
     </div>
   );
 }
