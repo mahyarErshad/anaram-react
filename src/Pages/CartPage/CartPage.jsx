a0f010280eecc1491594116546cde0f60f66b545
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:19:58 +0330

    Created a state for modal

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -4,11 +4,16 @@ import CartIsEmpty from "../../Components/CartPage/CartIsEmpty";
 
 function CartPage() {
   const [data, setData] = useState([1, 2, 3]);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const handleClick = useCallback((index) => {
     setData((prevData) => prevData.filter((_, i) => i !== index));
   }, []);
-  return <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">{data.length > 0 ? <CartHasValue data={data} handleClick={handleClick} /> : <CartIsEmpty data={data} />}</section>;
+  return (
+    <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
+      {data.length > 0 ? <CartHasValue data={data} handleClick={handleClick} /> : <CartIsEmpty data={data} />}
+    </section>
+  );
 }
 
 export default CartPage;
