610acd6b32ffd86a61c8a0b413949b5ac618b311
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:29:45 +0330

    Passed down the setModalOpen

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -13,7 +13,7 @@ function CartPage() {
   }, []);
   return (
     <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
-      {data.length > 0 ? <CartHasValue data={data} handleClick={handleClick} /> : <CartIsEmpty data={data} />}
+      {data.length > 0 ? <CartHasValue setModalOpen={setModalOpen} data={data} handleClick={handleClick} /> : <CartIsEmpty data={data} />}
       <Modal isOpen={modalOpen} children={<CartModal />} />
     </section>
   );
