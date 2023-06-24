20160746043631b8640765ed5ed97039795d1bc8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 10:40:49 +0330

    onClick is the new prop

--- src/Components/Utils/Buttons/GhostButton/GhostButton.jsx
+++ src/Components/Utils/Buttons/GhostButton/GhostButton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function GhostButton({ text, className }) {
-  return <button className={`flex-center rounded-md bg-transparent border py-[0.625rem] w-[8rem] text-sm duration-150 ${className}`}>{text}</button>;
+function GhostButton({ text, className, onClick }) {
+  return (
+    <button onClick={onClick} className={`flex-center rounded-md bg-transparent border py-[0.625rem] w-[8rem] text-sm duration-150 ${className}`}>
+      {text}
+    </button>
+  );
 }
 
 export default GhostButton;
