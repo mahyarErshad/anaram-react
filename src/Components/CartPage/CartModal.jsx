e359cc5f3deb8e9f2a996c6bdba564c34e9c0e2c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:56:34 +0330

    Cleaned the code

--- src/Components/CartPage/CartModal.jsx
+++ src/Components/CartPage/CartModal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CartModal({setModalOpen}) {
   return (
-    <div>CartModal</div>
+    <div className='bg-white rounded-2xl w-full max-w-[40.625rem] px-4 pt-4 pb-11'>CartModal</div>
   )
 }
 
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -3,16 +3,8 @@ import styles from "./styles.module.css";
 
 function Modal({ children, isOpen }) {
   const backdropClass = isOpen ? `fixed top-0 bottom-0 right-0 left-0 w-full h-full flex-center bg-black ${styles["modal-fadeIn"]}` : "hidden";
-  const contentClass = isOpen ? "opacity-100" : "modal-content opacity-0 pointer-events-none";
 
-  if (isOpen)
-    return (
-      <div>
-        <div className={`${backdropClass}`}>
-          <div className={`${contentClass}`}>{children}</div>
-        </div>
-      </div>
-    );
+  if (isOpen) return <div className={`${backdropClass}`}>{children}</div>;
 }
 
 export default Modal;
