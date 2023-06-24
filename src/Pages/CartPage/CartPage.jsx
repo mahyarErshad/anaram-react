18d8658f189e0be8fb94cc2181375f771537f2ae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 08:46:40 +0330

    Imported CartHasValue

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import CartHasValue from "../../Components/CartPage/CartHasValue";
 
 function CartPage() {
-
   return (
     <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
-
+      <CartHasValue />
     </section>
   );
 }
