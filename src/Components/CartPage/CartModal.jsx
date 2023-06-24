22077f685a9106c0cb8ca3c569b5115fabaed5a4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:57:45 +0330

    Imported CloseIcon

--- src/Components/CartPage/CartModal.jsx
+++ src/Components/CartPage/CartModal.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { ReactComponent as CloseIcon } from '../../assets/images/icons/close-icon.svg'
 
 function CartModal({setModalOpen}) {
   return (
-    <div className='bg-white rounded-2xl w-full max-w-[40.625rem] px-4 pt-4 pb-11'>CartModal</div>
+    <div className='flex-col flex-center bg-white rounded-2xl w-full max-w-[40.625rem] px-4 pt-4 pb-11'>
+
+    </div>
   )
 }
 
