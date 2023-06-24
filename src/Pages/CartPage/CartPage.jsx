fffda1c8b9eb87f910af5128c92f2aea659ba6e3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 08:53:45 +0330

    CartHasValue is rendered while data.length > 0

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -9,7 +9,9 @@ function CartPage() {
   }, []);
   return (
     <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
-      <CartHasValue data={data} handleClick={handleClick} />
+      {
+        data.length > 0 && <CartHasValue data={data} handleClick={handleClick} />
+      }
     </section>
   );
 }
