0c4b3eee37ee65d0224fac063caca87b02591666
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:46:07 +0330

    Imported styles

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./styles.module.css"
 
 function Modal({ children, isOpen }) {
   const backdropClass = isOpen ? "fixed top-0 bottom-0 right-0 left-0 w-full h-full flex-center bg-black modal-fadeIn" : "hidden";
