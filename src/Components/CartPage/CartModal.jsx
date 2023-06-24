16c5ea1afaa4a93a9a36af7158072e9ac931dd63
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 10:01:44 +0330

    Closing Modal is done

--- src/Components/CartPage/CartModal.jsx
+++ src/Components/CartPage/CartModal.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { ReactComponent as CloseIcon } from '../../assets/images/icons/close-icon.svg'
+import React from "react";
+import { ReactComponent as CloseIcon } from "../../assets/images/icons/close-icon.svg";
 
-function CartModal({setModalOpen}) {
+function CartModal({ setModalOpen }) {
   return (
-    <div className='flex-col flex-center bg-white rounded-2xl w-full max-w-[40.625rem] px-4 pt-4 pb-11'>
-
+    <div className="flex-col flex-center bg-white rounded-2xl w-full max-w-[40.625rem] px-4 pt-4 pb-11">
+      <button className="self-end mb-2" onClick={() => setModalOpen(false)}>
+        <CloseIcon />
+      </button>
     </div>
-  )
+  );
 }
 
-export default CartModal
+export default CartModal;
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css";
 function Modal({ children, isOpen }) {
   const backdropClass = isOpen ? `fixed top-0 bottom-0 right-0 left-0 w-full h-full flex-center bg-black ${styles["modal-fadeIn"]}` : "hidden";
 
-  if (isOpen) return <div className={`${backdropClass}`}>{children}</div>;
+  if (isOpen) return <div className={`${backdropClass} z-50`}>{children}</div>;
 }
 
 export default Modal;
