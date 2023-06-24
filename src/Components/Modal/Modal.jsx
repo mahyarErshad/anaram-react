d717ff2bc0d0c6d4994a998fe77d57428baa9494
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:27:17 +0330

    Modal is created

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
 
-function Modal() {
-  return (
-    <div>Modal</div>
-  )
+function Modal({ children, isOpen }) {
+  const opacity = isOpen ? "opacity-25" : "opacity-0";
+  if (isOpen) return <div className={`fixed top-0 bottom-0 right-0 left-0 w-full h-full flex-center bg-black ${opacity} blur-md duration-300`}>{children}</div>;
 }
 
-export default Modal
+export default Modal;
