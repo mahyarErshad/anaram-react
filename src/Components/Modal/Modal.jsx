5499b0bc897af76e1e44efcf6b5e99ae73eec9f9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:44:50 +0330

    Modal is rendered conditionally

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -4,13 +4,14 @@ function Modal({ children, isOpen }) {
   const backdropClass = isOpen ? "fixed top-0 bottom-0 right-0 left-0 w-full h-full flex-center bg-black modal-fadeIn" : "hidden";
   const contentClass = isOpen ? "opacity-100" : "modal-content opacity-0 pointer-events-none";
 
-  return (
-    <div>
-      <div className={`${backdropClass}`}>
-        <div className={`${contentClass}`}>{children}</div>
+  if (isOpen)
+    return (
+      <div>
+        <div className={`${backdropClass}`}>
+          <div className={`${contentClass}`}>{children}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Modal;
