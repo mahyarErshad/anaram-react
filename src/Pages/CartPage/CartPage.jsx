b16eaa2a8dc3cd45f6f36a81f30716f45737e36a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:12:19 +0330

    Merged the 2 if statements

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -8,12 +8,7 @@ function CartPage() {
   const handleClick = useCallback((index) => {
     setData((prevData) => prevData.filter((_, i) => i !== index));
   }, []);
-  return (
-    <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
-      {data.length > 0 && <CartHasValue data={data} handleClick={handleClick} />}
-      {data.length <= 0 && <CartIsEmpty data={data} />}
-    </section>
-  );
+  return <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">{data.length > 0 ? <CartHasValue data={data} handleClick={handleClick} /> : <CartIsEmpty data={data} />}</section>;
 }
 
 export default CartPage;
