1ba76e43103b39407b2383674efbb639bb12eaef
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:43:34 +0330

    Modal coming with an animation

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 function Modal({ children, isOpen }) {
-  const opacity = isOpen ? "opacity-25" : "opacity-0";
-  if (isOpen) return <div className={`fixed top-0 bottom-0 right-0 left-0 w-full h-full flex-center bg-black ${opacity} blur-md duration-300`}>{children}</div>;
+  const backdropClass = isOpen ? "fixed top-0 bottom-0 right-0 left-0 w-full h-full flex-center bg-black modal-fadeIn" : "hidden";
+  const contentClass = isOpen ? "opacity-100" : "modal-content opacity-0 pointer-events-none";
+
+  return (
+    <div>
+      <div className={`${backdropClass}`}>
+        <div className={`${contentClass}`}>{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
