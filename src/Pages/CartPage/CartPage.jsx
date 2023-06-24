e611c2c9eae30ee66c23113becd7554c6b0f09ab
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-24 09:27:48 +0330

    Imported Modal

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from "react";
 import CartHasValue from "../../Components/CartPage/CartHasValue";
 import CartIsEmpty from "../../Components/CartPage/CartIsEmpty";
+import Modal from "../../Components/Modal/Modal";
 
 function CartPage() {
   const [data, setData] = useState([1, 2, 3]);
@@ -12,6 +13,7 @@ function CartPage() {
   return (
     <section className="flex items-start max-w-[82.5rem] w-full mx-auto rounded-2xl p-6 bg-white mt-16 mb-72 gap-14">
       {data.length > 0 ? <CartHasValue data={data} handleClick={handleClick} /> : <CartIsEmpty data={data} />}
+      <Modal />
     </section>
   );
 }
