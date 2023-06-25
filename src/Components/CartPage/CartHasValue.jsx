5cdac6e402bb8fd852d085b59ad11ac4949e31dd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-06-25 08:39:55 +0330

    Made a form for coupon

--- src/Components/CartPage/CartHasValue.jsx
+++ src/Components/CartPage/CartHasValue.jsx
@@ -6,6 +6,9 @@ import PrimaryButton from "../Utils/Buttons/PrimaryButton/PrimaryButton";
 
 function CartHasValue({ data, handleClick, setModalOpen }) {
   const [coupon, setCoupon] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <>
       <div className="flex-col flex-grow">
@@ -18,11 +21,15 @@ function CartHasValue({ data, handleClick, setModalOpen }) {
         <ActionSectionQuantity className="pb-8 border-b border-NeutralN30" title="تعداد محصولات :" quantity="3" counter />
         <ActionSectionQuantity className="border-b border-NeutralN30 pt-8 pb-4" title="قیمت نهایی محصولات :" quantity={620000} toman />
         <p className="text-NeutralN100 text-center text-xs pb-6 mb-6 border-b border-NeutralN30">مبلغ نهایی سفارش شامل مالیات بر ارزش افزوده است.</p>
-        <button onClick={setCoupon((prev) => !prev)} className="bg-transparent p-0 mb-8 self-start text-primary6 text-xs hover:text-primary8 duration-300">
+        <button onClick={() => setCoupon((prev) => !prev)} className="bg-transparent p-0 self-start text-primary6 text-xs hover:text-primary8 duration-300">
           کد تخفیف دارم
         </button>
-        <input type="search" />
-        <PrimaryButton text="ادامه" />
+        {coupon && (
+          <form onSubmit={(e) => handleSubmit(e)} className="w-full min-h-8 bg-white rounded-lg border border-NeutralN30 pr-2 py-1 pl-1 flex-center mt-4">
+            <input className="border-none outline-none flex-grow h-full" />
+          </form>
+        )}
+        <PrimaryButton className="mt-8" text="ادامه" />
       </div>
     </>
   );
